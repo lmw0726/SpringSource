e0653446f4d438447f6af90c4d0c9c031867a27b
LMW <CNlmw0726@gmail.com>
2023-12-24 22:19:18 +0800

    docs(ArgumentResolverConfigurer): 添加自定义方法参数解析器代码的注释

--- spring-webflux/src/main/java/org/springframework/web/reactive/result/method/annotation/ArgumentResolverConfigurer.java
+++ spring-webflux/src/main/java/org/springframework/web/reactive/result/method/annotation/ArgumentResolverConfigurer.java
@@ -16,35 +16,45 @@
 
 package org.springframework.web.reactive.result.method.annotation;
 
+import org.springframework.util.Assert;
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver;
-
 /**
- * Helps to configure resolvers for Controller method arguments.
+ * 帮助配置控制器方法参数的解析器。
  *
  * @author Rossen Stoyanchev
  * @since 5.0
  */
 public class ArgumentResolverConfigurer {
 
+	/**
+	 * 自定义解析器列表
+	 */
 	private final List<HandlerMethodArgumentResolver> customResolvers = new ArrayList<>(8);
 
 
 	/**
-	 * Configure resolvers for custom controller method arguments.
-	 * @param resolver the resolver(s) to add
+	 * 配置自定义控制器方法参数的解析器。
+	 *
+	 * @param resolver 要添加的解析器（们）
 	 */
 	public void addCustomResolver(HandlerMethodArgumentResolver... resolver) {
+		// 确保解析器不为 null
 		Assert.notNull(resolver, "'resolvers' must not be null");
+		// 将解析器添加到自定义解析器列表中
 		this.customResolvers.addAll(Arrays.asList(resolver));
 	}
 
 
+	/**
+	 * 获取自定义解析器列表的方法。
+	 *
+	 * @return 自定义解析器列表
+	 */
 	List<HandlerMethodArgumentResolver> getCustomResolvers() {
 		return this.customResolvers;
 	}
