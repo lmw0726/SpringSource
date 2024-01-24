01876be12bc9702c8478bb698fb1a33669484e14
LMW <CNlmw0726@gmail.com>
2024-01-24 23:03:30 +0800

    docs: 添加InitializingBean的代码注释

--- spring-beans/src/main/java/org/springframework/beans/factory/InitializingBean.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/InitializingBean.java
@@ -17,13 +17,11 @@
 package org.springframework.beans.factory;
 
 /**
- * Interface to be implemented by beans that need to react once all their properties
- * have been set by a {@link BeanFactory}: e.g. to perform custom initialization,
- * or merely to check that all mandatory properties have been set.
+ * 需要在由{@link BeanFactory}设置了所有bean属性之后做出反应的bean实现的接口：
+ * 例如，执行自定义初始化，或者仅仅检查是否已设置了所有必需的属性。
  *
- * <p>An alternative to implementing {@code InitializingBean} is specifying a custom
- * init method, for example in an XML bean definition. For a list of all bean
- * lifecycle methods, see the {@link BeanFactory BeanFactory javadocs}.
+ * <p>实现{@code InitializingBean}的另一种方法是指定自定义初始化方法，例如在XML bean定义中。
+ * 有关所有bean生命周期方法的列表，请参见{@link BeanFactory BeanFactory javadocs}。
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
@@ -34,12 +32,11 @@ package org.springframework.beans.factory;
 public interface InitializingBean {
 
 	/**
-	 * Invoked by the containing {@code BeanFactory} after it has set all bean properties
-	 * and satisfied {@link BeanFactoryAware}, {@code ApplicationContextAware} etc.
-	 * <p>This method allows the bean instance to perform validation of its overall
-	 * configuration and final initialization when all bean properties have been set.
-	 * @throws Exception in the event of misconfiguration (such as failure to set an
-	 * essential property) or if initialization fails for any other reason
+	 * 在包含的{@code BeanFactory}设置了所有bean属性并满足{@link BeanFactoryAware}、
+	 * {@code ApplicationContextAware}等之后调用。
+	 * <p>此方法允许bean实例在设置了所有bean属性后执行其整体配置的验证和最终初始化。
+	 *
+	 * @throws Exception 如果存在配置错误（例如未设置必需属性）或由于其他原因导致初始化失败
 	 */
 	void afterPropertiesSet() throws Exception;
 
