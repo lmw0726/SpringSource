9aa1269fdd2350fa1f4baa5db2e5b107cf6bbefa
LMW <CNlmw0726@gmail.com>
2024-01-25 23:54:58 +0800

    feat: 添加bean生命周期的示例

--- spring-learn/src/main/java/com/lmw/learn/bean/life/cycle/LifeCycleBean.java
+++ spring-learn/src/main/java/com/lmw/learn/bean/life/cycle/LifeCycleBean.java
@@ -3,6 +3,9 @@ package com.lmw.learn.bean.life.cycle;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Bean生命周期演示
@@ -71,5 +74,25 @@ public class LifeCycleBean implements BeanNameAware, BeanFactoryAware, BeanClass
 
 	public void destroyMethdo(){
 		System.out.println("destroy-method 被调用...");
+	}
+
+	public void display(){
+		System.out.println("方法调用...");
+	}
+
+	public static void main(String[] args) {
+		ClassPathResource resource = new ClassPathResource("life/cycle/lifeCycleBean.xml");
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions(resource);
+		// BeanFactory 容器一定要调用该方法进行 BeanPostProcessor 注册
+		factory.addBeanPostProcessor(new LifeCycleBean());
+
+		LifeCycleBean lifeCycleBean = (LifeCycleBean) factory.getBean("lifeCycle");
+		lifeCycleBean.display();
+
+		System.out.println("方法调用完成，容器开始关闭....");
+		// 关闭容器
+		factory.destroySingletons();
 	}
 }
