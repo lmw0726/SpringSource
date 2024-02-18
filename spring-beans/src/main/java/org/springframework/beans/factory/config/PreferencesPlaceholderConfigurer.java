6aa5d0ec1771c6f316679bacc1707ebab4c22c1e
LMW <CNlmw0726@gmail.com>
2024-02-18 22:49:54 +0800

    docs: 添加PropertiesLoaderSupport所有子类的注释

--- spring-beans/src/main/java/org/springframework/beans/factory/config/PreferencesPlaceholderConfigurer.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/PreferencesPlaceholderConfigurer.java
@@ -16,59 +16,63 @@
 
 package org.springframework.beans.factory.config;
 
-import java.util.Properties;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
-
 import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.lang.Nullable;
 
+import java.util.Properties;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
 /**
- * Subclass of PropertyPlaceholderConfigurer that supports JDK 1.4's
- * Preferences API ({@code java.util.prefs}).
+ * 支持 JDK 1.4 的首选项 API（{@code java.util.prefs}）的 PropertyPlaceholderConfigurer 的子类。
  *
- * <p>Tries to resolve placeholders as keys first in the user preferences,
- * then in the system preferences, then in this configurer's properties.
- * Thus, behaves like PropertyPlaceholderConfigurer if no corresponding
- * preferences defined.
+ * <p>首先尝试在用户首选项中解析占位符作为键，然后在系统首选项中解析，最后在此配置器的属性中解析。因此，如果没有对应的首选项定义，则行为类似于 PropertyPlaceholderConfigurer。
  *
- * <p>Supports custom paths for the system and user preferences trees. Also
- * supports custom paths specified in placeholders ("myPath/myPlaceholderKey").
- * Uses the respective root node if not specified.
+ * <p>支持自定义系统和用户首选项树的路径。还支持在占位符中指定的自定义路径（“myPath/myPlaceholderKey”）。如果未指定，则使用相应的根节点。
  *
  * @author Juergen Hoeller
- * @since 16.02.2004
  * @see #setSystemTreePath
  * @see #setUserTreePath
  * @see java.util.prefs.Preferences
- * @deprecated as of 5.2, along with {@link PropertyPlaceholderConfigurer}
+ * @since 16.02.2004
+ * @deprecated 自 5.2 起，与 {@link PropertyPlaceholderConfigurer} 一起不推荐使用
  */
 @Deprecated
 public class PreferencesPlaceholderConfigurer extends PropertyPlaceholderConfigurer implements InitializingBean {
 
+	/**
+	 * 系统首选项树的路径。
+	 */
 	@Nullable
 	private String systemTreePath;
 
+	/**
+	 * 用户首选项树的路径。
+	 */
 	@Nullable
 	private String userTreePath;
 
+	/**
+	 * 系统首选项。
+	 */
 	private Preferences systemPrefs = Preferences.systemRoot();
 
+	/**
+	 * 用户首选项。
+	 */
 	private Preferences userPrefs = Preferences.userRoot();
 
 
 	/**
-	 * Set the path in the system preferences tree to use for resolving
-	 * placeholders. Default is the root node.
+	 * 设置在系统首选项树中用于解析占位符的路径。默认为根节点。
 	 */
 	public void setSystemTreePath(String systemTreePath) {
 		this.systemTreePath = systemTreePath;
 	}
 
 	/**
-	 * Set the path in the system preferences tree to use for resolving
-	 * placeholders. Default is the root node.
+	 * 设置在用户首选项树中用于解析占位符的路径。默认为根节点。
 	 */
 	public void setUserTreePath(String userTreePath) {
 		this.userTreePath = userTreePath;
@@ -76,36 +80,43 @@ public class PreferencesPlaceholderConfigurer extends PropertyPlaceholderConfigu
 
 
 	/**
-	 * This implementation eagerly fetches the Preferences instances
-	 * for the required system and user tree nodes.
+	 * 此实现会提前获取所需的系统和用户树节点的首选项实例。
 	 */
 	@Override
 	public void afterPropertiesSet() {
+		// 如果指定了系统树路径，则使用系统偏好节点创建指定路径的节点。
 		if (this.systemTreePath != null) {
 			this.systemPrefs = this.systemPrefs.node(this.systemTreePath);
 		}
+		// 如果指定了用户树路径，则使用用户偏好节点创建指定路径的节点。
 		if (this.userTreePath != null) {
 			this.userPrefs = this.userPrefs.node(this.userTreePath);
 		}
 	}
 
 	/**
-	 * This implementation tries to resolve placeholders as keys first
-	 * in the user preferences, then in the system preferences, then in
-	 * the passed-in properties.
+	 * 此实现会首先尝试在用户首选项中以占位符作为键解析，然后在系统首选项中，最后在传入的属性中解析。
 	 */
 	@Override
 	protected String resolvePlaceholder(String placeholder, Properties props) {
+		// 初始化路径和键变量
 		String path = null;
 		String key = placeholder;
+		// 获取路径的末尾索引
 		int endOfPath = placeholder.lastIndexOf('/');
+		// 如果路径存在
 		if (endOfPath != -1) {
+			// 获取路径
 			path = placeholder.substring(0, endOfPath);
+			// 获取键
 			key = placeholder.substring(endOfPath + 1);
 		}
+		// 从用户偏好节点解析占位符
 		String value = resolvePlaceholder(path, key, this.userPrefs);
+		// 如果用户偏好节点中未找到值，则从系统偏好节点解析
 		if (value == null) {
 			value = resolvePlaceholder(path, key, this.systemPrefs);
+			// 如果系统偏好节点中未找到值，则从属性文件中查找
 			if (value == null) {
 				value = props.getProperty(placeholder);
 			}
@@ -114,29 +125,27 @@ public class PreferencesPlaceholderConfigurer extends PropertyPlaceholderConfigu
 	}
 
 	/**
-	 * Resolve the given path and key against the given Preferences.
-	 * @param path the preferences path (placeholder part before '/')
-	 * @param key the preferences key (placeholder part after '/')
-	 * @param preferences the Preferences to resolve against
-	 * @return the value for the placeholder, or {@code null} if none found
+	 * 解析给定路径和键相对于给定的首选项。
+	 *
+	 * @param path        首选项路径（斜杠 '/' 前面的占位符部分）
+	 * @param key         首选项键（斜杠 '/' 后面的占位符部分）
+	 * @param preferences 要解析的首选项
+	 * @return 占位符的值，如果找不到则返回 {@code null}
 	 */
 	@Nullable
 	protected String resolvePlaceholder(@Nullable String path, String key, Preferences preferences) {
 		if (path != null) {
-			// Do not create the node if it does not exist...
+			// 如果节点不存在，则不要创建该节点...
 			try {
 				if (preferences.nodeExists(path)) {
 					return preferences.node(path).get(key, null);
-				}
-				else {
+				} else {
 					return null;
 				}
-			}
-			catch (BackingStoreException ex) {
+			} catch (BackingStoreException ex) {
 				throw new BeanDefinitionStoreException("Cannot access specified node path [" + path + "]", ex);
 			}
-		}
-		else {
+		} else {
 			return preferences.get(key, null);
 		}
 	}
--- spring-beans/src/main/java/org/springframework/beans/factory/config/PropertyPlaceholderConfigurer.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/PropertyPlaceholderConfigurer.java
@@ -16,8 +16,6 @@
 
 package org.springframework.beans.factory.config;
 
-import java.util.Properties;
-
 import org.springframework.beans.BeansException;
 import org.springframework.core.Constants;
 import org.springframework.core.SpringProperties;
@@ -27,63 +25,65 @@ import org.springframework.util.PropertyPlaceholderHelper;
 import org.springframework.util.PropertyPlaceholderHelper.PlaceholderResolver;
 import org.springframework.util.StringValueResolver;
 
+import java.util.Properties;
+
 /**
- * {@link PlaceholderConfigurerSupport} subclass that resolves ${...} placeholders against
- * {@link #setLocation local} {@link #setProperties properties} and/or system properties
- * and environment variables.
+ * {@link PlaceholderConfigurerSupport} 的子类，用于解析 ${...} 占位符针对 {@link #setLocation 本地} {@link #setProperties 属性} 和/或系统属性以及环境变量。
  *
- * <p>{@link PropertyPlaceholderConfigurer} is still appropriate for use when:
+ * <p>当以下情况仍然适用于使用 {@code PropertyPlaceholderConfigurer}：
  * <ul>
- * <li>the {@code spring-context} module is not available (i.e., one is using Spring's
- * {@code BeanFactory} API as opposed to {@code ApplicationContext}).
- * <li>existing configuration makes use of the {@link #setSystemPropertiesMode(int) "systemPropertiesMode"}
- * and/or {@link #setSystemPropertiesModeName(String) "systemPropertiesModeName"} properties.
- * Users are encouraged to move away from using these settings, and rather configure property
- * source search order through the container's {@code Environment}; however, exact preservation
- * of functionality may be maintained by continuing to use {@code PropertyPlaceholderConfigurer}.
+ * <li> {@code spring-context} 模块不可用（即，使用 Spring 的 {@code BeanFactory} API 而不是 {@code ApplicationContext}）。
+ * <li>现有配置使用 {@link #setSystemPropertiesMode(int) "systemPropertiesMode"} 和/或 {@link #setSystemPropertiesModeName(String) "systemPropertiesModeName"} 属性。鼓励用户摆脱使用这些设置，而是通过容器的 {@code Environment} 配置属性源搜索顺序；然而，通过继续使用 {@code PropertyPlaceholderConfigurer} 可以保持确切的功能保留。
  * </ul>
  *
  * @author Juergen Hoeller
  * @author Chris Beams
- * @since 02.10.2003
  * @see #setSystemPropertiesModeName
  * @see PlaceholderConfigurerSupport
  * @see PropertyOverrideConfigurer
- * @deprecated as of 5.2; use {@code org.springframework.context.support.PropertySourcesPlaceholderConfigurer}
- * instead which is more flexible through taking advantage of the {@link org.springframework.core.env.Environment}
- * and {@link org.springframework.core.env.PropertySource} mechanisms.
+ * @since 02.10.2003
+ * @deprecated 从 5.2 开始；使用 {@code org.springframework.context.support.PropertySourcesPlaceholderConfigurer} 代替，它更加灵活，通过利用 {@link org.springframework.core.env.Environment} 和 {@link org.springframework.core.env.PropertySource} 机制。
  */
 @Deprecated
 public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport {
 
-	/** Never check system properties. */
+	/**
+	 * 从不检查系统属性。
+	 */
 	public static final int SYSTEM_PROPERTIES_MODE_NEVER = 0;
 
 	/**
-	 * Check system properties if not resolvable in the specified properties.
-	 * This is the default.
+	 * 如果在指定的属性中无法解析，则检查系统属性。这是默认值。
 	 */
 	public static final int SYSTEM_PROPERTIES_MODE_FALLBACK = 1;
 
 	/**
-	 * Check system properties first, before trying the specified properties.
-	 * This allows system properties to override any other property source.
+	 * 首先检查系统属性，然后再尝试指定的属性。这允许系统属性覆盖任何其他属性源。
 	 */
 	public static final int SYSTEM_PROPERTIES_MODE_OVERRIDE = 2;
 
-
+	/**
+	 * 常量
+	 */
 	private static final Constants constants = new Constants(PropertyPlaceholderConfigurer.class);
 
+	/**
+	 * 系统属性模式，默认为回退模式
+	 */
 	private int systemPropertiesMode = SYSTEM_PROPERTIES_MODE_FALLBACK;
 
+	/**
+	 * 是否搜索系统环境
+	 */
 	private boolean searchSystemEnvironment =
 			!SpringProperties.getFlag(AbstractEnvironment.IGNORE_GETENV_PROPERTY_NAME);
 
 
 	/**
-	 * Set the system property mode by the name of the corresponding constant,
-	 * e.g. "SYSTEM_PROPERTIES_MODE_OVERRIDE".
-	 * @param constantName name of the constant
+	 * 根据相应常量的名称设置系统属性模式，例如"SYSTEM_PROPERTIES_MODE_OVERRIDE"。
+	 *
+	 * @param constantName 常量的名称
+	 * @throws IllegalArgumentException 如果常量名无效
 	 * @see #setSystemPropertiesMode
 	 */
 	public void setSystemPropertiesModeName(String constantName) throws IllegalArgumentException {
@@ -91,12 +91,12 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 	}
 
 	/**
-	 * Set how to check system properties: as fallback, as override, or never.
-	 * For example, will resolve ${user.dir} to the "user.dir" system property.
-	 * <p>The default is "fallback": If not being able to resolve a placeholder
-	 * with the specified properties, a system property will be tried.
-	 * "override" will check for a system property first, before trying the
-	 * specified properties. "never" will not check system properties at all.
+	 * 设置如何检查系统属性：作为后备、作为覆盖，或者从不检查。
+	 * 例如，将会将 ${user.dir} 解析为 "user.dir" 系统属性。
+	 * <p>默认是 "fallback"：如果无法使用指定的属性解析占位符，则尝试使用系统属性。
+	 * "override" 将首先检查系统属性，然后再尝试指定的属性。"never" 将完全不检查系统属性。
+	 *
+	 * @param systemPropertiesMode 检查系统属性的模式
 	 * @see #SYSTEM_PROPERTIES_MODE_NEVER
 	 * @see #SYSTEM_PROPERTIES_MODE_FALLBACK
 	 * @see #SYSTEM_PROPERTIES_MODE_OVERRIDE
@@ -107,14 +107,12 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 	}
 
 	/**
-	 * Set whether to search for a matching system environment variable
-	 * if no matching system property has been found. Only applied when
-	 * "systemPropertyMode" is active (i.e. "fallback" or "override"), right
-	 * after checking JVM system properties.
-	 * <p>Default is "true". Switch this setting off to never resolve placeholders
-	 * against system environment variables. Note that it is generally recommended
-	 * to pass external values in as JVM system properties: This can easily be
-	 * achieved in a startup script, even for existing environment variables.
+	 * 设置是否在未找到匹配的系统属性时搜索匹配的系统环境变量。
+	 * 仅当 "systemPropertyMode" 激活时（即 "fallback" 或 "override"）应用，紧接在检查 JVM 系统属性之后。
+	 * <p>默认值为 "true"。关闭此设置以永远不会解析占位符为系统环境变量。
+	 * 请注意，通常建议将外部值传递为 JVM 系统属性：这可以很容易地在启动脚本中实现，即使是对于现有环境变量。
+	 *
+	 * @param searchSystemEnvironment 是否搜索系统环境变量
 	 * @see #setSystemPropertiesMode
 	 * @see System#getProperty(String)
 	 * @see System#getenv(String)
@@ -124,47 +122,47 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 	}
 
 	/**
-	 * Resolve the given placeholder using the given properties, performing
-	 * a system properties check according to the given mode.
-	 * <p>The default implementation delegates to {@code resolvePlaceholder
-	 * (placeholder, props)} before/after the system properties check.
-	 * <p>Subclasses can override this for custom resolution strategies,
-	 * including customized points for the system properties check.
-	 * @param placeholder the placeholder to resolve
-	 * @param props the merged properties of this configurer
-	 * @param systemPropertiesMode the system properties mode,
-	 * according to the constants in this class
-	 * @return the resolved value, of null if none
+	 * 使用给定的属性解析给定的占位符，并根据给定的模式执行系统属性检查。
+	 * <p>默认实现在系统属性检查之前/之后委托给 {@code resolvePlaceholder(placeholder, props)}。
+	 * <p>子类可以重写此方法以实现自定义的解析策略，包括自定义的系统属性检查点。
+	 *
+	 * @param placeholder          要解析的占位符
+	 * @param props                此配置器的合并属性
+	 * @param systemPropertiesMode 系统属性模式，根据此类中的常量
+	 * @return 解析后的值，如果没有则为 null
 	 * @see #setSystemPropertiesMode
 	 * @see System#getProperty
 	 * @see #resolvePlaceholder(String, java.util.Properties)
 	 */
 	@Nullable
 	protected String resolvePlaceholder(String placeholder, Properties props, int systemPropertiesMode) {
+		// 初始化属性值为空。
 		String propVal = null;
+		// 如果系统属性模式为 SYSTEM_PROPERTIES_MODE_OVERRIDE，则尝试从系统属性中解析占位符。
 		if (systemPropertiesMode == SYSTEM_PROPERTIES_MODE_OVERRIDE) {
 			propVal = resolveSystemProperty(placeholder);
 		}
+		// 如果属性值仍为空，则尝试从属性文件中解析占位符。
 		if (propVal == null) {
 			propVal = resolvePlaceholder(placeholder, props);
 		}
+		// 如果属性值仍为空且系统属性模式为 SYSTEM_PROPERTIES_MODE_FALLBACK，则再次尝试从系统属性中解析占位符。
 		if (propVal == null && systemPropertiesMode == SYSTEM_PROPERTIES_MODE_FALLBACK) {
 			propVal = resolveSystemProperty(placeholder);
 		}
+		// 返回解析后的属性值。
 		return propVal;
 	}
 
 	/**
-	 * Resolve the given placeholder using the given properties.
-	 * The default implementation simply checks for a corresponding property key.
-	 * <p>Subclasses can override this for customized placeholder-to-key mappings
-	 * or custom resolution strategies, possibly just using the given properties
-	 * as fallback.
-	 * <p>Note that system properties will still be checked before respectively
-	 * after this method is invoked, according to the system properties mode.
-	 * @param placeholder the placeholder to resolve
-	 * @param props the merged properties of this configurer
-	 * @return the resolved value, of {@code null} if none
+	 * 使用给定的属性解析给定的占位符。
+	 * 默认实现只检查相应的属性键。
+	 * <p>子类可以重写此方法以进行自定义的占位符到键的映射或自定义的解析策略，可能只使用给定的属性作为回退。
+	 * <p>请注意，根据系统属性模式，在调用此方法之前或之后仍将检查系统属性。
+	 *
+	 * @param placeholder 要解析的占位符
+	 * @param props       此配置器的合并属性
+	 * @return 解析后的值，如果没有则为 {@code null}
 	 * @see #setSystemPropertiesMode
 	 */
 	@Nullable
@@ -173,10 +171,10 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 	}
 
 	/**
-	 * Resolve the given key as JVM system property, and optionally also as
-	 * system environment variable if no matching system property has been found.
-	 * @param key the placeholder to resolve as system property key
-	 * @return the system property value, or {@code null} if not found
+	 * 将给定的键解析为 JVM 系统属性，并在没有找到匹配的系统属性时可选择作为系统环境变量。
+	 *
+	 * @param key 要解析为系统属性键的占位符
+	 * @return 系统属性值，如果未找到则为 {@code null}
 	 * @see #setSearchSystemEnvironment
 	 * @see System#getProperty(String)
 	 * @see System#getenv(String)
@@ -189,8 +187,7 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 				value = System.getenv(key);
 			}
 			return value;
-		}
-		catch (Throwable ex) {
+		} catch (Throwable ex) {
 			if (logger.isDebugEnabled()) {
 				logger.debug("Could not access system property '" + key + "': " + ex);
 			}
@@ -200,22 +197,33 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 
 
 	/**
-	 * Visit each bean definition in the given bean factory and attempt to replace ${...} property
-	 * placeholders with values from the given properties.
+	 * 访问给定的 Bean 工厂中的每个 Bean 定义，并尝试使用给定属性的值替换 ${...} 属性占位符。
+	 *
+	 * @param beanFactoryToProcess 要处理的 Bean 工厂
+	 * @param props                包含属性值的属性集合
+	 * @throws BeansException 如果在替换过程中发生异常
 	 */
 	@Override
 	protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
 			throws BeansException {
 
+		// 创建一个 PlaceholderResolvingStringValueResolver 实例，用于解析占位符。
 		StringValueResolver valueResolver = new PlaceholderResolvingStringValueResolver(props);
+		// 使用创建的 valueResolver 对象处理属性值。
 		doProcessProperties(beanFactoryToProcess, valueResolver);
 	}
 
 
 	private class PlaceholderResolvingStringValueResolver implements StringValueResolver {
 
+		/**
+		 * 属性占位符辅助器
+		 */
 		private final PropertyPlaceholderHelper helper;
 
+		/**
+		 * 占位符解析器
+		 */
 		private final PlaceholderResolver resolver;
 
 		public PlaceholderResolvingStringValueResolver(Properties props) {
@@ -227,10 +235,13 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 		@Override
 		@Nullable
 		public String resolveStringValue(String strVal) throws BeansException {
+			// 使用 PropertyPlaceholderHelper 实例替换属性占位符。
 			String resolved = this.helper.replacePlaceholders(strVal, this.resolver);
+			// 如果设置了 trimValues 标志，则去除解析后的值两端的空白字符。
 			if (trimValues) {
 				resolved = resolved.trim();
 			}
+			// 如果解析后的值等于 nullValue，则返回 null，否则返回解析后的值。
 			return (resolved.equals(nullValue) ? null : resolved);
 		}
 	}
@@ -238,12 +249,21 @@ public class PropertyPlaceholderConfigurer extends PlaceholderConfigurerSupport 
 
 	private final class PropertyPlaceholderConfigurerResolver implements PlaceholderResolver {
 
+		/**
+		 * 属性值
+		 */
 		private final Properties props;
 
 		private PropertyPlaceholderConfigurerResolver(Properties props) {
 			this.props = props;
 		}
 
+		/**
+		 * 解析占位符
+		 *
+		 * @param placeholderName 要解析的占位符的名称
+		 * @return 解析后的值
+		 */
 		@Override
 		@Nullable
 		public String resolvePlaceholder(String placeholderName) {
