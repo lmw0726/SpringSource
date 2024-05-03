bd5126df1aa22612d68f3bfe11f7b1f0491c642c
LMW <CNlmw0726@gmail.com>
2024-05-03 23:09:45 +0800

    docs: 添加 JSTL工具类 的代码注释

--- spring-webmvc/src/main/java/org/springframework/web/servlet/support/JstlUtils.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/support/JstlUtils.java
@@ -16,24 +16,22 @@
 
 package org.springframework.web.servlet.support;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.TimeZone;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceResourceBundle;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.lang.Nullable;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.jstl.core.Config;
 import javax.servlet.jsp.jstl.fmt.LocalizationContext;
-
-import org.springframework.context.MessageSource;
-import org.springframework.context.support.MessageSourceResourceBundle;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.lang.Nullable;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.TimeZone;
 
 /**
- * Helper class for preparing JSTL views,
- * in particular for exposing a JSTL localization context.
+ * 辅助类，用于准备 JSTL 视图，特别是用于暴露 JSTL 本地化上下文。
  *
  * @author Juergen Hoeller
  * @since 20.08.2003
@@ -41,85 +39,109 @@ import org.springframework.lang.Nullable;
 public abstract class JstlUtils {
 
 	/**
-	 * Checks JSTL's "javax.servlet.jsp.jstl.fmt.localizationContext"
-	 * context-param and creates a corresponding child message source,
-	 * with the provided Spring-defined MessageSource as parent.
-	 * @param servletContext the ServletContext we're running in
-	 * (to check JSTL-related context-params in {@code web.xml})
-	 * @param messageSource the MessageSource to expose, typically
-	 * the ApplicationContext of the current DispatcherServlet
-	 * @return the MessageSource to expose to JSTL; first checking the
-	 * JSTL-defined bundle, then the Spring-defined MessageSource
+	 * 检查 JSTL 的 "javax.servlet.jsp.jstl.fmt.localizationContext" 上下文参数，并创建相应的子消息源，
+	 * 并使用提供的 Spring 定义的 MessageSource 作为父级。
+	 *
+	 * @param servletContext 我们正在运行的 ServletContext
+	 *                       （用于检查 {@code web.xml} 中的 JSTL 相关上下文参数）
+	 * @param messageSource  要暴露的 MessageSource，通常是当前 DispatcherServlet 的 ApplicationContext
+	 * @return 要暴露给 JSTL 的 MessageSource；首先检查 JSTL 定义的 bundle，然后是 Spring 定义的 MessageSource
 	 * @see org.springframework.context.ApplicationContext
 	 */
 	public static MessageSource getJstlAwareMessageSource(
 			@Nullable ServletContext servletContext, MessageSource messageSource) {
 
+		// 如果存在 Servlet上下文
 		if (servletContext != null) {
+			// 获取web.xml中配置的JSTL上下文参数
 			String jstlInitParam = servletContext.getInitParameter(Config.FMT_LOCALIZATION_CONTEXT);
+			// 如果获取到了JSTL上下文参数
 			if (jstlInitParam != null) {
-				// Create a ResourceBundleMessageSource for the specified resource bundle
-				// basename in the JSTL context-param in web.xml, wiring it with the given
-				// Spring-defined MessageSource as parent.
+				// 创建一个ResourceBundleMessageSource，用于从JSTL资源文件加载消息，并将其设置为messageSource的父级
 				ResourceBundleMessageSource jstlBundleWrapper = new ResourceBundleMessageSource();
 				jstlBundleWrapper.setBasename(jstlInitParam);
 				jstlBundleWrapper.setParentMessageSource(messageSource);
+				// 返回JSTL包装后的MessageSource
 				return jstlBundleWrapper;
 			}
 		}
+
+		// 如果没有JSTL上下文参数，则直接返回messageSource
 		return messageSource;
 	}
 
 	/**
-	 * Exposes JSTL-specific request attributes specifying locale
-	 * and resource bundle for JSTL's formatting and message tags,
-	 * using Spring's locale and MessageSource.
-	 * @param request the current HTTP request
-	 * @param messageSource the MessageSource to expose,
-	 * typically the current ApplicationContext (may be {@code null})
+	 * 暴露 JSTL 特定的请求属性，指定 JSTL 的格式化和消息标签的区域设置和资源包，
+	 * 使用 Spring 的区域设置和 MessageSource。
+	 *
+	 * @param request       当前 HTTP 请求
+	 * @param messageSource 要暴露的 MessageSource，通常是当前 ApplicationContext（可以为 {@code null}）
 	 * @see #exposeLocalizationContext(RequestContext)
 	 */
 	public static void exposeLocalizationContext(HttpServletRequest request, @Nullable MessageSource messageSource) {
+		// 获取JSTL的 区域设置
 		Locale jstlLocale = RequestContextUtils.getLocale(request);
+		// 设置到请求属性中
 		Config.set(request, Config.FMT_LOCALE, jstlLocale);
+
+		// 获取JSTL的 时区
 		TimeZone timeZone = RequestContextUtils.getTimeZone(request);
 		if (timeZone != null) {
+			// 时区不为空，将其设置到请求属性中
 			Config.set(request, Config.FMT_TIME_ZONE, timeZone);
 		}
+
 		if (messageSource != null) {
+			// 如果存在消息源
+			// 创建SpringLocalizationContext对象
 			LocalizationContext jstlContext = new SpringLocalizationContext(messageSource, request);
+			// 并将其设置到请求属性中
 			Config.set(request, Config.FMT_LOCALIZATION_CONTEXT, jstlContext);
 		}
 	}
 
 	/**
-	 * Exposes JSTL-specific request attributes specifying locale
-	 * and resource bundle for JSTL's formatting and message tags,
-	 * using Spring's locale and MessageSource.
-	 * @param requestContext the context for the current HTTP request,
-	 * including the ApplicationContext to expose as MessageSource
+	 * 暴露 JSTL 特定的请求属性，指定 JSTL 的格式化和消息标签的区域设置和资源包，
+	 * 使用 Spring 的区域设置和 MessageSource。
+	 *
+	 * @param requestContext 当前 HTTP 请求的上下文，包括要暴露为 MessageSource 的 ApplicationContext
 	 */
 	public static void exposeLocalizationContext(RequestContext requestContext) {
+		// 将获取到的 区域设置 到请求属性中
 		Config.set(requestContext.getRequest(), Config.FMT_LOCALE, requestContext.getLocale());
+
+		// 获取请求上下文的逝去
 		TimeZone timeZone = requestContext.getTimeZone();
 		if (timeZone != null) {
+			// 如果时区不为空，则设置到请求属性中
 			Config.set(requestContext.getRequest(), Config.FMT_TIME_ZONE, timeZone);
 		}
+
+		// 获取JSTL感知的消息源
 		MessageSource messageSource = getJstlAwareMessageSource(
 				requestContext.getServletContext(), requestContext.getMessageSource());
+
+		// 创建SpringLocalizationContext对象
 		LocalizationContext jstlContext = new SpringLocalizationContext(messageSource, requestContext.getRequest());
+		// 并将其设置到请求属性中
 		Config.set(requestContext.getRequest(), Config.FMT_LOCALIZATION_CONTEXT, jstlContext);
 	}
 
 
 	/**
-	 * Spring-specific LocalizationContext adapter that merges session-scoped
-	 * JSTL LocalizationContext/Locale attributes with the local Spring request context.
+	 * Spring 特定的 LocalizationContext 适配器，将会话范围的 JSTL LocalizationContext/Locale
+	 * 属性与本地 Spring 请求上下文合并。
 	 */
 	private static class SpringLocalizationContext extends LocalizationContext {
 
+		/**
+		 * 消息源
+		 */
 		private final MessageSource messageSource;
 
+		/**
+		 * Http请求
+		 */
 		private final HttpServletRequest request;
 
 		public SpringLocalizationContext(MessageSource messageSource, HttpServletRequest request) {
@@ -129,26 +151,38 @@ public abstract class JstlUtils {
 
 		@Override
 		public ResourceBundle getResourceBundle() {
+			// 获取请求中的Http会话
 			HttpSession session = this.request.getSession(false);
+
 			if (session != null) {
+				// 如果会话存在，根据JSTL配置和Http会话获取本地化上下文对象
 				Object lcObject = Config.get(session, Config.FMT_LOCALIZATION_CONTEXT);
 				if (lcObject instanceof LocalizationContext) {
+					// 如果 本地化上下文对象 是LocalizationContext类型
+					// 获取绑定资源
 					ResourceBundle lcBundle = ((LocalizationContext) lcObject).getResourceBundle();
+					// 返回使用 消息源、区域设置、绑定资源 构建的MessageSourceResourceBundle对象
 					return new MessageSourceResourceBundle(this.messageSource, getLocale(), lcBundle);
 				}
 			}
+
+			// 返回使用 消息源、区域设置 构建的MessageSourceResourceBundle对象
 			return new MessageSourceResourceBundle(this.messageSource, getLocale());
 		}
 
 		@Override
 		public Locale getLocale() {
+			// 获取Http会话
 			HttpSession session = this.request.getSession(false);
 			if (session != null) {
+				// 如果会话存在，根据JSTL配置和Http会话获取本地化对象
 				Object localeObject = Config.get(session, Config.FMT_LOCALE);
 				if (localeObject instanceof Locale) {
+					// 如果本地化对象是Locale实例，则返回该对象
 					return (Locale) localeObject;
 				}
 			}
+			// 使用Http请求获取区域设置
 			return RequestContextUtils.getLocale(this.request);
 		}
 	}
