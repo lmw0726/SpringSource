ca52113337f65ad94ce3db78dd083a65a5c37a17
LMW <CNlmw0726@gmail.com>
2024-05-13 00:34:57 +0800

    docs: 完成 StreamingResponseBody 的代码注释

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/StreamingResponseBody.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/StreamingResponseBody.java
@@ -21,17 +21,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * A controller method return value type for asynchronous request processing
- * where the application can write directly to the response {@code OutputStream}
- * without holding up the Servlet container thread.
+ * 用于异步请求处理的控制器方法返回值类型，在应用程序可以直接写入响应 {@code OutputStream} 而不阻塞 Servlet 容器线程时使用。
  *
- * <p><strong>Note:</strong> when using this option it is highly recommended to
- * configure explicitly the TaskExecutor used in Spring MVC for executing
- * asynchronous requests. Both the MVC Java config and the MVC namespaces provide
- * options to configure asynchronous handling. If not using those, an application
- * can set the {@code taskExecutor} property of
- * {@link org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
- * RequestMappingHandlerAdapter}.
+ * <p><strong>注意：</strong> 当使用此选项时，强烈建议显式配置 Spring MVC 中用于执行异步请求的 TaskExecutor。
+ * MVC Java 配置和 MVC 命名空间都提供了配置异步处理的选项。
+ * 如果不使用这些选项，应用程序可以设置 {@link org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter
+ * RequestMappingHandlerAdapter} 的 {@code taskExecutor} 属性。
  *
  * @author Rossen Stoyanchev
  * @since 4.2
@@ -40,9 +35,10 @@ import java.io.OutputStream;
 public interface StreamingResponseBody {
 
 	/**
-	 * A callback for writing to the response body.
-	 * @param outputStream the stream for the response body
-	 * @throws IOException an exception while writing
+	 * 用于写入响应主体的回调。
+	 *
+	 * @param outputStream 响应主体的流
+	 * @throws IOException 写入时发生的异常
 	 */
 	void writeTo(OutputStream outputStream) throws IOException;
 
