6036441a8354b76a81e05c2263aab199a75467b3
LMW <CNlmw0726@gmail.com>
2024-05-17 23:19:18 +0800

    docs: 添加 RequestPredicate 的代码注释

--- spring-webmvc/src/main/java/org/springframework/web/servlet/function/RequestPredicate.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/function/RequestPredicate.java
@@ -19,65 +19,65 @@ package org.springframework.web.servlet.function;
 import java.util.Optional;
 
 /**
- * Represents a function that evaluates on a given {@link ServerRequest}.
- * Instances of this function that evaluate on common request properties
- * can be found in {@link RequestPredicates}.
+ * 表示在给定的{@link ServerRequest}上进行评估的函数。
+ * 在{@link RequestPredicates}中可以找到在常见请求属性上进行评估的此函数的实例。
  *
  * @author Arjen Poutsma
- * @since 5.2
  * @see RequestPredicates
  * @see RouterFunctions#route(RequestPredicate, HandlerFunction)
  * @see RouterFunctions#nest(RequestPredicate, RouterFunction)
+ * @since 5.2
  */
 @FunctionalInterface
 public interface RequestPredicate {
 
 	/**
-	 * Evaluate this predicate on the given request.
-	 * @param request the request to match against
-	 * @return {@code true} if the request matches the predicate; {@code false} otherwise
+	 * 在给定的请求上评估此谓词。
+	 *
+	 * @param request 要匹配的请求
+	 * @return 如果请求与谓词匹配，则为{@code true}；否则为{@code false}
 	 */
 	boolean test(ServerRequest request);
 
 	/**
-	 * Return a composed request predicate that tests against both this predicate AND
-	 * the {@code other} predicate. When evaluating the composed predicate, if this
-	 * predicate is {@code false}, then the {@code other} predicate is not evaluated.
-	 * @param other a predicate that will be logically-ANDed with this predicate
-	 * @return a predicate composed of this predicate AND the {@code other} predicate
+	 * 返回一个组合的请求谓词，它对此谓词和{@code other}谓词进行测试。
+	 * 在评估组合谓词时，如果此谓词为{@code false}，则不会评估{@code other}谓词。
+	 *
+	 * @param other 将与此谓词逻辑AND的谓词
+	 * @return 由此谓词和{@code other}谓词组成的谓词
 	 */
 	default RequestPredicate and(RequestPredicate other) {
 		return new RequestPredicates.AndRequestPredicate(this, other);
 	}
 
 	/**
-	 * Return a predicate that represents the logical negation of this predicate.
-	 * @return a predicate that represents the logical negation of this predicate
+	 * 返回表示此谓词的逻辑否定的谓词。
+	 *
+	 * @return 表示此谓词的逻辑否定的谓词
 	 */
 	default RequestPredicate negate() {
 		return new RequestPredicates.NegateRequestPredicate(this);
 	}
 
 	/**
-	 * Return a composed request predicate that tests against both this predicate OR
-	 * the {@code other} predicate. When evaluating the composed predicate, if this
-	 * predicate is {@code true}, then the {@code other} predicate is not evaluated.
-	 * @param other a predicate that will be logically-ORed with this predicate
-	 * @return a predicate composed of this predicate OR the {@code other} predicate
+	 * 返回一个组合的请求谓词，它对此谓词和{@code other}谓词进行测试。
+	 * 在评估组合谓词时，如果此谓词为{@code true}，则不会评估{@code other}谓词。
+	 *
+	 * @param other 将与此谓词逻辑OR的谓词
+	 * @return 由此谓词和{@code other}谓词组成的谓词
 	 */
 	default RequestPredicate or(RequestPredicate other) {
 		return new RequestPredicates.OrRequestPredicate(this, other);
 	}
 
 	/**
-	 * Transform the given request into a request used for a nested route. For instance,
-	 * a path-based predicate can return a {@code ServerRequest} with a path remaining
-	 * after a match.
-	 * <p>The default implementation returns an {@code Optional} wrapping the given request if
-	 * {@link #test(ServerRequest)} evaluates to {@code true}; or {@link Optional#empty()}
-	 * if it evaluates to {@code false}.
-	 * @param request the request to be nested
-	 * @return the nested request
+	 * 将给定的请求转换为用于嵌套路由的请求。例如，
+	 * 基于路径的谓词可以返回一个匹配后剩余路径的{@code ServerRequest}。
+	 * <p>默认实现如果{@link #test(ServerRequest)}评估为{@code true}，则返回一个包装了给定请求的{@code Optional}；
+	 * 如果评估为{@code false}，则返回{@link Optional#empty()}。
+	 *
+	 * @param request 要嵌套的请求
+	 * @return 嵌套的请求
 	 * @see RouterFunctions#nest(RequestPredicate, RouterFunction)
 	 */
 	default Optional<ServerRequest> nest(ServerRequest request) {
@@ -85,11 +85,10 @@ public interface RequestPredicate {
 	}
 
 	/**
-	 * Accept the given visitor. Default implementation calls
-	 * {@link RequestPredicates.Visitor#unknown(RequestPredicate)}; composed {@code RequestPredicate}
-	 * implementations are expected to call {@code accept} for all components that make up this
-	 * request predicate.
-	 * @param visitor the visitor to accept
+	 * 接受给定的访问者。默认实现调用{@link RequestPredicates.Visitor#unknown(RequestPredicate)}；
+	 * 组合的{@code RequestPredicate}实现应该对构成此请求谓词的所有组件调用{@code accept}。
+	 *
+	 * @param visitor 要接受的访问者
 	 */
 	default void accept(RequestPredicates.Visitor visitor) {
 		visitor.unknown(this);
