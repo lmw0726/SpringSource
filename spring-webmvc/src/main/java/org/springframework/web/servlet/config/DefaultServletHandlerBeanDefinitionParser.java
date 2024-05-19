43979a510f01aae3a29c680e30d73d938e509356
LMW <CNlmw0726@gmail.com>
2024-05-19 23:05:28 +0800

    docs: 添加 Bean定义解析器 的代码注释

--- spring-webmvc/src/main/java/org/springframework/web/servlet/config/DefaultServletHandlerBeanDefinitionParser.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/config/DefaultServletHandlerBeanDefinitionParser.java
@@ -16,10 +16,6 @@
 
 package org.springframework.web.servlet.config;
 
-import java.util.Map;
-
-import org.w3c.dom.Element;
-
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.parsing.BeanComponentDefinition;
 import org.springframework.beans.factory.support.ManagedMap;
@@ -31,12 +27,15 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.mvc.HttpRequestHandlerAdapter;
 import org.springframework.web.servlet.resource.DefaultServletHttpRequestHandler;
+import org.w3c.dom.Element;
+
+import java.util.Map;
 
 /**
- * {@link BeanDefinitionParser} that parses a {@code default-servlet-handler} element to
- * register a {@link DefaultServletHttpRequestHandler}.  Will also register a
- * {@link SimpleUrlHandlerMapping} for mapping resource requests, and a
- * {@link HttpRequestHandlerAdapter}.
+ * 解析 {@code default-servlet-handler} 元素的 {@link BeanDefinitionParser}，
+ * 用于注册 {@link DefaultServletHttpRequestHandler}。
+ * 还将注册一个 {@link SimpleUrlHandlerMapping} 以映射资源请求，
+ * 以及一个 {@link HttpRequestHandlerAdapter}。
  *
  * @author Jeremy Grelle
  * @author Rossen Stoyanchev
@@ -47,32 +46,51 @@ class DefaultServletHandlerBeanDefinitionParser implements BeanDefinitionParser 
 	@Override
 	@Nullable
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
+		// 从解析上下文中提取元素的源对象
 		Object source = parserContext.extractSource(element);
 
+		// 获取元素的 "default-servlet-name" 属性
 		String defaultServletName = element.getAttribute("default-servlet-name");
+		// 创建 RootBeanDefinition 对象，表示 DefaultServletHttpRequestHandler 类的定义
 		RootBeanDefinition defaultServletHandlerDef = new RootBeanDefinition(DefaultServletHttpRequestHandler.class);
+		// 设置源对象
 		defaultServletHandlerDef.setSource(source);
+		// 设置角色为基础设施角色
 		defaultServletHandlerDef.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
 		if (StringUtils.hasText(defaultServletName)) {
+			// 如果存在 defaultServletName 属性，则添加到属性值中
 			defaultServletHandlerDef.getPropertyValues().add("defaultServletName", defaultServletName);
 		}
+		// 生成 DefaultServletHttpRequestHandler 的 bean 名称
 		String defaultServletHandlerName = parserContext.getReaderContext().generateBeanName(defaultServletHandlerDef);
+		// 将 DefaultServletHttpRequestHandler 注册到 BeanDefinitionRegistry 中
 		parserContext.getRegistry().registerBeanDefinition(defaultServletHandlerName, defaultServletHandlerDef);
+		// 将 DefaultServletHttpRequestHandler 注册为组件
 		parserContext.registerComponent(new BeanComponentDefinition(defaultServletHandlerDef, defaultServletHandlerName));
 
+		// 创建一个 ManagedMap 对象，用于存放 URL 和处理器映射
 		Map<String, String> urlMap = new ManagedMap<>();
+		// 将默认 servlet 处理器名称映射到所有 URL
 		urlMap.put("/**", defaultServletHandlerName);
 
+		// 创建 RootBeanDefinition 对象，表示 SimpleUrlHandlerMapping 类的定义
 		RootBeanDefinition handlerMappingDef = new RootBeanDefinition(SimpleUrlHandlerMapping.class);
+		// 设置源对象
 		handlerMappingDef.setSource(source);
+		// 设置角色为基础设施角色
 		handlerMappingDef.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+		// 添加 urlMap 属性值
 		handlerMappingDef.getPropertyValues().add("urlMap", urlMap);
 
+		// 生成 SimpleUrlHandlerMapping 的 bean 名称
 		String handlerMappingBeanName = parserContext.getReaderContext().generateBeanName(handlerMappingDef);
+		// 将 SimpleUrlHandlerMapping 注册到 BeanDefinitionRegistry 中
 		parserContext.getRegistry().registerBeanDefinition(handlerMappingBeanName, handlerMappingDef);
+		// 将 SimpleUrlHandlerMapping 注册为组件
 		parserContext.registerComponent(new BeanComponentDefinition(handlerMappingDef, handlerMappingBeanName));
 
-		// Ensure BeanNameUrlHandlerMapping (SPR-8289) and default HandlerAdapters are not "turned off"
+		// 确保 BeanNameUrlHandlerMapping 和默认的 HandlerAdapters 不被关闭
+		// 注册默认组件
 		MvcNamespaceUtils.registerDefaultComponents(parserContext, source);
 
 		return null;
--- spring-webmvc/src/main/java/org/springframework/web/servlet/config/InterceptorsBeanDefinitionParser.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/config/InterceptorsBeanDefinitionParser.java
@@ -16,10 +16,6 @@
 
 package org.springframework.web.servlet.config;
 
-import java.util.List;
-
-import org.w3c.dom.Element;
-
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.parsing.BeanComponentDefinition;
@@ -31,10 +27,13 @@ import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.lang.Nullable;
 import org.springframework.util.xml.DomUtils;
 import org.springframework.web.servlet.handler.MappedInterceptor;
+import org.w3c.dom.Element;
+
+import java.util.List;
 
 /**
- * {@link org.springframework.beans.factory.xml.BeanDefinitionParser} that parses a
- * {@code interceptors} element to register a set of {@link MappedInterceptor} definitions.
+ * {@link org.springframework.beans.factory.xml.BeanDefinitionParser} 解析 {@code interceptors} 元素，
+ * 用于注册一组 {@link MappedInterceptor} 定义。
  *
  * @author Keith Donald
  * @since 3.0
@@ -44,54 +43,79 @@ class InterceptorsBeanDefinitionParser implements BeanDefinitionParser {
 	@Override
 	@Nullable
 	public BeanDefinition parse(Element element, ParserContext context) {
-		context.pushContainingComponent(
-				new CompositeComponentDefinition(element.getTagName(), context.extractSource(element)));
+		// 将包含组件推送到上下文中，其中包含元素的标签名和来源
+		context.pushContainingComponent(new CompositeComponentDefinition(element.getTagName(), context.extractSource(element)));
 
+		// 初始化路径匹配器引用
 		RuntimeBeanReference pathMatcherRef = null;
 		if (element.hasAttribute("path-matcher")) {
+			// 如果有 path-matcher 属性，则设置路径匹配器引用
 			pathMatcherRef = new RuntimeBeanReference(element.getAttribute("path-matcher"));
 		}
 
+		// 获取拦截器元素列表
 		List<Element> interceptors = DomUtils.getChildElementsByTagName(element, "bean", "ref", "interceptor");
+		// 遍历拦截器列表
 		for (Element interceptor : interceptors) {
+			// 创建拦截器的根Bean定义
 			RootBeanDefinition mappedInterceptorDef = new RootBeanDefinition(MappedInterceptor.class);
+			// 设置属性源
 			mappedInterceptorDef.setSource(context.extractSource(interceptor));
+			// 设置角色为基础设施角色
 			mappedInterceptorDef.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
 
 			ManagedList<String> includePatterns = null;
 			ManagedList<String> excludePatterns = null;
 			Object interceptorBean;
+			// 如果拦截器元素的本地名称是 "interceptor"
 			if ("interceptor".equals(interceptor.getLocalName())) {
+				// 获取包含模式和排除模式
 				includePatterns = getIncludePatterns(interceptor, "mapping");
 				excludePatterns = getIncludePatterns(interceptor, "exclude-mapping");
+				// 获取 bean 元素或 bean 引用元素
 				Element beanElem = DomUtils.getChildElementsByTagName(interceptor, "bean", "ref").get(0);
+				// 解析拦截器 bean 元素
 				interceptorBean = context.getDelegate().parsePropertySubElement(beanElem, null);
-			}
-			else {
+			} else {
+				// 解析拦截器元素
 				interceptorBean = context.getDelegate().parsePropertySubElement(interceptor, null);
 			}
+			// 设置拦截器的构造函数参数
+			// 第一个构造参数为 包含模式
 			mappedInterceptorDef.getConstructorArgumentValues().addIndexedArgumentValue(0, includePatterns);
+			// 第二个构造参数为 排除模式
 			mappedInterceptorDef.getConstructorArgumentValues().addIndexedArgumentValue(1, excludePatterns);
+			// 第三个构造参数为 拦截器
 			mappedInterceptorDef.getConstructorArgumentValues().addIndexedArgumentValue(2, interceptorBean);
 
 			if (pathMatcherRef != null) {
+				// 如果存在路径匹配器引用，则设置到拦截器定义中
 				mappedInterceptorDef.getPropertyValues().add("pathMatcher", pathMatcherRef);
 			}
 
+			// 注册拦截器定义，并获取生成的 bean 名称
 			String beanName = context.getReaderContext().registerWithGeneratedName(mappedInterceptorDef);
+			// 注册组件
 			context.registerComponent(new BeanComponentDefinition(mappedInterceptorDef, beanName));
 		}
 
+		// 弹出并注册包含组件
 		context.popAndRegisterContainingComponent();
+		// 返回 null
 		return null;
 	}
 
 	private ManagedList<String> getIncludePatterns(Element interceptor, String elementName) {
+		// 根据拦截器元素和元素名称获取 子元素列表
 		List<Element> paths = DomUtils.getChildElementsByTagName(interceptor, elementName);
+		// 创建一个 ManagedList 对象，用于保存路径模式
 		ManagedList<String> patterns = new ManagedList<>(paths.size());
+		// 遍历路径元素列表
 		for (Element path : paths) {
+			// 将路径元素的 "path" 属性添加到模式列表中
 			patterns.add(path.getAttribute("path"));
 		}
+		// 返回路径模式列表
 		return patterns;
 	}
 
