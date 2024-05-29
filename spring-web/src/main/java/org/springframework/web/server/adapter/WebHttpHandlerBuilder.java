994f23e1fd39ef270c4929ffa0936c42df4c64e6
LMW <CNlmw0726@gmail.com>
2024-05-29 22:18:03 +0800

    docs: 添加 WebHttpHandlerBuilder 的代码注释

--- spring-web/src/main/java/org/springframework/web/server/adapter/WebHttpHandlerBuilder.java
+++ spring-web/src/main/java/org/springframework/web/server/adapter/WebHttpHandlerBuilder.java
@@ -16,16 +16,6 @@
 
 package org.springframework.web.server.adapter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import reactor.blockhound.BlockHound;
-import reactor.blockhound.integration.BlockHoundIntegration;
-
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
@@ -44,70 +34,114 @@ import org.springframework.web.server.handler.FilteringWebHandler;
 import org.springframework.web.server.i18n.LocaleContextResolver;
 import org.springframework.web.server.session.DefaultWebSessionManager;
 import org.springframework.web.server.session.WebSessionManager;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.integration.BlockHoundIntegration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
- * This builder has two purposes:
+ * 此构建器有两个目的：
  *
- * <p>One is to assemble a processing chain that consists of a target {@link WebHandler},
- * then decorated with a set of {@link WebFilter WebFilters}, then further decorated with
- * a set of {@link WebExceptionHandler WebExceptionHandlers}.
+ * <p>一个是组装一个处理链，该链由目标{@link WebHandler}组成，然后装饰了一组{@link WebFilter WebFilters}，
+ * 然后进一步装饰了一组{@link WebExceptionHandler WebExceptionHandlers}。
  *
- * <p>The second purpose is to adapt the resulting processing chain to an {@link HttpHandler}:
- * the lowest-level reactive HTTP handling abstraction which can then be used with any of the
- * supported runtimes. The adaptation is done with the help of {@link HttpWebHandlerAdapter}.
+ * <p>第二个目的是将生成的处理链适配为{@link HttpHandler}：最低级别的反应式HTTP处理抽象，
+ * 然后可以与任何受支持的运行时一起使用。 使用{@link HttpWebHandlerAdapter}完成适配。
  *
- * <p>The processing chain can be assembled manually via builder methods, or detected from
- * a Spring {@link ApplicationContext} via {@link #applicationContext}, or a mix of both.
+ * <p>处理链可以通过构建器方法手动组装，也可以通过{@link #applicationContext}从Spring {@link ApplicationContext}中检测到，
+ * 或者两者混合使用。
  *
  * @author Rossen Stoyanchev
  * @author Sebastien Deleuze
- * @since 5.0
  * @see HttpWebHandlerAdapter
+ * @since 5.0
  */
 public final class WebHttpHandlerBuilder {
 
-	/** Well-known name for the target WebHandler in the bean factory. */
+	/**
+	 * 在bean工厂中用于目标WebHandler的众所周知的名称。
+	 */
 	public static final String WEB_HANDLER_BEAN_NAME = "webHandler";
 
-	/** Well-known name for the WebSessionManager in the bean factory. */
+	/**
+	 * 在bean工厂中用于WebSessionManager的众所周知的名称。
+	 */
 	public static final String WEB_SESSION_MANAGER_BEAN_NAME = "webSessionManager";
 
-	/** Well-known name for the ServerCodecConfigurer in the bean factory. */
+	/**
+	 * 在bean工厂中用于ServerCodecConfigurer的众所周知的名称。
+	 */
 	public static final String SERVER_CODEC_CONFIGURER_BEAN_NAME = "serverCodecConfigurer";
 
-	/** Well-known name for the LocaleContextResolver in the bean factory. */
+	/**
+	 * 在bean工厂中用于LocaleContextResolver的众所周知的名称。
+	 */
 	public static final String LOCALE_CONTEXT_RESOLVER_BEAN_NAME = "localeContextResolver";
 
-	/** Well-known name for the ForwardedHeaderTransformer in the bean factory. */
+	/**
+	 * 在bean工厂中用于ForwardedHeaderTransformer的众所周知的名称。
+	 */
 	public static final String FORWARDED_HEADER_TRANSFORMER_BEAN_NAME = "forwardedHeaderTransformer";
 
+	/**
+	 * WebHandler实例，用于处理HTTP请求。
+	 */
 	private final WebHandler webHandler;
 
+	/**
+	 * Spring应用程序上下文，用于检测组件是否在上下文中可用。
+	 */
 	@Nullable
 	private final ApplicationContext applicationContext;
 
+	/**
+	 * 用于对HTTP请求进行过滤的WebFilter列表。
+	 */
 	private final List<WebFilter> filters = new ArrayList<>();
 
+	/**
+	 * 用于处理WebHandler中发生的异常的WebExceptionHandler列表。
+	 */
 	private final List<WebExceptionHandler> exceptionHandlers = new ArrayList<>();
 
+	/**
+	 * 用于装饰HttpHandler的函数，对请求进行进一步处理。
+	 */
 	@Nullable
 	private Function<HttpHandler, HttpHandler> httpHandlerDecorator;
 
+	/**
+	 * Web会话管理器，用于管理Web会话。
+	 */
 	@Nullable
 	private WebSessionManager sessionManager;
 
+	/**
+	 * 用于配置服务器编解码器的组件。
+	 */
 	@Nullable
 	private ServerCodecConfigurer codecConfigurer;
 
+	/**
+	 * 用于解析和修改请求的区域设置上下文的解析器。
+	 */
 	@Nullable
 	private LocaleContextResolver localeContextResolver;
 
+	/**
+	 * 用于从“Forwarded”和“X-Forwarded-*”头中提取信息并修改请求的转发头转换器。
+	 */
 	@Nullable
 	private ForwardedHeaderTransformer forwardedHeaderTransformer;
 
 
 	/**
-	 * Private constructor to use when initialized from an ApplicationContext.
+	 * 从ApplicationContext初始化时使用的私有构造函数。
 	 */
 	private WebHttpHandlerBuilder(WebHandler webHandler, @Nullable ApplicationContext applicationContext) {
 		Assert.notNull(webHandler, "WebHandler must not be null");
@@ -116,7 +150,7 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Copy constructor.
+	 * 复制构造函数。
 	 */
 	private WebHttpHandlerBuilder(WebHttpHandlerBuilder other) {
 		this.webHandler = other.webHandler;
@@ -132,147 +166,166 @@ public final class WebHttpHandlerBuilder {
 
 
 	/**
-	 * Static factory method to create a new builder instance.
-	 * @param webHandler the target handler for the request
-	 * @return the prepared builder
+	 * 创建一个新的构建器实例，指定目标处理程序。
+	 *
+	 * @param webHandler 请求的目标处理程序
+	 * @return 准备好的构建器
 	 */
 	public static WebHttpHandlerBuilder webHandler(WebHandler webHandler) {
 		return new WebHttpHandlerBuilder(webHandler, null);
 	}
 
 	/**
-	 * Static factory method to create a new builder instance by detecting beans
-	 * in an {@link ApplicationContext}. The following are detected:
+	 * 通过检测应用程序上下文中的bean来创建一个新的构建器实例。会检测到以下bean：
 	 * <ul>
-	 * <li>{@link WebHandler} [1] -- looked up by the name
-	 * {@link #WEB_HANDLER_BEAN_NAME}.
-	 * <li>{@link WebFilter} [0..N] -- detected by type and ordered,
-	 * see {@link AnnotationAwareOrderComparator}.
-	 * <li>{@link WebExceptionHandler} [0..N] -- detected by type and
-	 * ordered.
-	 * <li>{@link HttpHandlerDecoratorFactory} [0..N] -- detected by type and
-	 * ordered.
-	 * <li>{@link WebSessionManager} [0..1] -- looked up by the name
-	 * {@link #WEB_SESSION_MANAGER_BEAN_NAME}.
-	 * <li>{@link ServerCodecConfigurer} [0..1] -- looked up by the name
-	 * {@link #SERVER_CODEC_CONFIGURER_BEAN_NAME}.
-	 * <li>{@link LocaleContextResolver} [0..1] -- looked up by the name
-	 * {@link #LOCALE_CONTEXT_RESOLVER_BEAN_NAME}.
+	 * <li>{@link WebHandler} [1] -- 根据名称{@link #WEB_HANDLER_BEAN_NAME}查找。
+	 * <li>{@link WebFilter} [0..N] -- 根据类型检测并排序，参见{@link AnnotationAwareOrderComparator}。
+	 * <li>{@link WebExceptionHandler} [0..N] -- 根据类型检测并排序。
+	 * <li>{@link HttpHandlerDecoratorFactory} [0..N] -- 根据类型检测并排序。
+	 * <li>{@link WebSessionManager} [0..1] -- 根据名称{@link #WEB_SESSION_MANAGER_BEAN_NAME}查找。
+	 * <li>{@link ServerCodecConfigurer} [0..1] -- 根据名称{@link #SERVER_CODEC_CONFIGURER_BEAN_NAME}查找。
+	 * <li>{@link LocaleContextResolver} [0..1] -- 根据名称{@link #LOCALE_CONTEXT_RESOLVER_BEAN_NAME}查找。
 	 * </ul>
-	 * @param context the application context to use for the lookup
-	 * @return the prepared builder
+	 *
+	 * @param context 用于查找bean的应用程序上下文
+	 * @return 准备好的构建器
 	 */
 	public static WebHttpHandlerBuilder applicationContext(ApplicationContext context) {
 
+		// 创建一个WebHttpHandlerBuilder实例
 		WebHttpHandlerBuilder builder = new WebHttpHandlerBuilder(
+				// 获取WebHandler实例
 				context.getBean(WEB_HANDLER_BEAN_NAME, WebHandler.class), context);
 
+		// 收集WebFilter实例并添加到过滤器列表中
 		List<WebFilter> webFilters = context
 				.getBeanProvider(WebFilter.class)
 				.orderedStream()
 				.collect(Collectors.toList());
 		builder.filters(filters -> filters.addAll(webFilters));
 
+		// 收集WebExceptionHandler实例并添加到异常处理器列表中
 		List<WebExceptionHandler> exceptionHandlers = context
 				.getBeanProvider(WebExceptionHandler.class)
 				.orderedStream()
 				.collect(Collectors.toList());
 		builder.exceptionHandlers(handlers -> handlers.addAll(exceptionHandlers));
 
+		// 添加HttpHandlerDecoratorFactory实例到构建器中
 		context.getBeanProvider(HttpHandlerDecoratorFactory.class)
 				.orderedStream()
 				.forEach(builder::httpHandlerDecorator);
 
 		try {
+			// 设置会话管理器，如果存在的话
 			builder.sessionManager(
 					context.getBean(WEB_SESSION_MANAGER_BEAN_NAME, WebSessionManager.class));
-		}
-		catch (NoSuchBeanDefinitionException ex) {
-			// Fall back on default
+		} catch (NoSuchBeanDefinitionException ex) {
+			// 默认情况
 		}
 
 		try {
+			// 设置编解码配置器，如果存在的话
 			builder.codecConfigurer(
 					context.getBean(SERVER_CODEC_CONFIGURER_BEAN_NAME, ServerCodecConfigurer.class));
-		}
-		catch (NoSuchBeanDefinitionException ex) {
-			// Fall back on default
+		} catch (NoSuchBeanDefinitionException ex) {
+			// 默认情况
 		}
 
 		try {
+			// 设置区域上下文解析器，如果存在的话
 			builder.localeContextResolver(
 					context.getBean(LOCALE_CONTEXT_RESOLVER_BEAN_NAME, LocaleContextResolver.class));
-		}
-		catch (NoSuchBeanDefinitionException ex) {
-			// Fall back on default
+		} catch (NoSuchBeanDefinitionException ex) {
+			// 默认情况
 		}
 
 		try {
+			// 设置转发头部转换器，如果存在的话
 			builder.forwardedHeaderTransformer(
 					context.getBean(FORWARDED_HEADER_TRANSFORMER_BEAN_NAME, ForwardedHeaderTransformer.class));
-		}
-		catch (NoSuchBeanDefinitionException ex) {
-			// Fall back on default
+		} catch (NoSuchBeanDefinitionException ex) {
+			// 默认情况
 		}
 
+		// 返回构建器
 		return builder;
 	}
 
 
 	/**
-	 * Add the given filter(s).
-	 * @param filters the filter(s) to add that's
+	 * 添加给定的过滤器。
+	 *
+	 * @param filters 要添加的过滤器(s)
+	 * @return 当前构建器实例
 	 */
 	public WebHttpHandlerBuilder filter(WebFilter... filters) {
 		if (!ObjectUtils.isEmpty(filters)) {
+			// 如果过滤器列表不为空，添加过滤器列表
 			this.filters.addAll(Arrays.asList(filters));
+			// 更新过滤器
 			updateFilters();
 		}
+		// 返回当前实例
 		return this;
 	}
 
 	/**
-	 * Manipulate the "live" list of currently configured filters.
-	 * @param consumer the consumer to use
+	 * 操作当前配置的过滤器的 "live" 列表。
+	 *
+	 * @param consumer 使用的 Consumer
+	 * @return 当前构建器实例
 	 */
 	public WebHttpHandlerBuilder filters(Consumer<List<WebFilter>> consumer) {
+		// 消费过滤器
 		consumer.accept(this.filters);
+		// 更新过滤器
 		updateFilters();
 		return this;
 	}
 
 	private void updateFilters() {
 		if (this.filters.isEmpty()) {
+			// 如果过滤器列表为空，则直接返回
 			return;
 		}
 
+		// 过滤器列表中包含转发头部转换器的情况下，设置转发头部转换器属性
 		List<WebFilter> filtersToUse = this.filters.stream()
 				.peek(filter -> {
 					if (filter instanceof ForwardedHeaderTransformer && this.forwardedHeaderTransformer == null) {
+						// 如果拦截器是 转换请求头转换器，并且当前没有设置转换请求头转换器，则设置转换请求头转换器
 						this.forwardedHeaderTransformer = (ForwardedHeaderTransformer) filter;
 					}
 				})
+				// 过滤掉转发头部转换器
 				.filter(filter -> !(filter instanceof ForwardedHeaderTransformer))
 				.collect(Collectors.toList());
 
+		// 清空原始过滤器列表，并将过滤器列表更新为过滤掉转发头部转换器后的列表
 		this.filters.clear();
 		this.filters.addAll(filtersToUse);
 	}
 
 	/**
-	 * Add the given exception handler(s).
-	 * @param handlers the exception handler(s)
+	 * 添加给定的异常处理器。
+	 *
+	 * @param handlers 异常处理器(s)
+	 * @return 当前构建器实例
 	 */
 	public WebHttpHandlerBuilder exceptionHandler(WebExceptionHandler... handlers) {
 		if (!ObjectUtils.isEmpty(handlers)) {
+			// 如果异常处理器列表不为空，添加异常处理器列表
 			this.exceptionHandlers.addAll(Arrays.asList(handlers));
 		}
 		return this;
 	}
 
 	/**
-	 * Manipulate the "live" list of currently configured exception handlers.
-	 * @param consumer the consumer to use
+	 * 操作当前配置的异常处理器的 "live" 列表。
+	 *
+	 * @param consumer 使用的 Consumer
+	 * @return 当前构建器实例
 	 */
 	public WebHttpHandlerBuilder exceptionHandlers(Consumer<List<WebExceptionHandler>> consumer) {
 		consumer.accept(this.exceptionHandlers);
@@ -280,10 +333,11 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Configure the {@link WebSessionManager} to set on the
-	 * {@link ServerWebExchange WebServerExchange}.
-	 * <p>By default {@link DefaultWebSessionManager} is used.
-	 * @param manager the session manager
+	 * 配置要设置在 {@link ServerWebExchange WebServerExchange} 上的 {@link WebSessionManager}。
+	 * 默认情况下，使用 {@link DefaultWebSessionManager}。
+	 *
+	 * @param manager 会话管理器
+	 * @return 当前构建器实例
 	 * @see HttpWebHandlerAdapter#setSessionManager(WebSessionManager)
 	 */
 	public WebHttpHandlerBuilder sessionManager(WebSessionManager manager) {
@@ -292,8 +346,9 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Whether a {@code WebSessionManager} is configured or not, either detected from an
-	 * {@code ApplicationContext} or explicitly configured via {@link #sessionManager}.
+	 * 是否已配置 {@code WebSessionManager}，无论是从 {@code ApplicationContext} 中检测到的还是通过 {@link #sessionManager} 显式配置的。
+	 *
+	 * @return 如果已配置 {@code WebSessionManager}，则为 true；否则为 false
 	 * @since 5.0.9
 	 */
 	public boolean hasSessionManager() {
@@ -301,8 +356,10 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Configure the {@link ServerCodecConfigurer} to set on the {@code WebServerExchange}.
-	 * @param codecConfigurer the codec configurer
+	 * 配置要设置在 {@link ServerWebExchange WebServerExchange} 上的 {@link ServerCodecConfigurer}。
+	 *
+	 * @param codecConfigurer 编解码配置器
+	 * @return 当前构建器实例
 	 */
 	public WebHttpHandlerBuilder codecConfigurer(ServerCodecConfigurer codecConfigurer) {
 		this.codecConfigurer = codecConfigurer;
@@ -311,8 +368,9 @@ public final class WebHttpHandlerBuilder {
 
 
 	/**
-	 * Whether a {@code ServerCodecConfigurer} is configured or not, either detected from an
-	 * {@code ApplicationContext} or explicitly configured via {@link #codecConfigurer}.
+	 * 是否已配置 {@code ServerCodecConfigurer}，无论是从 {@code ApplicationContext} 中检测到的还是通过 {@link #codecConfigurer} 显式配置的。
+	 *
+	 * @return 如果已配置 {@code ServerCodecConfigurer}，则为 true；否则为 false
 	 * @since 5.0.9
 	 */
 	public boolean hasCodecConfigurer() {
@@ -320,9 +378,10 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Configure the {@link LocaleContextResolver} to set on the
-	 * {@link ServerWebExchange WebServerExchange}.
-	 * @param localeContextResolver the locale context resolver
+	 * 配置要设置在 {@link ServerWebExchange WebServerExchange} 上的 {@link LocaleContextResolver}。
+	 *
+	 * @param localeContextResolver 区域环境解析器
+	 * @return 当前构建器实例
 	 */
 	public WebHttpHandlerBuilder localeContextResolver(LocaleContextResolver localeContextResolver) {
 		this.localeContextResolver = localeContextResolver;
@@ -330,8 +389,9 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Whether a {@code LocaleContextResolver} is configured or not, either detected from an
-	 * {@code ApplicationContext} or explicitly configured via {@link #localeContextResolver}.
+	 * 是否已配置 {@code LocaleContextResolver}，无论是从 {@code ApplicationContext} 中检测到的还是通过 {@link #localeContextResolver} 显式配置的。
+	 *
+	 * @return 如果已配置 {@code LocaleContextResolver}，则为 true；否则为 false
 	 * @since 5.0.9
 	 */
 	public boolean hasLocaleContextResolver() {
@@ -339,9 +399,10 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Configure the {@link ForwardedHeaderTransformer} for extracting and/or
-	 * removing forwarded headers.
-	 * @param transformer the transformer
+	 * 配置 {@link ForwardedHeaderTransformer} 用于提取和/或删除转发的标头。
+	 *
+	 * @param transformer 转换器
+	 * @return 当前构建器实例
 	 * @since 5.1
 	 */
 	public WebHttpHandlerBuilder forwardedHeaderTransformer(ForwardedHeaderTransformer transformer) {
@@ -350,9 +411,9 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Whether a {@code ForwardedHeaderTransformer} is configured or not, either
-	 * detected from an {@code ApplicationContext} or explicitly configured via
-	 * {@link #forwardedHeaderTransformer(ForwardedHeaderTransformer)}.
+	 * 是否配置了 {@code ForwardedHeaderTransformer}，无论是从 {@code ApplicationContext} 中检测到的还是通过 {@link #forwardedHeaderTransformer(ForwardedHeaderTransformer)} 显式配置的。
+	 *
+	 * @return 如果已配置 {@code ForwardedHeaderTransformer}，则为 true；否则为 false
 	 * @since 5.1
 	 */
 	public boolean hasForwardedHeaderTransformer() {
@@ -360,13 +421,10 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Configure a {@link Function} to decorate the {@link HttpHandler} returned
-	 * by this builder which effectively wraps the entire
-	 * {@link WebExceptionHandler} - {@link WebFilter} - {@link WebHandler}
-	 * processing chain. This provides access to the request and response before
-	 * the entire chain and likewise the ability to observe the result of
-	 * the entire chain.
-	 * @param handlerDecorator the decorator to apply
+	 * 配置一个 {@link Function} 来装饰由此构建器返回的 {@link HttpHandler}，这实际上包装了整个 {@link WebExceptionHandler} - {@link WebFilter} - {@link WebHandler} 处理链。这提供了在整个链之前访问请求和响应以及观察整个链结果的能力。
+	 *
+	 * @param handlerDecorator 要应用的装饰器
+	 * @return 当前构建器实例
 	 * @since 5.3
 	 */
 	public WebHttpHandlerBuilder httpHandlerDecorator(Function<HttpHandler, HttpHandler> handlerDecorator) {
@@ -376,8 +434,9 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Whether a decorator for {@link HttpHandler} is configured or not via
-	 * {@link #httpHandlerDecorator(Function)}.
+	 * 是否通过 {@link #httpHandlerDecorator(Function)} 配置了 {@link HttpHandler} 的装饰器。
+	 *
+	 * @return 如果已配置 {@link HttpHandler} 的装饰器，则为 true；否则为 false
 	 * @since 5.3
 	 */
 	public boolean hasHttpHandlerDecorator() {
@@ -385,12 +444,15 @@ public final class WebHttpHandlerBuilder {
 	}
 
 	/**
-	 * Build the {@link HttpHandler}.
+	 * 构建 {@link HttpHandler} 实例
 	 */
 	public HttpHandler build() {
+		// 创建过滤后的WebHandler
 		WebHandler decorated = new FilteringWebHandler(this.webHandler, this.filters);
-		decorated = new ExceptionHandlingWebHandler(decorated,  this.exceptionHandlers);
+		// 添加异常处理功能到WebHandler
+		decorated = new ExceptionHandlingWebHandler(decorated, this.exceptionHandlers);
 
+		// 创建HttpWebHandlerAdapter，并设置相应的属性
 		HttpWebHandlerAdapter adapted = new HttpWebHandlerAdapter(decorated);
 		if (this.sessionManager != null) {
 			adapted.setSessionManager(this.sessionManager);
@@ -407,14 +469,17 @@ public final class WebHttpHandlerBuilder {
 		if (this.applicationContext != null) {
 			adapted.setApplicationContext(this.applicationContext);
 		}
+		// 执行属性设置后的初始化操作
 		adapted.afterPropertiesSet();
 
+		// 如果有HttpHandlerDecorator，则应用装饰器，否则直接返回adapted
 		return (this.httpHandlerDecorator != null ? this.httpHandlerDecorator.apply(adapted) : adapted);
 	}
 
 	/**
-	 * Clone this {@link WebHttpHandlerBuilder}.
-	 * @return the cloned builder instance
+	 * 克隆此 {@link WebHttpHandlerBuilder}。
+	 *
+	 * @return 克隆的构建器实例
 	 */
 	@Override
 	public WebHttpHandlerBuilder clone() {
@@ -423,7 +488,8 @@ public final class WebHttpHandlerBuilder {
 
 
 	/**
-	 * {@code BlockHoundIntegration} for spring-web classes.
+	 * 用于 spring-web 类的 {@code BlockHoundIntegration}。
+	 *
 	 * @since 5.3.6
 	 */
 	public static class SpringWebBlockHoundIntegration implements BlockHoundIntegration {
@@ -431,7 +497,7 @@ public final class WebHttpHandlerBuilder {
 		@Override
 		public void applyTo(BlockHound.Builder builder) {
 
-			// Avoid hard references potentially anywhere in spring-web (no need for structural dependency)
+			// 避免在 spring-web 中的任何地方出现硬引用（无需结构依赖）
 
 			builder.allowBlockingCallsInside("org.springframework.http.MediaTypeFactory", "<clinit>");
 			builder.allowBlockingCallsInside("org.springframework.web.util.HtmlUtils", "<clinit>");
