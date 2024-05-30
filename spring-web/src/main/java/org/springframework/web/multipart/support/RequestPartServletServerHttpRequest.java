fc66912ee983f9b148d641de00ca5a1565017e4e
MingWei <CNlmw0726@gmail.com>
2024-05-30 13:05:39 +0800

    docs: 添加 请求部分Servlet服务端Http请求 的代码注释

--- spring-web/src/main/java/org/springframework/web/multipart/support/RequestPartServletServerHttpRequest.java
+++ spring-web/src/main/java/org/springframework/web/multipart/support/RequestPartServletServerHttpRequest.java
@@ -16,14 +16,6 @@
 
 package org.springframework.web.multipart.support;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.Part;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
@@ -34,44 +26,65 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.MultipartResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
 /**
- * {@link ServerHttpRequest} implementation that accesses one part of a multipart
- * request. If using {@link MultipartResolver} configuration the part is accessed
- * through a {@link MultipartFile}. Or if using Servlet 3.0 multipart processing
- * the part is accessed through {@code ServletRequest.getPart}.
+ * {@link ServerHttpRequest} 实现，用于访问多部分请求的一个部分。
+ * 如果使用 {@link MultipartResolver} 配置，则通过 {@link MultipartFile} 访问该部分。
+ * 或者如果使用 Servlet 3.0 多部分处理，则通过 {@code ServletRequest.getPart} 访问该部分。
  *
  * @author Rossen Stoyanchev
  * @author Juergen Hoeller
  * @since 3.1
  */
 public class RequestPartServletServerHttpRequest extends ServletServerHttpRequest {
-
+	/**
+	 * 多部分请求
+	 */
 	private final MultipartHttpServletRequest multipartRequest;
 
+	/**
+	 * 请求部分名称
+	 */
 	private final String requestPartName;
 
+	/**
+	 * 多部分请求头
+	 */
 	private final HttpHeaders multipartHeaders;
 
 
 	/**
-	 * Create a new {@code RequestPartServletServerHttpRequest} instance.
-	 * @param request the current servlet request
-	 * @param requestPartName the name of the part to adapt to the {@link ServerHttpRequest} contract
-	 * @throws MissingServletRequestPartException if the request part cannot be found
-	 * @throws MultipartException if MultipartHttpServletRequest cannot be initialized
+	 * 创建一个新的 {@code RequestPartServletServerHttpRequest} 实例。
+	 *
+	 * @param request         当前的 servlet 请求
+	 * @param requestPartName 要适应 {@link ServerHttpRequest} 合同的部分的名称
+	 * @throws MissingServletRequestPartException 如果找不到请求部分
+	 * @throws MultipartException                 如果无法初始化 MultipartHttpServletRequest
 	 */
 	public RequestPartServletServerHttpRequest(HttpServletRequest request, String requestPartName)
 			throws MissingServletRequestPartException {
 
+		// 调用父类构造函数，传入HttpServletRequest对象
 		super(request);
 
+		// 将HttpServletRequest对象转换为MultipartHttpServletRequest对象
 		this.multipartRequest = MultipartResolutionDelegate.asMultipartHttpServletRequest(request);
+		// 设置请求部分的名称
 		this.requestPartName = requestPartName;
 
+		// 获取请求部分的头部信息
 		HttpHeaders multipartHeaders = this.multipartRequest.getMultipartHeaders(requestPartName);
+		// 如果头部信息为空，则抛出MissingServletRequestPartException异常
 		if (multipartHeaders == null) {
 			throw new MissingServletRequestPartException(requestPartName);
 		}
+		// 设置请求部分的头部信息
 		this.multipartHeaders = multipartHeaders;
 	}
 
@@ -83,33 +96,40 @@ public class RequestPartServletServerHttpRequest extends ServletServerHttpReques
 
 	@Override
 	public InputStream getBody() throws IOException {
-		// Prefer Servlet Part resolution to cover file as well as parameter streams
+		// 更喜欢 Servlet 部分解析，以覆盖文件和参数流
 		boolean servletParts = (this.multipartRequest instanceof StandardMultipartHttpServletRequest);
 		if (servletParts) {
+			// 如果是Servlet部分，检索Servlet部分
 			Part part = retrieveServletPart();
 			if (part != null) {
+				// 如果检索到了部分，则返回该部分的输入流
 				return part.getInputStream();
 			}
 		}
 
-		// Spring-style distinction between MultipartFile and String parameters
+		// Spring 风格的区分 MultipartFile 和 String 参数
 		MultipartFile file = this.multipartRequest.getFile(this.requestPartName);
 		if (file != null) {
+			// 如果是多部分文件，则返回多部分文件的输入流
 			return file.getInputStream();
 		}
+		// 获取参数值
 		String paramValue = this.multipartRequest.getParameter(this.requestPartName);
 		if (paramValue != null) {
+			// 如果参数值存在，则返回该参数值对应的字节数组输入流
 			return new ByteArrayInputStream(paramValue.getBytes(determineCharset()));
 		}
 
-		// Fallback: Servlet Part resolution even if not indicated
+		// 如果未指示，则回退到 Servlet 部分解析
 		if (!servletParts) {
+			// 如果不是Servlet部分，检索Servlet部分
 			Part part = retrieveServletPart();
 			if (part != null) {
+				// 如果检索到了部分，则返回该部分的输入流
 				return part.getInputStream();
 			}
 		}
-
+		// 否则抛出异常
 		throw new IllegalStateException("No body available for request part '" + this.requestPartName + "'");
 	}
 
@@ -117,21 +137,26 @@ public class RequestPartServletServerHttpRequest extends ServletServerHttpReques
 	private Part retrieveServletPart() {
 		try {
 			return this.multipartRequest.getPart(this.requestPartName);
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			throw new MultipartException("Failed to retrieve request part '" + this.requestPartName + "'", ex);
 		}
 	}
 
 	private Charset determineCharset() {
+		// 获取请求头部的Content-Type
 		MediaType contentType = getHeaders().getContentType();
+		// 如果Content-Type不为空
 		if (contentType != null) {
+			// 获取Content-Type的字符集
 			Charset charset = contentType.getCharset();
+			// 如果字符集不为空，则返回该字符集
 			if (charset != null) {
 				return charset;
 			}
 		}
+		// 获取请求的字符编码
 		String encoding = this.multipartRequest.getCharacterEncoding();
+		// 如果字符编码不为空，则返回对应的字符集；否则返回默认的表单字符集
 		return (encoding != null ? Charset.forName(encoding) : FORM_CHARSET);
 	}
 
