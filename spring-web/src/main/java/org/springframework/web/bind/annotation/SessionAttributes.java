4f8f0d4d2cbfc2bdb29febd025a45912b21af3eb
LMW <CNlmw0726@gmail.com>
2024-05-30 23:38:49 +0800

    docs: 添加 SessionAttributes注解以及处理类 的代码注释

--- spring-web/src/main/java/org/springframework/web/bind/annotation/SessionAttributes.java
+++ spring-web/src/main/java/org/springframework/web/bind/annotation/SessionAttributes.java
@@ -16,40 +16,25 @@
 
 package org.springframework.web.bind.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.core.annotation.AliasFor;
 
+import java.lang.annotation.*;
+
 /**
- * Annotation that indicates the session attributes that a specific handler uses.
+ * 指示特定处理程序使用的会话属性的注解。
  *
- * <p>This will typically list the names of model attributes which should be
- * transparently stored in the session or some conversational storage,
- * serving as form-backing beans. <b>Declared at the type level</b>, applying
- * to the model attributes that the annotated handler class operates on.
+ * <p>通常会列出应该透明存储在会话或某些会话存储中的模型属性的名称，作为表单后端 bean。
+ * <b>声明在类型级别</b>，应用于被注解的处理程序类操作的模型属性。
  *
- * <p><b>NOTE:</b> Session attributes as indicated using this annotation
- * correspond to a specific handler's model attributes, getting transparently
- * stored in a conversational session. Those attributes will be removed once
- * the handler indicates completion of its conversational session. Therefore,
- * use this facility for such conversational attributes which are supposed
- * to be stored in the session <i>temporarily</i> during the course of a
- * specific handler's conversation.
+ * <p><b>注意：</b>使用此注解指示的会话属性对应于特定处理程序的特定模型属性，会在会话期间被透明存储在对话式会话中。
+ * 一旦处理程序指示其会话对话完成，这些属性将被删除。因此，在特定处理程序对话过程中暂时存储在会话中的会话属性，
+ * 使用此功能。
  *
- * <p>For permanent session attributes, e.g. a user authentication object,
- * use the traditional {@code session.setAttribute} method instead.
- * Alternatively, consider using the attribute management capabilities of the
- * generic {@link org.springframework.web.context.request.WebRequest} interface.
+ * <p>对于永久性会话属性，例如用户身份验证对象，请改用传统的{@code session.setAttribute}方法。
+ * 或者，考虑使用通用{@link org.springframework.web.context.request.WebRequest}接口的属性管理功能。
  *
- * <p><b>NOTE:</b> When using controller interfaces (e.g. for AOP proxying),
- * make sure to consistently put <i>all</i> your mapping annotations &mdash;
- * such as {@code @RequestMapping} and {@code @SessionAttributes} &mdash; on
- * the controller <i>interface</i> rather than on the implementation class.
+ * <p><b>注意：</b>使用控制器接口（例如用于AOP代理），请确保一致地将<i>所有</i>映射注解，例如{@code @RequestMapping}
+ * 和{@code @SessionAttributes}，放在控制器<i>接口</i>上，而不是在实现类上。
  *
  * @author Juergen Hoeller
  * @author Sam Brannen
@@ -62,28 +47,25 @@ import org.springframework.core.annotation.AliasFor;
 public @interface SessionAttributes {
 
 	/**
-	 * Alias for {@link #names}.
+	 * {@link #names}的别名。
 	 */
 	@AliasFor("names")
 	String[] value() default {};
 
 	/**
-	 * The names of session attributes in the model that should be stored in the
-	 * session or some conversational storage.
-	 * <p><strong>Note</strong>: This indicates the <em>model attribute names</em>.
-	 * The <em>session attribute names</em> may or may not match the model attribute
-	 * names. Applications should therefore not rely on the session attribute
-	 * names but rather operate on the model only.
+	 * 应该存储在会话或某些会话存储中的模型中的会话属性的名称。
+	 * <p><strong>注意</strong>：这表示<em>模型属性名称</em>。
+	 * <em>会话属性名称</em>可能与模型属性名称匹配，也可能不匹配。
+	 * 因此，应用程序不应依赖于会话属性名称，而应仅操作模型。
+	 *
 	 * @since 4.2
 	 */
 	@AliasFor("value")
 	String[] names() default {};
 
 	/**
-	 * The types of session attributes in the model that should be stored in the
-	 * session or some conversational storage.
-	 * <p>All model attributes of these types will be stored in the session,
-	 * regardless of attribute name.
+	 * 应该存储在会话或某些会话存储中的模型中的会话属性的类型。
+	 * <p>这些类型的所有模型属性都将存储在会话中，而不管属性名称如何。
 	 */
 	Class<?>[] types() default {};
 
--- spring-web/src/main/java/org/springframework/web/method/annotation/SessionAttributesHandler.java
+++ spring-web/src/main/java/org/springframework/web/method/annotation/SessionAttributesHandler.java
@@ -16,13 +16,6 @@
 
 package org.springframework.web.method.annotation;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
@@ -31,129 +24,158 @@ import org.springframework.web.bind.support.SessionAttributeStore;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
- * Manages controller-specific session attributes declared via
- * {@link SessionAttributes @SessionAttributes}. Actual storage is
- * delegated to a {@link SessionAttributeStore} instance.
+ * 管理通过{@link SessionAttributes @SessionAttributes}声明的特定于控制器的会话属性。
+ * 实际存储被委托给{@link SessionAttributeStore}实例。
  *
- * <p>When a controller annotated with {@code @SessionAttributes} adds
- * attributes to its model, those attributes are checked against names and
- * types specified via {@code @SessionAttributes}. Matching model attributes
- * are saved in the HTTP session and remain there until the controller calls
- * {@link SessionStatus#setComplete()}.
+ * <p>当标记有{@code @SessionAttributes}注解的控制器将属性添加到其模型时，这些属性将与通过
+ * {@code @SessionAttributes}指定的名称和类型进行匹配。匹配的模型属性将保存在HTTP会话中，
+ * 并在控制器调用{@link SessionStatus#setComplete()}时保留在那里。
  *
  * @author Rossen Stoyanchev
  * @author Juergen Hoeller
  * @since 3.1
  */
 public class SessionAttributesHandler {
-
+	/**
+	 * 存放 @SessionAttribute 的属性名称集合
+	 */
 	private final Set<String> attributeNames = new HashSet<>();
 
+	/**
+	 * 存放 @SessionAttribute 的属性类型集合
+	 */
 	private final Set<Class<?>> attributeTypes = new HashSet<>();
 
+	/**
+	 * 已知的属性名称集合
+	 */
 	private final Set<String> knownAttributeNames = Collections.newSetFromMap(new ConcurrentHashMap<>(4));
 
+	/**
+	 * 会话属性值存储
+	 */
 	private final SessionAttributeStore sessionAttributeStore;
 
 
 	/**
-	 * Create a new session attributes handler. Session attribute names and types
-	 * are extracted from the {@code @SessionAttributes} annotation, if present,
-	 * on the given type.
-	 * @param handlerType the controller type
-	 * @param sessionAttributeStore used for session access
+	 * 创建一个新的会话属性处理程序。会话属性名称和类型从给定类型的{@code @SessionAttributes}注解中提取。
+	 *
+	 * @param handlerType           控制器类型
+	 * @param sessionAttributeStore 用于会话访问的SessionAttributeStore
 	 */
 	public SessionAttributesHandler(Class<?> handlerType, SessionAttributeStore sessionAttributeStore) {
+		// 确保 会话属性值存储 不为空，如果为空则抛出异常
 		Assert.notNull(sessionAttributeStore, "SessionAttributeStore may not be null");
+		// 将传入的 会话属性值存储 赋值给当前对象的 会话属性值存储 属性
 		this.sessionAttributeStore = sessionAttributeStore;
 
+		// 查找 处理类 上是否存在 SessionAttributes 注解，并获取该注解
 		SessionAttributes ann = AnnotatedElementUtils.findMergedAnnotation(handlerType, SessionAttributes.class);
+		// 如果存在@SessionAttributes注解
 		if (ann != null) {
+			// 将注解中的属性名添加到 属性名称 集合中
 			Collections.addAll(this.attributeNames, ann.names());
+			// 将注解中的属性类型添加到 属性类型 集合中
 			Collections.addAll(this.attributeTypes, ann.types());
 		}
+		// 将 属性名称 集合中的所有元素添加到 已知的属性名称集合 集合中
 		this.knownAttributeNames.addAll(this.attributeNames);
 	}
 
 
 	/**
-	 * Whether the controller represented by this instance has declared any
-	 * session attributes through an {@link SessionAttributes} annotation.
+	 * 此实例表示的控制器是否通过{@link SessionAttributes @SessionAttributes}注解声明了任何会话属性。
 	 */
 	public boolean hasSessionAttributes() {
 		return (!this.attributeNames.isEmpty() || !this.attributeTypes.isEmpty());
 	}
 
 	/**
-	 * Whether the attribute name or type match the names and types specified
-	 * via {@code @SessionAttributes} on the underlying controller.
-	 * <p>Attributes successfully resolved through this method are "remembered"
-	 * and subsequently used in {@link #retrieveAttributes(WebRequest)} and
-	 * {@link #cleanupAttributes(WebRequest)}.
-	 * @param attributeName the attribute name to check
-	 * @param attributeType the type for the attribute
+	 * 属性名称或类型是否与基础控制器上通过{@code @SessionAttributes}指定的名称和类型匹配。
+	 * <p>通过此方法成功解析的属性会被“记住”，并在{@link #retrieveAttributes(WebRequest)}和
+	 * {@link #cleanupAttributes(WebRequest)}中后续使用。
+	 *
+	 * @param attributeName 属性名称
+	 * @param attributeType 属性类型
 	 */
 	public boolean isHandlerSessionAttribute(String attributeName, Class<?> attributeType) {
+		// 确保 属性名称 不为空，如果为空则抛出异常
 		Assert.notNull(attributeName, "Attribute name must not be null");
+		// 判断 属性名称集合 中是否包含 属性名称 或 属性类型集合 中是否包含 属性类型
 		if (this.attributeNames.contains(attributeName) || this.attributeTypes.contains(attributeType)) {
+			// 如果包含，将 属性名称 添加到 已知属性名称集合 中
 			this.knownAttributeNames.add(attributeName);
+			// 返回 true，表示该属性名或属性类型是已知的
 			return true;
-		}
-		else {
+		} else {
+			// 返回 false，表示该属性名或属性类型不是已知的
 			return false;
 		}
+
 	}
 
 	/**
-	 * Store a subset of the given attributes in the session. Attributes not
-	 * declared as session attributes via {@code @SessionAttributes} are ignored.
-	 * @param request the current request
-	 * @param attributes candidate attributes for session storage
+	 * 将给定属性的子集存储在会话中。未通过{@code @SessionAttributes}声明为会话属性的属性将被忽略。
+	 *
+	 * @param request    当前请求
+	 * @param attributes 候选的会话存储属性
 	 */
 	public void storeAttributes(WebRequest request, Map<String, ?> attributes) {
+		// 遍历 attributes 集合中的每个键值对
 		attributes.forEach((name, value) -> {
+			// 如果值不为空且该属性名和属性类型是处理器的会话属性
 			if (value != null && isHandlerSessionAttribute(name, value.getClass())) {
+				// 将该属性存储到会话中
 				this.sessionAttributeStore.storeAttribute(request, name, value);
 			}
 		});
 	}
 
 	/**
-	 * Retrieve "known" attributes from the session, i.e. attributes listed
-	 * by name in {@code @SessionAttributes} or attributes previously stored
-	 * in the model that matched by type.
-	 * @param request the current request
-	 * @return a map with handler session attributes, possibly empty
+	 * 从会话中检索“已知”属性，即在{@code @SessionAttributes}中列出的名称或以前存储在模型中的匹配类型的属性。
+	 *
+	 * @param request 当前请求
+	 * @return 带有处理程序会话属性的映射，可能为空
 	 */
 	public Map<String, Object> retrieveAttributes(WebRequest request) {
+		// 创建一个新的 HashMap 来存储属性
 		Map<String, Object> attributes = new HashMap<>();
+		// 遍历已知的属性名称集合
 		for (String name : this.knownAttributeNames) {
+			// 从会话中检索该属性名对应的值
 			Object value = this.sessionAttributeStore.retrieveAttribute(request, name);
+			// 如果值不为空，则将其添加到属性映射中
 			if (value != null) {
 				attributes.put(name, value);
 			}
 		}
+		// 返回包含属性的映射
 		return attributes;
 	}
 
 	/**
-	 * Remove "known" attributes from the session, i.e. attributes listed
-	 * by name in {@code @SessionAttributes} or attributes previously stored
-	 * in the model that matched by type.
-	 * @param request the current request
+	 * 从会话中删除“已知”属性，即在{@code @SessionAttributes}中列出的名称或以前存储在模型中的匹配类型的属性。
+	 *
+	 * @param request 当前请求
 	 */
 	public void cleanupAttributes(WebRequest request) {
+		// 遍历已知的属性名称集合
 		for (String attributeName : this.knownAttributeNames) {
+			// 清理会话中的该属性名对应的值
 			this.sessionAttributeStore.cleanupAttribute(request, attributeName);
 		}
 	}
 
 	/**
-	 * A pass-through call to the underlying {@link SessionAttributeStore}.
-	 * @param request the current request
-	 * @param attributeName the name of the attribute of interest
-	 * @return the attribute value, or {@code null} if none
+	 * 对底层的{@link SessionAttributeStore}进行的一个透传调用。
+	 *
+	 * @param request       当前请求
+	 * @param attributeName 属性的名称
+	 * @return 属性值，如果没有则为{@code null}
 	 */
 	@Nullable
 	Object retrieveAttribute(WebRequest request, String attributeName) {
