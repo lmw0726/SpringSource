6e844da711f3a2dd6302e7f36d9857ec385e2d9f
LMW <CNlmw0726@gmail.com>
2024-05-30 23:39:08 +0800

    docs: 添加 方法参数异常 的代码注释

--- spring-web/src/main/java/org/springframework/web/method/annotation/MethodArgumentConversionNotSupportedException.java
+++ spring-web/src/main/java/org/springframework/web/method/annotation/MethodArgumentConversionNotSupportedException.java
@@ -21,23 +21,35 @@ import org.springframework.core.MethodParameter;
 import org.springframework.lang.Nullable;
 
 /**
- * A ConversionNotSupportedException raised while resolving a method argument.
- * Provides access to the target {@link org.springframework.core.MethodParameter
- * MethodParameter}.
+ * 在解析方法参数时引发的ConversionNotSupportedException。
+ * 提供对目标{@link org.springframework.core.MethodParameter MethodParameter}的访问。
  *
  * @author Rossen Stoyanchev
  * @since 4.2
  */
 @SuppressWarnings("serial")
 public class MethodArgumentConversionNotSupportedException extends ConversionNotSupportedException {
-
+	/**
+	 * 方法参数的名称
+	 */
 	private final String name;
 
+	/**
+	 * 目标方法参数
+	 */
 	private final MethodParameter parameter;
 
-
+	/**
+	 * 使用给定的值、所需类型、名称、方法参数和原因构造一个MethodArgumentConversionNotSupportedException实例。
+	 *
+	 * @param value 转换失败的值
+	 * @param requiredType 所需的类型
+	 * @param name 方法参数的名称
+	 * @param param 目标方法参数
+	 * @param cause 引起转换失败的异常
+	 */
 	public MethodArgumentConversionNotSupportedException(@Nullable Object value,
-			@Nullable Class<?> requiredType, String name, MethodParameter param, Throwable cause) {
+														 @Nullable Class<?> requiredType, String name, MethodParameter param, Throwable cause) {
 
 		super(value, requiredType, cause);
 		this.name = name;
@@ -46,14 +58,18 @@ public class MethodArgumentConversionNotSupportedException extends ConversionNot
 
 
 	/**
-	 * Return the name of the method argument.
+	 * 返回方法参数的名称。
+	 *
+	 * @return 方法参数的名称
 	 */
 	public String getName() {
 		return this.name;
 	}
 
 	/**
-	 * Return the target method parameter.
+	 * 返回目标方法参数。
+	 *
+	 * @return 目标方法参数
 	 */
 	public MethodParameter getParameter() {
 		return this.parameter;
