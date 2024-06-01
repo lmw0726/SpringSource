23a74c935bcef6e2fcb94c6ee880b6e56d6d404b
LMW <CNlmw0726@gmail.com>
2024-06-01 16:40:18 +0800

    docs: 添加 ResponseStatus注解 的代码注释

--- spring-web/src/main/java/org/springframework/web/bind/annotation/ResponseStatus.java
+++ spring-web/src/main/java/org/springframework/web/bind/annotation/ResponseStatus.java
@@ -16,44 +16,28 @@
 
 package org.springframework.web.bind.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.http.HttpStatus;
 
+import java.lang.annotation.*;
+
 /**
- * Marks a method or exception class with the status {@link #code} and
- * {@link #reason} that should be returned.
+ * 使用 {@link #code} 和 {@link #reason} 标记方法或异常类应返回的状态。
  *
- * <p>The status code is applied to the HTTP response when the handler
- * method is invoked and overrides status information set by other means,
- * like {@code ResponseEntity} or {@code "redirect:"}.
+ * <p>当调用处理程序方法时，状态码将应用于 HTTP 响应，并覆盖通过其他方式设置的状态信息，如 {@code ResponseEntity} 或 {@code "redirect:"}。
  *
- * <p><strong>Warning</strong>: when using this annotation on an exception
- * class, or when setting the {@code reason} attribute of this annotation,
- * the {@code HttpServletResponse.sendError} method will be used.
+ * <p><strong>警告</strong>：在异常类上使用此注解，或在此注解的 {@code reason} 属性上设置值时，将使用 {@code HttpServletResponse.sendError} 方法。
  *
- * <p>With {@code HttpServletResponse.sendError}, the response is considered
- * complete and should not be written to any further. Furthermore, the Servlet
- * container will typically write an HTML error page therefore making the
- * use of a {@code reason} unsuitable for REST APIs. For such cases it is
- * preferable to use a {@link org.springframework.http.ResponseEntity} as
- * a return type and avoid the use of {@code @ResponseStatus} altogether.
+ * <p>使用 {@code HttpServletResponse.sendError}，响应被视为已完成，并且不应进一步写入。此外，Servlet 容器通常会编写 HTML 错误页面，因此不适合在 REST API 中使用 {@code reason}。
+ * 对于这种情况，最好使用 {@link org.springframework.http.ResponseEntity} 作为返回类型，并完全避免使用 {@code @ResponseStatus}。
  *
- * <p>Note that a controller class may also be annotated with
- * {@code @ResponseStatus} which is then inherited by all {@code @RequestMapping}
- * and {@code @ExceptionHandler} methods in that class and its subclasses unless
- * overridden by a local {@code @ResponseStatus} declaration on the method.
+ * <p>请注意，控制器类还可以使用 {@code @ResponseStatus} 进行注释，然后该注解将被该类及其子类中的所有 {@code @RequestMapping} 和 {@code @ExceptionHandler} 方法继承，除非在方法上使用本地的 {@code @ResponseStatus} 声明进行覆盖。
  *
  * @author Arjen Poutsma
  * @author Sam Brannen
- * @since 3.0
  * @see org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver
  * @see javax.servlet.http.HttpServletResponse#sendError(int, String)
+ * @since 3.0
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
@@ -61,26 +45,26 @@ import org.springframework.http.HttpStatus;
 public @interface ResponseStatus {
 
 	/**
-	 * Alias for {@link #code}.
+	 * {@link #code} 的别名。
 	 */
 	@AliasFor("code")
 	HttpStatus value() default HttpStatus.INTERNAL_SERVER_ERROR;
 
 	/**
-	 * The status <em>code</em> to use for the response.
-	 * <p>Default is {@link HttpStatus#INTERNAL_SERVER_ERROR}, which should
-	 * typically be changed to something more appropriate.
-	 * @since 4.2
+	 * 用于响应的状态 <em>code</em>。
+	 * <p>默认为 {@link HttpStatus#INTERNAL_SERVER_ERROR}，通常应更改为更合适的值。
+	 *
 	 * @see javax.servlet.http.HttpServletResponse#setStatus(int)
 	 * @see javax.servlet.http.HttpServletResponse#sendError(int)
+	 * @since 4.2
 	 */
 	@AliasFor("value")
 	HttpStatus code() default HttpStatus.INTERNAL_SERVER_ERROR;
 
 	/**
-	 * The <em>reason</em> to be used for the response.
-	 * <p>Defaults to an empty string which will be ignored. Set the reason to a
-	 * non-empty value to have it used for the response.
+	 * 用于响应的 <em>reason</em>。
+	 * <p>默认为空字符串，将被忽略。将原因设置为非空值以将其用于响应。
+	 *
 	 * @see javax.servlet.http.HttpServletResponse#sendError(int, String)
 	 */
 	String reason() default "";
