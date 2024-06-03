5840b2a63e8423e9908083069b3efda9dc32fe2c
LMW <CNlmw0726@gmail.com>
2024-06-03 23:04:34 +0800

    docs: 完成 GenericFilterBean体系 的代码注释

--- spring-web/src/main/java/org/springframework/web/filter/FormContentFilter.java
+++ spring-web/src/main/java/org/springframework/web/filter/FormContentFilter.java
@@ -16,25 +16,6 @@
 
 package org.springframework.web.filter;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.FormHttpMessageConverter;
@@ -46,24 +27,38 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.*;
+
 /**
- * {@code Filter} that parses form data for HTTP PUT, PATCH, and DELETE requests
- * and exposes it as Servlet request parameters. By default the Servlet spec
- * only requires this for HTTP POST.
+ * {@code Filter} 解析 HTTP PUT、PATCH 和 DELETE 请求的表单数据，并将其作为 Servlet 请求参数公开。默认情况下，
+ * Servlet 规范仅要求这样做 HTTP POST 请求。
  *
  * @author Rossen Stoyanchev
  * @since 5.1
  */
 public class FormContentFilter extends OncePerRequestFilter {
-
+	/**
+	 * Http方法列表
+	 */
 	private static final List<String> HTTP_METHODS = Arrays.asList("PUT", "PATCH", "DELETE");
 
+	/**
+	 * 表单转换器
+	 */
 	private FormHttpMessageConverter formConverter = new AllEncompassingFormHttpMessageConverter();
 
 
 	/**
-	 * Set the converter to use for parsing form content.
-	 * <p>By default this is an instance of {@link AllEncompassingFormHttpMessageConverter}.
+	 * 设置用于解析表单内容的转换器。
+	 * <p>默认情况下，这是 {@link AllEncompassingFormHttpMessageConverter} 的实例。
 	 */
 	public void setFormConverter(FormHttpMessageConverter converter) {
 		Assert.notNull(converter, "FormHttpMessageConverter is required");
@@ -71,9 +66,9 @@ public class FormContentFilter extends OncePerRequestFilter {
 	}
 
 	/**
-	 * The default character set to use for reading form data.
-	 * This is a shortcut for:<br>
-	 * {@code getFormConverter.setCharset(charset)}.
+	 * 用于读取表单数据的默认字符集。
+	 * 这是一个快捷方式：<br>
+	 * {@code getFormConverter.setCharset(charset)}。
 	 */
 	public void setCharset(Charset charset) {
 		this.formConverter.setCharset(charset);
@@ -85,47 +80,66 @@ public class FormContentFilter extends OncePerRequestFilter {
 			HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
+		// 解析请求参数
 		MultiValueMap<String, String> params = parseIfNecessary(request);
+
+		// 如果解析出的参数不为空，则使用包装器对象处理请求
 		if (!CollectionUtils.isEmpty(params)) {
 			filterChain.doFilter(new FormContentRequestWrapper(request, params), response);
-		}
-		else {
+		} else {
+			// 如果解析出的参数为空，则直接处理请求
 			filterChain.doFilter(request, response);
 		}
 	}
 
 	@Nullable
 	private MultiValueMap<String, String> parseIfNecessary(HttpServletRequest request) throws IOException {
+		// 如果请求不应该被解析，则返回 null
 		if (!shouldParse(request)) {
 			return null;
 		}
 
+		// 创建 HttpInputMessage 对象，从请求中获取输入流
 		HttpInputMessage inputMessage = new ServletServerHttpRequest(request) {
 			@Override
 			public InputStream getBody() throws IOException {
 				return request.getInputStream();
 			}
 		};
+
+		// 使用表单转换器读取输入消息并返回解析的参数
 		return this.formConverter.read(null, inputMessage);
 	}
 
 	private boolean shouldParse(HttpServletRequest request) {
+		// 获取请求的内容类型
 		String contentType = request.getContentType();
+
+		// 获取请求的方法
 		String method = request.getMethod();
+
+		// 检查内容类型是否有长度，并且请求方法是否在 Http方法 集合中
 		if (StringUtils.hasLength(contentType) && HTTP_METHODS.contains(method)) {
 			try {
+				// 解析内容类型为 媒体类型 对象
 				MediaType mediaType = MediaType.parseMediaType(contentType);
+
+				// 检查是否是 application/x-www-form-urlencoded 类型
 				return MediaType.APPLICATION_FORM_URLENCODED.includes(mediaType);
-			}
-			catch (IllegalArgumentException ex) {
+			} catch (IllegalArgumentException ex) {
+				// 捕捉非法参数异常，但不做处理
 			}
 		}
+
+		// 如果内容类型为空，或者请求方法不在 Http方法 集合中，则返回 false
 		return false;
 	}
 
 
 	private static class FormContentRequestWrapper extends HttpServletRequestWrapper {
-
+		/**
+		 * 表单参数
+		 */
 		private MultiValueMap<String, String> formParams;
 
 		public FormContentRequestWrapper(HttpServletRequest request, MultiValueMap<String, String> params) {
@@ -136,45 +150,81 @@ public class FormContentFilter extends OncePerRequestFilter {
 		@Override
 		@Nullable
 		public String getParameter(String name) {
+			// 从父类中获取指定参数名的查询字符串值
 			String queryStringValue = super.getParameter(name);
+
+			// 从表单参数中获取指定参数名的第一个值
 			String formValue = this.formParams.getFirst(name);
+
+			// 如果查询字符串值不为空，则返回查询字符串值；否则返回表单参数值
 			return (queryStringValue != null ? queryStringValue : formValue);
 		}
 
 		@Override
 		public Map<String, String[]> getParameterMap() {
+			// 创建一个存储结果的 LinkedHashMap 对象，键为字符串，值为字符串数组
 			Map<String, String[]> result = new LinkedHashMap<>();
+
+			// 获取所有参数名称的枚举对象
 			Enumeration<String> names = getParameterNames();
+
+			// 遍历所有参数名称
 			while (names.hasMoreElements()) {
+				// 获取当前参数名称
 				String name = names.nextElement();
+
+				// 获取当前参数名称对应的所有值，并存储到结果映射中
 				result.put(name, getParameterValues(name));
 			}
+
+			// 返回结果映射
 			return result;
 		}
 
 		@Override
 		public Enumeration<String> getParameterNames() {
+			// 创建一个存储参数名称的 LinkedHashSet 对象，保证参数名称的唯一性和顺序
 			Set<String> names = new LinkedHashSet<>();
+
+			// 将父类中的参数名称添加到 名称 集合中
 			names.addAll(Collections.list(super.getParameterNames()));
+
+			// 将表单参数的名称添加到 名称 集合中
 			names.addAll(this.formParams.keySet());
+
+			// 将 名称 集合转换为枚举对象并返回
 			return Collections.enumeration(names);
 		}
 
 		@Override
 		@Nullable
 		public String[] getParameterValues(String name) {
+			// 从父类获取指定参数名称的参数值数组
 			String[] parameterValues = super.getParameterValues(name);
+
+			// 从表单参数中获取指定参数名称的参数值列表
 			List<String> formParam = this.formParams.get(name);
+
+			// 如果表单参数值为 null，直接返回父类的参数值数组
 			if (formParam == null) {
 				return parameterValues;
 			}
+
+			// 如果父类的参数值数组为 null 或者查询字符串为 null
 			if (parameterValues == null || getQueryString() == null) {
+				// 将表单参数值列表转换为数组并返回
 				return StringUtils.toStringArray(formParam);
-			}
-			else {
+			} else {
+				// 创建一个新的列表用于存储合并后的参数值
 				List<String> result = new ArrayList<>(parameterValues.length + formParam.size());
+
+				// 将父类的参数值数组添加到结果列表中
 				result.addAll(Arrays.asList(parameterValues));
+
+				// 将表单参数值列表添加到结果列表中
 				result.addAll(formParam);
+
+				// 将结果列表转换为数组并返回
 				return StringUtils.toStringArray(result);
 			}
 		}
--- spring-web/src/main/java/org/springframework/web/filter/ShallowEtagHeaderFilter.java
+++ spring-web/src/main/java/org/springframework/web/filter/ShallowEtagHeaderFilter.java
@@ -16,17 +16,6 @@
 
 package org.springframework.web.filter;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.util.Assert;
@@ -36,18 +25,25 @@ import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 import org.springframework.web.util.WebUtils;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
 /**
- * {@link javax.servlet.Filter} that generates an {@code ETag} value based on the
- * content on the response. This ETag is compared to the {@code If-None-Match}
- * header of the request. If these headers are equal, the response content is
- * not sent, but rather a {@code 304 "Not Modified"} status instead.
+ * {@link javax.servlet.Filter} 根据响应内容生成一个 {@code ETag} 值。
+ * 该 ETag 值与请求的 {@code If-None-Match} 头进行比较。
+ * 如果这些头相等，则不发送响应内容，而是发送 {@code 304 "Not Modified"} 状态。
  *
- * <p>Since the ETag is based on the response content, the response
- * (e.g. a {@link org.springframework.web.servlet.View}) is still rendered.
- * As such, this filter only saves bandwidth, not server performance.
+ * <p>由于 ETag 是基于响应内容的，因此响应（例如 {@link org.springframework.web.servlet.View}）仍然会被渲染。
+ * 因此，该过滤器只节省带宽，而不节省服务器性能。
  *
- * <p><b>NOTE:</b> As of Spring Framework 5.0, this filter uses request/response
- * decorators built on the Servlet 3.1 API.
+ * <p><b>注意：</b> 从 Spring Framework 5.0 开始，此过滤器使用基于 Servlet 3.1 API 的请求/响应装饰器。
  *
  * @author Arjen Poutsma
  * @author Rossen Stoyanchev
@@ -56,28 +52,36 @@ import org.springframework.web.util.WebUtils;
  * @since 3.0
  */
 public class ShallowEtagHeaderFilter extends OncePerRequestFilter {
-
+	/**
+	 * 不存储指定
+	 */
 	private static final String DIRECTIVE_NO_STORE = "no-store";
 
+	/**
+	 * 流属性
+	 */
 	private static final String STREAMING_ATTRIBUTE = ShallowEtagHeaderFilter.class.getName() + ".STREAMING";
 
-
+	/**
+	 * 写入响应的 ETag 值是否应该是弱 ETag
+	 */
 	private boolean writeWeakETag = false;
 
 
 	/**
-	 * Set whether the ETag value written to the response should be weak, as per RFC 7232.
-	 * <p>Should be configured using an {@code <init-param>} for parameter name
-	 * "writeWeakETag" in the filter definition in {@code web.xml}.
-	 * @since 4.3
+	 * 设置写入响应的 ETag 值是否应该是弱 ETag，按照 RFC 7232 规定。
+	 * <p>应在 {@code web.xml} 中过滤器定义的 {@code <init-param>} 中使用参数名 "writeWeakETag" 进行配置。
+	 *
 	 * @see <a href="https://tools.ietf.org/html/rfc7232#section-2.3">RFC 7232 section 2.3</a>
+	 * @since 4.3
 	 */
 	public void setWriteWeakETag(boolean writeWeakETag) {
 		this.writeWeakETag = writeWeakETag;
 	}
 
 	/**
-	 * Return whether the ETag value written to the response should be weak, as per RFC 7232.
+	 * 返回写入响应的 ETag 值是否应该是弱 ETag，按照 RFC 7232 规定。
+	 *
 	 * @since 4.3
 	 */
 	public boolean isWriteWeakETag() {
@@ -86,8 +90,7 @@ public class ShallowEtagHeaderFilter extends OncePerRequestFilter {
 
 
 	/**
-	 * The default value is {@code false} so that the filter may delay the generation
-	 * of an ETag until the last asynchronously dispatched thread.
+	 * 默认值为 {@code false}，因此过滤器可以延迟生成 ETag 直到最后一个异步调度线程。
 	 */
 	@Override
 	protected boolean shouldNotFilterAsyncDispatch() {
@@ -98,95 +101,119 @@ public class ShallowEtagHeaderFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
+		// 初始化一个用于处理的响应对象，默认为传入的响应对象
 		HttpServletResponse responseToUse = response;
+
+		// 如果请求不是异步分派并且响应对象，不是 ConditionalContentCachingResponseWrapper 的实例
 		if (!isAsyncDispatch(request) && !(response instanceof ConditionalContentCachingResponseWrapper)) {
+			// 用 ConditionalContentCachingResponseWrapper 包装响应对象
 			responseToUse = new ConditionalContentCachingResponseWrapper(response, request);
 		}
 
+		// 调用过滤链，传递请求和处理后的响应对象
 		filterChain.doFilter(request, responseToUse);
 
+		// 如果请求没有开始异步处理并且没有禁用内容缓存
 		if (!isAsyncStarted(request) && !isContentCachingDisabled(request)) {
+			// 更新响应对象
 			updateResponse(request, responseToUse);
 		}
 	}
 
 	private void updateResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// 从原生响应中获取 ConditionalContentCachingResponseWrapper 对象
 		ConditionalContentCachingResponseWrapper wrapper =
 				WebUtils.getNativeResponse(response, ConditionalContentCachingResponseWrapper.class);
+
 		Assert.notNull(wrapper, "ContentCachingResponseWrapper not found");
+
+		// 获取原生的 HttpServletResponse 对象
 		HttpServletResponse rawResponse = (HttpServletResponse) wrapper.getResponse();
 
+		// 如果请求和响应满足生成 ETag 的条件
 		if (isEligibleForEtag(request, wrapper, wrapper.getStatus(), wrapper.getContentInputStream())) {
+			// 获取响应头中的 ETag
 			String eTag = wrapper.getHeader(HttpHeaders.ETAG);
+			// 如果响应头中没有 ETag
 			if (!StringUtils.hasText(eTag)) {
+				// 生成 ETag 的值并设置到响应头中
 				eTag = generateETagHeaderValue(wrapper.getContentInputStream(), this.writeWeakETag);
 				rawResponse.setHeader(HttpHeaders.ETAG, eTag);
 			}
+			// 检查客户端的请求是否与当前资源的 ETag 匹配，如果匹配则返回 304 Not Modified
 			if (new ServletWebRequest(request, rawResponse).checkNotModified(eTag)) {
 				return;
 			}
 		}
 
+		// 将响应体复制到原生响应中
 		wrapper.copyBodyToResponse();
 	}
 
 	/**
-	 * Whether an ETag should be calculated for the given request and response
-	 * exchange. By default this is {@code true} if all of the following match:
+	 * 是否应为给定的请求和响应交换计算 ETag。默认情况下，如果以下所有条件都匹配，则为 {@code true}：
 	 * <ul>
-	 * <li>Response is not committed.</li>
-	 * <li>Response status codes is in the {@code 2xx} series.</li>
-	 * <li>Request method is a GET.</li>
-	 * <li>Response Cache-Control header does not contain "no-store" (or is not present at all).</li>
+	 * <li>响应未提交。</li>
+	 * <li>响应状态代码在 {@code 2xx} 系列中。</li>
+	 * <li>请求方法是 GET。</li>
+	 * <li>响应的 Cache-Control 头不包含 "no-store"（或根本不存在）。</li>
 	 * </ul>
-	 * @param request the HTTP request
-	 * @param response the HTTP response
-	 * @param responseStatusCode the HTTP response status code
-	 * @param inputStream the response body
-	 * @return {@code true} if eligible for ETag generation, {@code false} otherwise
+	 *
+	 * @param request            HTTP 请求
+	 * @param response           HTTP 响应
+	 * @param responseStatusCode HTTP 响应状态代码
+	 * @param inputStream        响应体
+	 * @return 如果符合 ETag 生成条件，则为 {@code true}，否则为 {@code false}
 	 */
 	protected boolean isEligibleForEtag(HttpServletRequest request, HttpServletResponse response,
-			int responseStatusCode, InputStream inputStream) {
+										int responseStatusCode, InputStream inputStream) {
 
+		// 如果响应未提交且响应状态码在 200 到 299 之间，且请求方法为 GET
 		if (!response.isCommitted() &&
 				responseStatusCode >= 200 && responseStatusCode < 300 &&
 				HttpMethod.GET.matches(request.getMethod())) {
-
+			// 检查响应头中是否包含 Cache-Control，并且不包含 no-store 指令
 			String cacheControl = response.getHeader(HttpHeaders.CACHE_CONTROL);
 			return (cacheControl == null || !cacheControl.contains(DIRECTIVE_NO_STORE));
 		}
 
+		// 否则返回 false
 		return false;
 	}
 
 	/**
-	 * Generate the ETag header value from the given response body byte array.
-	 * <p>The default implementation generates an MD5 hash.
-	 * @param inputStream the response body as an InputStream
-	 * @param isWeak whether the generated ETag should be weak
-	 * @return the ETag header value
+	 * 从给定的响应体字节数组生成 ETag 头值。
+	 * <p>默认实现生成一个 MD5 哈希。
+	 *
+	 * @param inputStream 响应体作为 InputStream
+	 * @param isWeak      是否生成的 ETag 应该是弱 ETag
+	 * @return ETag 头值
+	 * @throws IOException 如果发生 I/O 异常
 	 * @see org.springframework.util.DigestUtils
 	 */
 	protected String generateETagHeaderValue(InputStream inputStream, boolean isWeak) throws IOException {
-		// length of W/ + " + 0 + 32bits md5 hash + "
+		// 创建一个 StringBuilder 对象，初始容量为 37（W/ + " + 0 + 32bits md5 hash + "）
 		StringBuilder builder = new StringBuilder(37);
+		// 如果是弱 ETag，则在 builder 中添加 "W/"
 		if (isWeak) {
 			builder.append("W/");
 		}
+		// 添加 "0"
 		builder.append("\"0");
+		// 使用 DigestUtils 将 inputStream 的 MD5 哈希值追加到 builder 中
 		DigestUtils.appendMd5DigestAsHex(inputStream, builder);
+		// 添加 '"'
 		builder.append('"');
+		// 返回构建好的 ETag 字符串
 		return builder.toString();
 	}
 
 
 	/**
-	 * This method can be used to suppress the content caching response wrapper
-	 * of the ShallowEtagHeaderFilter. The main reason for this is streaming
-	 * scenarios which are not to be cached and do not need an eTag.
-	 * <p><strong>Note:</strong> This method must be called before the response
-	 * is written to in order for the entire response content to be written
-	 * without caching.
+	 * 此方法可用于禁用 ShallowEtagHeaderFilter 的内容缓存响应包装器。
+	 * 主要原因是不需要缓存的流式场景，也不需要 ETag。
+	 * <p><strong>注意：</strong> 必须在写入响应之前调用此方法，以便完整的响应内容写入时无需缓存。
+	 *
 	 * @since 4.2
 	 */
 	public static void disableContentCaching(ServletRequest request) {
@@ -200,11 +227,13 @@ public class ShallowEtagHeaderFilter extends OncePerRequestFilter {
 
 
 	/**
-	 * Returns the raw OutputStream, instead of the one that does caching,
-	 * if {@link #isContentCachingDisabled}.
+	 * 如果 {@link #isContentCachingDisabled}，则返回原始的 OutputStream，
+	 * 而不是执行缓存的 OutputStream。
 	 */
 	private static class ConditionalContentCachingResponseWrapper extends ContentCachingResponseWrapper {
-
+		/**
+		 * Http请求
+		 */
 		private final HttpServletRequest request;
 
 		ConditionalContentCachingResponseWrapper(HttpServletResponse response, HttpServletRequest request) {
@@ -220,7 +249,7 @@ public class ShallowEtagHeaderFilter extends OncePerRequestFilter {
 
 		@Override
 		public PrintWriter getWriter() throws IOException {
-			return (isContentCachingDisabled(this.request) || hasETag()?
+			return (isContentCachingDisabled(this.request) || hasETag() ?
 					getResponse().getWriter() : super.getWriter());
 		}
 
