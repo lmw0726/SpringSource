511d3a26a22a3a156ceabf31f757fbe1292843bd
LMW <CNlmw0726@gmail.com>
2024-06-14 21:36:28 +0800

    docs: 完成 Http方法枚举 的代码注释

--- spring-web/src/main/java/org/springframework/http/HttpMethod.java
+++ spring-web/src/main/java/org/springframework/http/HttpMethod.java
@@ -16,15 +16,14 @@
 
 package org.springframework.http;
 
+import org.springframework.lang.Nullable;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.lang.Nullable;
-
 /**
- * Enumeration of HTTP request methods. Intended for use
- * with {@link org.springframework.http.client.ClientHttpRequest}
- * and {@link org.springframework.web.client.RestTemplate}.
+ * HTTP 请求方法枚举。用于与 {@link org.springframework.http.client.ClientHttpRequest}
+ * 和 {@link org.springframework.web.client.RestTemplate} 一起使用。
  *
  * @author Arjen Poutsma
  * @author Juergen Hoeller
@@ -34,20 +33,25 @@ public enum HttpMethod {
 
 	GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
 
-
+	/**
+	 * 方法名 —— 方法映射
+	 */
 	private static final Map<String, HttpMethod> mappings = new HashMap<>(16);
 
 	static {
+		// 遍历枚举类HTTP方法中的所有枚举值
 		for (HttpMethod httpMethod : values()) {
+			// 将枚举值的名称作为键，枚举值本身作为值，存入映射中
 			mappings.put(httpMethod.name(), httpMethod);
 		}
 	}
 
 
 	/**
-	 * Resolve the given method value to an {@code HttpMethod}.
-	 * @param method the method value as a String
-	 * @return the corresponding {@code HttpMethod}, or {@code null} if not found
+	 * 将给定的方法值解析为 {@code HttpMethod}。
+	 *
+	 * @param method 方法值，作为字符串
+	 * @return 对应的 {@code HttpMethod}，如果未找到则返回 {@code null}
 	 * @since 4.2.4
 	 */
 	@Nullable
@@ -57,9 +61,10 @@ public enum HttpMethod {
 
 
 	/**
-	 * Determine whether this {@code HttpMethod} matches the given method value.
-	 * @param method the HTTP method as a String
-	 * @return {@code true} if it matches, {@code false} otherwise
+	 * 判断此 {@code HttpMethod} 是否与给定的方法值匹配。
+	 *
+	 * @param method HTTP 方法，作为字符串
+	 * @return 如果匹配则返回 {@code true}，否则返回 {@code false}
 	 * @since 4.2.4
 	 */
 	public boolean matches(String method) {
