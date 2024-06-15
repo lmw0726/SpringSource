8d0cc4c247e18f5dfd4807963fb03b7103d4cf40
LMW <CNlmw0726@gmail.com>
2024-06-15 22:32:11 +0800

    docs: 完成 路径容器体系 的代码注释

--- spring-web/src/main/java/org/springframework/http/server/DefaultPathContainer.java
+++ spring-web/src/main/java/org/springframework/http/server/DefaultPathContainer.java
@@ -16,43 +16,46 @@
 
 package org.springframework.http.server;
 
+import org.springframework.lang.Nullable;
+import org.springframework.util.*;
+
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
-import org.springframework.lang.Nullable;
-import org.springframework.util.Assert;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StringUtils;
-
 /**
- * Default implementation of {@link PathContainer}.
+ * {@link PathContainer}的默认实现。
  *
  * @author Rossen Stoyanchev
  * @author Sam Brannen
  * @since 5.0
  */
 final class DefaultPathContainer implements PathContainer {
-
+	/**
+	 * 空路径的路径容器
+	 */
 	private static final PathContainer EMPTY_PATH = new DefaultPathContainer("", Collections.emptyList());
 
+	/**
+	 * 字符集和分隔符映射
+	 */
 	private static final Map<Character, DefaultSeparator> SEPARATORS = new HashMap<>(2);
 
 	static {
+		// 添加 ‘/’ 和 '.' 两种分隔符
 		SEPARATORS.put('/', new DefaultSeparator('/', "%2F"));
 		SEPARATORS.put('.', new DefaultSeparator('.', "%2E"));
 	}
 
-
+	/**
+	 * 路径
+	 */
 	private final String path;
 
+	/**
+	 * 元素列表
+	 */
 	private final List<Element> elements;
 
 
@@ -96,86 +99,152 @@ final class DefaultPathContainer implements PathContainer {
 
 
 	static PathContainer createFromUrlPath(String path, Options options) {
+		// 如果路径字符串为空，则返回一个表示空路径的路径容器
 		if (path.isEmpty()) {
 			return EMPTY_PATH;
 		}
+
+		// 获取路径分隔符
 		char separator = options.separator();
+
+		// 根据分隔符获取对应的分隔符元素
 		DefaultSeparator separatorElement = SEPARATORS.get(separator);
+
+		// 如果分隔符元素为 null，则抛出异常，表示分隔符不合法
 		if (separatorElement == null) {
 			throw new IllegalArgumentException("Unexpected separator: '" + separator + "'");
 		}
+
+		// 创建一个空的元素列表，用于存储路径的各个部分
 		List<Element> elements = new ArrayList<>();
+
+		// 初始化起始位置
 		int begin;
+
+		// 如果路径的第一个字符是分隔符
 		if (path.charAt(0) == separator) {
+			// 则从第二个字符开始处理
 			begin = 1;
+			// 并添加一个分隔符元素到元素列表中
 			elements.add(separatorElement);
-		}
-		else {
+		} else {
+			// 否则从第一个字符开始处理
 			begin = 0;
 		}
+
+		// 循环处理路径字符串
 		while (begin < path.length()) {
+			// 查找下一个分隔符的位置
 			int end = path.indexOf(separator, begin);
+
+			// 提取当前段的字符串
 			String segment = (end != -1 ? path.substring(begin, end) : path.substring(begin));
+
+			// 如果段字符串不为空，则根据选项判断是否需要解码和解析段
 			if (!segment.isEmpty()) {
 				elements.add(options.shouldDecodeAndParseSegments() ?
 						decodeAndParsePathSegment(segment) :
 						DefaultPathSegment.from(segment, separatorElement));
 			}
+
+			// 如果已经到达路径末尾，则结束循环
 			if (end == -1) {
 				break;
 			}
+
+			// 添加分隔符元素到元素列表中
 			elements.add(separatorElement);
+
+			// 更新起始位置为下一个段的起始位置
 			begin = end + 1;
 		}
+
+		// 返回一个新的默认路径容器，包含原始路径字符串和处理后的元素列表
 		return new DefaultPathContainer(path, elements);
 	}
 
 	private static PathSegment decodeAndParsePathSegment(String segment) {
+		// 指定字符集为 UTF-8
 		Charset charset = StandardCharsets.UTF_8;
+
+		// 查找路径段中的分号位置
 		int index = segment.indexOf(';');
+
+		// 如果没有找到分号，则说明路径段不包含参数
 		if (index == -1) {
+			// 对路径段进行 URI 解码，并作为匹配值创建默认路径段对象
 			String valueToMatch = StringUtils.uriDecode(segment, charset);
 			return DefaultPathSegment.from(segment, valueToMatch);
-		}
-		else {
+		} else {
+			// 如果找到分号，则分割路径段为值和参数部分
+			// 解码分号前的部分作为匹配值
 			String valueToMatch = StringUtils.uriDecode(segment.substring(0, index), charset);
+			// 获取分号后的参数部分
 			String pathParameterContent = segment.substring(index);
+			// 解析路径参数
 			MultiValueMap<String, String> parameters = parsePathParams(pathParameterContent, charset);
+			// 创建默认路径段对象，包括原始路径段、匹配值和参数
 			return DefaultPathSegment.from(segment, valueToMatch, parameters);
 		}
 	}
 
 	private static MultiValueMap<String, String> parsePathParams(String input, Charset charset) {
+		// 创建一个空的 MultiValueMap，用于存储解析后的参数和对应的值列表
 		MultiValueMap<String, String> result = new LinkedMultiValueMap<>();
+
+		// 初始化起始位置
 		int begin = 1;
+
+		// 循环处理输入字符串
 		while (begin < input.length()) {
+			// 查找下一个分号 ';' 的位置
 			int end = input.indexOf(';', begin);
+
+			// 提取当前参数字符串
 			String param = (end != -1 ? input.substring(begin, end) : input.substring(begin));
+
+			// 解析当前参数字符串的值，并将解析结果存储到 结果列表 中
 			parsePathParamValues(param, charset, result);
+
+			// 如果已经到达字符串末尾，则结束循环
 			if (end == -1) {
 				break;
 			}
+
+			// 更新起始位置为下一个参数的起始位置
 			begin = end + 1;
 		}
+
+		// 返回解析后的结果映射
 		return result;
 	}
 
 	private static void parsePathParamValues(String input, Charset charset, MultiValueMap<String, String> output) {
+		// 如果输入字符串不为空或不为空白
 		if (StringUtils.hasText(input)) {
+			// 查找等号 '=' 的位置
 			int index = input.indexOf('=');
+			// 如果找到等号
 			if (index != -1) {
+				// 提取参数名部分
 				String name = input.substring(0, index);
+				// 对参数名进行 URI 解码
 				name = StringUtils.uriDecode(name, charset);
+				// 如果解码后的参数名不为空或不为空白
 				if (StringUtils.hasText(name)) {
+					// 提取参数值部分
 					String value = input.substring(index + 1);
+					// 将参数值按逗号分隔，并对每个值进行 URI 解码
 					for (String v : StringUtils.commaDelimitedListToStringArray(value)) {
+						// 添加到输出对象中
 						output.add(name, StringUtils.uriDecode(v, charset));
 					}
 				}
-			}
-			else {
+			} else {
+				// 如果未找到等号，则整个输入字符串作为参数名，对其进行 URI 解码
 				String name = StringUtils.uriDecode(input, charset);
 				if (StringUtils.hasText(name)) {
+					// 如果解码后的名称不为空，将空字符串作为值添加到输出对象中
 					output.add(input, "");
 				}
 			}
@@ -183,10 +252,15 @@ final class DefaultPathContainer implements PathContainer {
 	}
 
 	static PathContainer subPath(PathContainer container, int fromIndex, int toIndex) {
+		// 获取路径容器中的所有元素列表
 		List<Element> elements = container.elements();
+
+		// 如果提取的子路径恰好是整个路径容器的范围，则直接返回原路径容器
 		if (fromIndex == 0 && toIndex == elements.size()) {
 			return container;
 		}
+
+		// 如果 开始索引 等于 结束索引，则返回表示空路径的路径容器
 		if (fromIndex == toIndex) {
 			return EMPTY_PATH;
 		}
@@ -195,16 +269,26 @@ final class DefaultPathContainer implements PathContainer {
 		Assert.isTrue(toIndex >= 0 && toIndex <= elements.size(), () -> "Invalid toIndex: " + toIndex);
 		Assert.isTrue(fromIndex < toIndex, () -> "fromIndex: " + fromIndex + " should be < toIndex " + toIndex);
 
+		// 提取子列表，包括 开始索引 到 结束索引 之间的元素
 		List<Element> subList = elements.subList(fromIndex, toIndex);
+
+		// 将子列表中的元素值拼接成一个新的路径字符串
 		String path = subList.stream().map(Element::value).collect(Collectors.joining(""));
+
+		// 返回一个新的默认路径容器，包含提取的子路径字符串和子列表元素
 		return new DefaultPathContainer(path, subList);
 	}
 
 
 	private static class DefaultSeparator implements Separator {
-
+		/**
+		 * 分隔符
+		 */
 		private final String separator;
 
+		/**
+		 * 编码序列
+		 */
 		private final String encodedSequence;
 
 
@@ -226,18 +310,33 @@ final class DefaultPathContainer implements PathContainer {
 
 
 	private static final class DefaultPathSegment implements PathSegment {
-
+		/**
+		 * 空的参数值
+		 */
 		private static final MultiValueMap<String, String> EMPTY_PARAMS =
 				CollectionUtils.unmodifiableMultiValueMap(new LinkedMultiValueMap<>());
 
+		/**
+		 * 值
+		 */
 		private final String value;
 
+		/**
+		 * 要匹配的值
+		 */
 		private final String valueToMatch;
 
+		/**
+		 * 参数映射
+		 */
 		private final MultiValueMap<String, String> parameters;
 
 		/**
-		 * Factory for segments without decoding and parsing.
+		 * 创建一个不需要解码和解析的路径段。
+		 *
+		 * @param value     路径段的原始值
+		 * @param separator 分隔符
+		 * @return 创建的路径段对象
 		 */
 		static DefaultPathSegment from(String value, DefaultSeparator separator) {
 			String valueToMatch = value.contains(separator.encodedSequence()) ?
@@ -246,14 +345,23 @@ final class DefaultPathContainer implements PathContainer {
 		}
 
 		/**
-		 * Factory for decoded and parsed segments.
+		 * 创建一个已解码和解析的路径段。
+		 *
+		 * @param value        路径段的原始值
+		 * @param valueToMatch 匹配值
+		 * @return 创建的路径段对象
 		 */
 		static DefaultPathSegment from(String value, String valueToMatch) {
 			return new DefaultPathSegment(value, valueToMatch, EMPTY_PARAMS);
 		}
 
 		/**
-		 * Factory for decoded and parsed segments.
+		 * 创建一个已解码和解析的路径段，并附带路径参数。
+		 *
+		 * @param value        路径段的原始值
+		 * @param valueToMatch 匹配值
+		 * @param params       路径段的参数
+		 * @return 创建的路径段对象
 		 */
 		static DefaultPathSegment from(String value, String valueToMatch, MultiValueMap<String, String> params) {
 			return new DefaultPathSegment(value, valueToMatch, CollectionUtils.unmodifiableMultiValueMap(params));
--- spring-web/src/main/java/org/springframework/http/server/PathContainer.java
+++ spring-web/src/main/java/org/springframework/http/server/PathContainer.java
@@ -16,18 +16,15 @@
 
 package org.springframework.http.server;
 
-import java.util.List;
-
 import org.springframework.util.MultiValueMap;
 
+import java.util.List;
+
 /**
- * Structured representation of a URI path parsed via {@link #parsePath(String)}
- * into a sequence of {@link Separator} and {@link PathSegment} elements.
+ * URI路径的结构化表示形式，通过 {@link #parsePath(String)} 解析为 {@link Separator} 和 {@link PathSegment} 元素的序列。
  *
- * <p>Each {@link PathSegment} exposes its content in decoded form and with path
- * parameters removed. This makes it safe to match one path segment at a time
- * without the risk of decoded reserved characters altering the structure of
- * the path.
+ * <p>每个 {@link PathSegment} 都以解码形式公开其内容，并删除了路径参数。
+ * 这样可以安全地逐个匹配路径段，而不会因解码的保留字符而改变路径的结构。
  *
  * @author Rossen Stoyanchev
  * @since 5.0
@@ -35,30 +32,35 @@ import org.springframework.util.MultiValueMap;
 public interface PathContainer {
 
 	/**
-	 * The original path from which this instance was parsed.
+	 * 此实例解析的原始路径。
+	 *
+	 * @return 原始路径字符串
 	 */
 	String value();
 
 	/**
-	 * The contained path elements, either {@link Separator} or {@link PathSegment}.
+	 * 包含的路径元素，可以是 {@link Separator} 或 {@link PathSegment}。
+	 *
+	 * @return 路径元素列表
 	 */
 	List<Element> elements();
 
 	/**
-	 * Extract a sub-path from the given offset into the elements list.
-	 * @param index the start element index (inclusive)
-	 * @return the sub-path
+	 * 从给定的偏移量提取子路径到元素列表中。
+	 *
+	 * @param index 开始元素的索引（包括）
+	 * @return 子路径
 	 */
 	default PathContainer subPath(int index) {
 		return subPath(index, elements().size());
 	}
 
 	/**
-	 * Extract a sub-path from the given start offset into the element list
-	 * (inclusive) and to the end offset (exclusive).
-	 * @param startIndex the start element index (inclusive)
-	 * @param endIndex the end element index (exclusive)
-	 * @return the sub-path
+	 * 从给定的起始偏移量提取子路径到元素列表中（包括起始偏移量），直到结束偏移量（不包括）。
+	 *
+	 * @param startIndex 开始元素的索引（包括）
+	 * @param endIndex   结束元素的索引（不包括）
+	 * @return 子路径
 	 */
 	default PathContainer subPath(int startIndex, int endIndex) {
 		return DefaultPathContainer.subPath(this, startIndex, endIndex);
@@ -66,21 +68,21 @@ public interface PathContainer {
 
 
 	/**
-	 * Parse the path value into a sequence of {@code "/"} {@link Separator Separator}
-	 * and {@link PathSegment PathSegment} elements.
-	 * @param path the encoded, raw path value to parse
-	 * @return the parsed path
+	 * 将路径值解析为 {@code "/"} {@link Separator Separator} 和 {@link PathSegment PathSegment} 元素序列。
+	 *
+	 * @param path 要解析的编码、原始路径值
+	 * @return 解析后的路径
 	 */
 	static PathContainer parsePath(String path) {
 		return DefaultPathContainer.createFromUrlPath(path, Options.HTTP_PATH);
 	}
 
 	/**
-	 * Parse the path value into a sequence of {@link Separator Separator} and
-	 * {@link PathSegment PathSegment} elements.
-	 * @param path the encoded, raw path value to parse
-	 * @param options to customize parsing
-	 * @return the parsed path
+	 * 将路径值解析为 {@link Separator Separator} 和 {@link PathSegment PathSegment} 元素序列。
+	 *
+	 * @param path    要解析的编码、原始路径值
+	 * @param options 自定义解析选项
+	 * @return 解析后的路径
 	 * @since 5.2
 	 */
 	static PathContainer parsePath(String path, Options options) {
@@ -89,69 +91,81 @@ public interface PathContainer {
 
 
 	/**
-	 * A path element, either separator or path segment.
+	 * 表示路径元素，可以是分隔符或路径段。
 	 */
 	interface Element {
 
 		/**
-		 * The unmodified, original value of this element.
+		 * 获取此元素的未修改的原始值。
+		 *
+		 * @return 元素的原始值
 		 */
 		String value();
 	}
 
 
 	/**
-	 * Path separator element.
+	 * 路径分隔符元素。
 	 */
 	interface Separator extends Element {
 	}
 
 
 	/**
-	 * Path segment element.
+	 * 路径段元素。
 	 */
 	interface PathSegment extends Element {
 
 		/**
-		 * Return the path segment value, decoded and sanitized, for path matching.
+		 * 返回路径段的值，用于路径匹配时解码和清理。
+		 *
+		 * @return 路径段的值
 		 */
 		String valueToMatch();
 
 		/**
-		 * Expose {@link #valueToMatch()} as a character array.
+		 * 将 {@link #valueToMatch()} 以字符数组形式公开。
+		 *
+		 * @return 字符数组形式的路径段值
 		 */
 		char[] valueToMatchAsChars();
 
 		/**
-		 * Path parameters associated with this path segment.
-		 * @return an unmodifiable map containing the parameters
+		 * 与此路径段关联的路径参数。
+		 *
+		 * @return 包含参数的不可修改的映射
 		 */
 		MultiValueMap<String, String> parameters();
 	}
 
 
 	/**
-	 * Options to customize parsing based on the type of input path.
+	 * 用于根据输入路径类型自定义解析选项的选项。
+	 *
 	 * @since 5.2
 	 */
-	class Options {
+	public class Options {
 
 		/**
-		 * Options for HTTP URL paths.
-		 * <p>Separator '/' with URL decoding and parsing of path parameters.
+		 * HTTP URL路径的选项。
+		 * <p>使用分隔符 '/' 进行URL解码，并解析路径参数。</p>
 		 */
 		public final static Options HTTP_PATH = Options.create('/', true);
 
 		/**
-		 * Options for a message route.
-		 * <p>Separator '.' with neither URL decoding nor parsing of path parameters.
-		 * Escape sequences for the separator character in segment values are still
-		 * decoded.
+		 * 消息路由的选项。
+		 * <p>使用分隔符 '.'，既不进行URL解码，也不解析路径参数。
+		 * 在段值中，分隔符字符的转义序列仍然会被解码。</p>
 		 */
 		public final static Options MESSAGE_ROUTE = Options.create('.', false);
-
+		/**
+		 * 分割符
+		 */
 		private final char separator;
 
+		/**
+		 * 是否应对路径段值进行URL解码和路径参数解析
+		 */
 		private final boolean decodeAndParseSegments;
 
 		private Options(char separator, boolean decodeAndParseSegments) {
@@ -159,21 +173,31 @@ public interface PathContainer {
 			this.decodeAndParseSegments = decodeAndParseSegments;
 		}
 
+		/**
+		 * 返回用于解析路径成段的分隔符。
+		 *
+		 * @return 解析路径成段的分隔符
+		 */
 		public char separator() {
 			return this.separator;
 		}
 
+		/**
+		 * 返回是否应对路径段值进行URL解码和路径参数解析。
+		 *
+		 * @return 如果应解码和解析路径段，则返回 true；否则返回 false。
+		 */
 		public boolean shouldDecodeAndParseSegments() {
 			return this.decodeAndParseSegments;
 		}
 
 		/**
-		 * Create an {@link Options} instance with the given settings.
-		 * @param separator the separator for parsing the path into segments;
-		 * currently this must be slash or dot.
-		 * @param decodeAndParseSegments whether to URL decode path segment
-		 * values and parse path parameters. If set to false, only escape
-		 * sequences for the separator char are decoded.
+		 * 使用给定的设置创建一个 {@link Options} 实例。
+		 *
+		 * @param separator              解析路径成段所用的分隔符；目前必须是斜杠 '/' 或点 '.'
+		 * @param decodeAndParseSegments 是否对路径段值进行URL解码和路径参数解析。
+		 *                               如果设置为 false，则仅解码段值中分隔符字符的转义序列。
+		 * @return 包含给定设置的 {@link Options} 实例
 		 */
 		public static Options create(char separator, boolean decodeAndParseSegments) {
 			return new Options(separator, decodeAndParseSegments);
