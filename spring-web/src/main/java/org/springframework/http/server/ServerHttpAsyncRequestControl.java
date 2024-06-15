ca7cf091b3cf19a324e0d3afd9a6001c3500dcf9
LMW <CNlmw0726@gmail.com>
2024-06-15 21:51:53 +0800

    docs: 完成 ServerHttpAsyncRequestControl及其子类 的代码注释

--- spring-web/src/main/java/org/springframework/http/server/ServerHttpAsyncRequestControl.java
+++ spring-web/src/main/java/org/springframework/http/server/ServerHttpAsyncRequestControl.java
@@ -17,8 +17,7 @@
 package org.springframework.http.server;
 
 /**
- * A control that can put the processing of an HTTP request in asynchronous mode during
- * which the response remains open until explicitly closed.
+ * 控制可以将HTTP请求的处理放入异步模式，期间响应保持打开状态，直到显式关闭。
  *
  * @author Rossen Stoyanchev
  * @since 4.0
@@ -26,31 +25,33 @@ package org.springframework.http.server;
 public interface ServerHttpAsyncRequestControl {
 
 	/**
-	 * Enable asynchronous processing after which the response remains open until a call
-	 * to {@link #complete()} is made or the server times out the request. Once enabled,
-	 * additional calls to this method are ignored.
+	 * 启用异步处理，此后响应保持打开状态，直到调用 {@link #complete()} 方法或服务器超时请求。一旦启用，额外调用此方法将被忽略。
 	 */
 	void start();
 
 	/**
-	 * A variation on {@link #start()} that allows specifying a timeout value to use to
-	 * use for asynchronous processing. If {@link #complete()} is not called within the
-	 * specified value, the request times out.
+	 * 启用异步处理的变体，允许指定超时值用于异步处理。如果在指定值内未调用 {@link #complete()}，则请求超时。
+	 *
+	 * @param timeout 超时时间（毫秒）
 	 */
 	void start(long timeout);
 
 	/**
-	 * Return whether asynchronous request processing has been started.
+	 * 返回是否已启动异步请求处理。
+	 *
+	 * @return 如果已启动异步请求处理，则返回 true；否则返回 false。
 	 */
 	boolean isStarted();
 
 	/**
-	 * Mark asynchronous request processing as completed.
+	 * 标记异步请求处理已完成。
 	 */
 	void complete();
 
 	/**
-	 * Return whether asynchronous request processing has been completed.
+	 * 返回是否已完成异步请求处理。
+	 *
+	 * @return 如果已完成异步请求处理，则返回 true；否则返回 false。
 	 */
 	boolean isCompleted();
 
--- spring-web/src/main/java/org/springframework/http/server/ServletServerHttpAsyncRequestControl.java
+++ spring-web/src/main/java/org/springframework/http/server/ServletServerHttpAsyncRequestControl.java
@@ -16,43 +16,54 @@
 
 package org.springframework.http.server;
 
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.AsyncEvent;
 import javax.servlet.AsyncListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.lang.Nullable;
-import org.springframework.util.Assert;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * A {@link ServerHttpAsyncRequestControl} to use on Servlet containers (Servlet 3.0+).
+ * 用于Servlet容器（Servlet 3.0+）的 {@link ServerHttpAsyncRequestControl} 实现。
  *
  * @author Rossen Stoyanchev
  * @since 4.0
  */
 public class ServletServerHttpAsyncRequestControl implements ServerHttpAsyncRequestControl, AsyncListener {
-
+	/**
+	 * 没有超时的值
+	 */
 	private static final long NO_TIMEOUT_VALUE = Long.MIN_VALUE;
 
-
+	/**
+	 * Servlet服务器Http请求
+	 */
 	private final ServletServerHttpRequest request;
 
+	/**
+	 * Servlet服务器Http响应
+	 */
 	private final ServletServerHttpResponse response;
 
+	/**
+	 * 异步上下文
+	 */
 	@Nullable
 	private AsyncContext asyncContext;
 
+	/**
+	 * 异步请求是否完成
+	 */
 	private AtomicBoolean asyncCompleted = new AtomicBoolean();
 
 
 	/**
-	 * Constructor accepting a request and response pair that are expected to be of type
-	 * {@link ServletServerHttpRequest} and {@link ServletServerHttpResponse}
-	 * respectively.
+	 * 构造函数接受一个期望为 {@link ServletServerHttpRequest} 和
+	 * {@link ServletServerHttpResponse} 类型的请求和响应对。
 	 */
 	public ServletServerHttpAsyncRequestControl(ServletServerHttpRequest request, ServletServerHttpResponse response) {
 		Assert.notNull(request, "request is required");
@@ -60,9 +71,9 @@ public class ServletServerHttpAsyncRequestControl implements ServerHttpAsyncRequ
 
 		Assert.isTrue(request.getServletRequest().isAsyncSupported(),
 				"Async support must be enabled on a servlet and for all filters involved " +
-				"in async request processing. This is done in Java code using the Servlet API " +
-				"or by adding \"<async-supported>true</async-supported>\" to servlet and " +
-				"filter declarations in web.xml. Also you must use a Servlet 3.0+ container");
+						"in async request processing. This is done in Java code using the Servlet API " +
+						"or by adding \"<async-supported>true</async-supported>\" to servlet and " +
+						"filter declarations in web.xml. Also you must use a Servlet 3.0+ container");
 
 		this.request = request;
 		this.response = response;
@@ -87,31 +98,40 @@ public class ServletServerHttpAsyncRequestControl implements ServerHttpAsyncRequ
 	@Override
 	public void start(long timeout) {
 		Assert.state(!isCompleted(), "Async processing has already completed");
+		// 如果异步上下文已经启动，则直接返回，避免重复启动
 		if (isStarted()) {
 			return;
 		}
 
+		// 从请求处理器中获取原始的 Servlet 请求和响应对象
 		HttpServletRequest servletRequest = this.request.getServletRequest();
 		HttpServletResponse servletResponse = this.response.getServletResponse();
 
+		// 使用原始 Servlet 请求和响应对象启动异步上下文
 		this.asyncContext = servletRequest.startAsync(servletRequest, servletResponse);
+
+		// 向异步上下文添加监听器，通常是当前对象本身
 		this.asyncContext.addListener(this);
 
+		// 如果超时时间不是非超时时间值
 		if (timeout != NO_TIMEOUT_VALUE) {
+			// 设置异步上下文的超时时间
 			this.asyncContext.setTimeout(timeout);
 		}
 	}
 
 	@Override
 	public void complete() {
+		// 如果异步上下文不为 null，并且已经启动，并且尚未完成
 		if (this.asyncContext != null && isStarted() && !isCompleted()) {
+			// 完成异步上下文，标记异步处理完成
 			this.asyncContext.complete();
 		}
 	}
 
 
 	// ---------------------------------------------------------------------
-	// Implementation of AsyncListener methods
+	// AsyncListener方法的实现
 	// ---------------------------------------------------------------------
 
 	@Override
