832da80fc77a240c19965aaeba981e1f0c4d9382
LMW <CNlmw0726@gmail.com>
2024-06-17 23:45:19 +0800

    docs: 完成 代理工厂bean 的代码注释

--- spring-web/src/main/java/org/springframework/http/client/support/ProxyFactoryBean.java
+++ spring-web/src/main/java/org/springframework/http/client/support/ProxyFactoryBean.java
@@ -16,53 +16,70 @@
 
 package org.springframework.http.client.support;
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.SocketAddress;
-
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.SocketAddress;
+
 /**
- * {@link FactoryBean} that creates a {@link Proxy java.net.Proxy}.
+ * {@link FactoryBean}，用于创建 {@link Proxy java.net.Proxy}。
  *
  * @author Arjen Poutsma
- * @since 3.0.4
- * @see	FactoryBean
+ * @see FactoryBean
  * @see Proxy
+ * @since 3.0.4
  */
 public class ProxyFactoryBean implements FactoryBean<Proxy>, InitializingBean {
-
+	/**
+	 * 代理类型
+	 */
 	private Proxy.Type type = Proxy.Type.HTTP;
 
+	/**
+	 * 主机名称
+	 */
 	@Nullable
 	private String hostname;
 
+	/**
+	 * 端口号
+	 */
 	private int port = -1;
 
+	/**
+	 * 代理
+	 */
 	@Nullable
 	private Proxy proxy;
 
 
 	/**
-	 * Set the proxy type.
-	 * <p>Defaults to {@link java.net.Proxy.Type#HTTP}.
+	 * 设置代理类型。
+	 * <p>默认为 {@link java.net.Proxy.Type#HTTP}。
+	 *
+	 * @param type 代理类型
 	 */
 	public void setType(Proxy.Type type) {
 		this.type = type;
 	}
 
 	/**
-	 * Set the proxy host name.
+	 * 设置代理主机名。
+	 *
+	 * @param hostname 代理主机名
 	 */
 	public void setHostname(String hostname) {
 		this.hostname = hostname;
 	}
 
 	/**
-	 * Set the proxy port.
+	 * 设置代理端口。
+	 *
+	 * @param port 代理端口
 	 */
 	public void setPort(int port) {
 		this.port = port;
@@ -70,14 +87,18 @@ public class ProxyFactoryBean implements FactoryBean<Proxy>, InitializingBean {
 
 
 	@Override
-	public void afterPropertiesSet() throws IllegalArgumentException {
+	public void afterPropertiesSet() {
 		Assert.notNull(this.type, "Property 'type' is required");
 		Assert.notNull(this.hostname, "Property 'hostname' is required");
+		// 如果端口不在有效范围内
 		if (this.port < 0 || this.port > 65535) {
+			// 抛出参数异常
 			throw new IllegalArgumentException("Property 'port' value out of range: " + this.port);
 		}
 
+		// 创建 SocketAddress 对象，指定主机名和端口号
 		SocketAddress socketAddress = new InetSocketAddress(this.hostname, this.port);
+		// 创建 Proxy 对象，指定代理类型和 SocketAddress
 		this.proxy = new Proxy(this.type, socketAddress);
 	}
 
