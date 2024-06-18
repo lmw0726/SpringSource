df5818b9ba8773bddfa83941b1d1c4adba537c73
LMW <CNlmw0726@gmail.com>
2024-06-18 21:08:33 +0800

    docs: 完成 HttpAccessor体系 的代码注释

--- spring-web/src/main/java/org/springframework/http/client/support/HttpAccessor.java
+++ spring-web/src/main/java/org/springframework/http/client/support/HttpAccessor.java
@@ -16,13 +16,7 @@
 
 package org.springframework.http.client.support;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
-
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.http.HttpLogging;
 import org.springframework.http.HttpMethod;
@@ -32,38 +26,54 @@ import org.springframework.http.client.ClientHttpRequestInitializer;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.util.Assert;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Base class for {@link org.springframework.web.client.RestTemplate}
- * and other HTTP accessing gateway helpers, defining common properties
- * such as the {@link ClientHttpRequestFactory} to operate on.
+ * {@link org.springframework.web.client.RestTemplate}和其他HTTP访问网关助手的基类，定义了通用属性，
+ * 如操作所需的{@link ClientHttpRequestFactory}。
  *
- * <p>Not intended to be used directly.
+ * <p>不打算直接使用。
  *
- * <p>See {@link org.springframework.web.client.RestTemplate} for an entry point.
+ * <p>参见{@link org.springframework.web.client.RestTemplate}以了解入口点。
+ *
+ * <p><b>注意：</b> 标准的JDK HTTP库不支持HTTP PATCH方法。
+ * 配置Apache HttpComponents或OkHttp请求工厂以启用PATCH。
  *
  * @author Arjen Poutsma
  * @author Juergen Hoeller
  * @author Phillip Webb
- * @since 3.0
  * @see ClientHttpRequestFactory
  * @see org.springframework.web.client.RestTemplate
+ * @since 3.0
  */
 public abstract class HttpAccessor {
 
-	/** Logger available to subclasses. */
+	/**
+	 * 供子类使用的日志记录器。
+	 */
 	protected final Log logger = HttpLogging.forLogName(getClass());
 
+	/**
+	 * 客户端Http请求工厂
+	 */
 	private ClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
 
+	/**
+	 * 客户端Http请求初始化器列表
+	 */
 	private final List<ClientHttpRequestInitializer> clientHttpRequestInitializers = new ArrayList<>();
 
 
 	/**
-	 * Set the request factory that this accessor uses for obtaining client request handles.
-	 * <p>The default is a {@link SimpleClientHttpRequestFactory} based on the JDK's own
-	 * HTTP libraries ({@link java.net.HttpURLConnection}).
-	 * <p><b>Note that the standard JDK HTTP library does not support the HTTP PATCH method.
-	 * Configure the Apache HttpComponents or OkHttp request factory to enable PATCH.</b>
+	 * 设置此访问器用于获取客户端请求句柄的请求工厂。
+	 * <p>默认是基于JDK自带HTTP库（{@link java.net.HttpURLConnection}）的{@link SimpleClientHttpRequestFactory}。
+	 * <p><b>注意：标准的JDK HTTP库不支持HTTP PATCH方法。
+	 * 配置Apache HttpComponents或OkHttp请求工厂以启用PATCH。</b>
+	 *
+	 * @param requestFactory 用于获取客户端请求句柄的请求工厂
 	 * @see #createRequest(URI, HttpMethod)
 	 * @see SimpleClientHttpRequestFactory
 	 * @see org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory
@@ -75,7 +85,7 @@ public abstract class HttpAccessor {
 	}
 
 	/**
-	 * Return the request factory that this accessor uses for obtaining client request handles.
+	 * 返回此访问器用于获取客户端请求句柄的请求工厂。
 	 */
 	public ClientHttpRequestFactory getRequestFactory() {
 		return this.requestFactory;
@@ -83,49 +93,57 @@ public abstract class HttpAccessor {
 
 
 	/**
-	 * Set the request initializers that this accessor should use.
-	 * <p>The initializers will get immediately sorted according to their
-	 * {@linkplain AnnotationAwareOrderComparator#sort(List) order}.
+	 * 设置此访问器应使用的请求初始化器。
+	 * <p>初始化器将立即按照其{@linkplain AnnotationAwareOrderComparator#sort(List)顺序}进行排序。
+	 *
+	 * @param clientHttpRequestInitializers 要使用的初始化器列表
 	 * @since 5.2
 	 */
-	public void setClientHttpRequestInitializers(
-			List<ClientHttpRequestInitializer> clientHttpRequestInitializers) {
-
+	public void setClientHttpRequestInitializers(List<ClientHttpRequestInitializer> clientHttpRequestInitializers) {
+		// 如果当前的 客户端Http请求初始化器列表 与传入的 初始化器列表 不同
 		if (this.clientHttpRequestInitializers != clientHttpRequestInitializers) {
+			// 清空当前的 客户端Http请求初始化器列表
 			this.clientHttpRequestInitializers.clear();
+			// 将 初始化器列表 全部添加到当前的 客户端Http请求初始化器列表 中
 			this.clientHttpRequestInitializers.addAll(clientHttpRequestInitializers);
+			// 对当前的 客户端Http请求初始化器列表 进行排序
 			AnnotationAwareOrderComparator.sort(this.clientHttpRequestInitializers);
 		}
 	}
 
 	/**
-	 * Get the request initializers that this accessor uses.
-	 * <p>The returned {@link List} is active and may be modified. Note,
-	 * however, that the initializers will not be resorted according to their
-	 * {@linkplain AnnotationAwareOrderComparator#sort(List) order} before the
-	 * {@link ClientHttpRequest} is initialized.
-	 * @since 5.2
+	 * 获取此访问器使用的请求初始化器。
+	 * <p>返回的{@link List}是活动的，可以进行修改。但是，请注意，在{@link ClientHttpRequest}初始化之前，
+	 * 初始化器不会按照其{@linkplain AnnotationAwareOrderComparator#sort(List)顺序}进行重新排序。
+	 *
 	 * @see #setClientHttpRequestInitializers(List)
+	 * @since 5.2
 	 */
 	public List<ClientHttpRequestInitializer> getClientHttpRequestInitializers() {
 		return this.clientHttpRequestInitializers;
 	}
 
 	/**
-	 * Create a new {@link ClientHttpRequest} via this template's {@link ClientHttpRequestFactory}.
-	 * @param url the URL to connect to
-	 * @param method the HTTP method to execute (GET, POST, etc)
-	 * @return the created request
-	 * @throws IOException in case of I/O errors
+	 * 通过此模板的{@link ClientHttpRequestFactory}创建一个新的{@link ClientHttpRequest}。
+	 *
+	 * @param url    要连接的URL
+	 * @param method 要执行的HTTP方法（GET、POST等）
+	 * @return 创建的请求
+	 * @throws IOException 如果发生I/O错误
 	 * @see #getRequestFactory()
 	 * @see ClientHttpRequestFactory#createRequest(URI, HttpMethod)
 	 */
 	protected ClientHttpRequest createRequest(URI url, HttpMethod method) throws IOException {
+		// 使用请求工厂创建 客户端Http请求 对象
 		ClientHttpRequest request = getRequestFactory().createRequest(url, method);
+		// 初始化请求
 		initialize(request);
+		// 如果日志记录器处于调试级别
 		if (logger.isDebugEnabled()) {
+			// 记录调试信息，包括 HTTP 方法和 URL
 			logger.debug("HTTP " + method.name() + " " + url);
 		}
+		// 返回创建的请求对象
 		return request;
 	}
 
--- spring-web/src/main/java/org/springframework/http/client/support/InterceptingHttpAccessor.java
+++ spring-web/src/main/java/org/springframework/http/client/support/InterceptingHttpAccessor.java
@@ -16,9 +16,6 @@
 
 package org.springframework.http.client.support;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -27,52 +24,64 @@ import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Base class for {@link org.springframework.web.client.RestTemplate}
- * and other HTTP accessing gateway helpers, adding interceptor-related
- * properties to {@link HttpAccessor}'s common properties.
+ * {@link org.springframework.web.client.RestTemplate}和其他HTTP访问网关助手的基类，
+ * 在{@link HttpAccessor}的通用属性中添加了拦截器相关的属性。
  *
- * <p>Not intended to be used directly.
- * See {@link org.springframework.web.client.RestTemplate} for an entry point.
+ * <p>不打算直接使用。
+ * 参见{@link org.springframework.web.client.RestTemplate}以了解入口点。
  *
  * @author Arjen Poutsma
  * @author Juergen Hoeller
- * @since 3.0
  * @see ClientHttpRequestInterceptor
  * @see InterceptingClientHttpRequestFactory
  * @see org.springframework.web.client.RestTemplate
+ * @since 3.0
  */
 public abstract class InterceptingHttpAccessor extends HttpAccessor {
-
+	/**
+	 * 客户端Http请求拦截器列表。
+	 */
 	private final List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
 
+	/**
+	 * 拦截请求工厂
+	 */
 	@Nullable
 	private volatile ClientHttpRequestFactory interceptingRequestFactory;
 
 
 	/**
-	 * Set the request interceptors that this accessor should use.
-	 * <p>The interceptors will get immediately sorted according to their
-	 * {@linkplain AnnotationAwareOrderComparator#sort(List) order}.
+	 * 设置此访问器应使用的请求拦截器。
+	 * <p>拦截器将立即按照其{@linkplain AnnotationAwareOrderComparator#sort(List)顺序}进行排序。
+	 *
+	 * @param interceptors 要使用的拦截器列表
 	 * @see #getRequestFactory()
 	 * @see AnnotationAwareOrderComparator
 	 */
 	public void setInterceptors(List<ClientHttpRequestInterceptor> interceptors) {
 		Assert.noNullElements(interceptors, "'interceptors' must not contain null elements");
-		// Take getInterceptors() List as-is when passed in here
+		// 传入时直接采用getInterceptors()列表
+		// 如果当前的拦截器列表与传入的拦截器列表不同
 		if (this.interceptors != interceptors) {
+			// 清空当前的拦截器列表
 			this.interceptors.clear();
+			// 将传入的拦截器列表全部添加到当前的拦截器列表中
 			this.interceptors.addAll(interceptors);
+			// 对当前的拦截器列表进行排序
 			AnnotationAwareOrderComparator.sort(this.interceptors);
 		}
 	}
 
 	/**
-	 * Get the request interceptors that this accessor uses.
-	 * <p>The returned {@link List} is active and may be modified. Note,
-	 * however, that the interceptors will not be resorted according to their
-	 * {@linkplain AnnotationAwareOrderComparator#sort(List) order} before the
-	 * {@link ClientHttpRequestFactory} is built.
+	 * 获取此访问器使用的请求拦截器。
+	 * <p>返回的{@link List}是活动的，可以进行修改。但是，请注意，在构建
+	 * {@link ClientHttpRequestFactory}之前，拦截器不会按照其{@linkplain AnnotationAwareOrderComparator#sort(List)顺序}重新排序。
+	 *
+	 * @return 请求拦截器列表
 	 */
 	public List<ClientHttpRequestInterceptor> getInterceptors() {
 		return this.interceptors;
@@ -88,22 +97,28 @@ public abstract class InterceptingHttpAccessor extends HttpAccessor {
 	}
 
 	/**
-	 * Overridden to expose an {@link InterceptingClientHttpRequestFactory}
-	 * if necessary.
+	 * 如果需要，重写以暴露{@link InterceptingClientHttpRequestFactory}。
+	 *
 	 * @see #getInterceptors()
 	 */
 	@Override
 	public ClientHttpRequestFactory getRequestFactory() {
+		// 获取拦截器列表
 		List<ClientHttpRequestInterceptor> interceptors = getInterceptors();
+		// 如果拦截器列表不为空
 		if (!CollectionUtils.isEmpty(interceptors)) {
+			// 获取当前的拦截请求工厂
 			ClientHttpRequestFactory factory = this.interceptingRequestFactory;
+			// 如果拦截请求工厂为空
 			if (factory == null) {
+				// 创建新的 拦截客户端Http请求工厂
 				factory = new InterceptingClientHttpRequestFactory(super.getRequestFactory(), interceptors);
 				this.interceptingRequestFactory = factory;
 			}
+			// 返回拦截请求工厂
 			return factory;
-		}
-		else {
+		} else {
+			// 如果拦截器列表为空，返回父类的请求工厂
 			return super.getRequestFactory();
 		}
 	}
