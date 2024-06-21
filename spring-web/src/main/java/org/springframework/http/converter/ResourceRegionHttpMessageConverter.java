0a2fae91f9edb75b9c15fd24900c8b668f2c8cca
LMW <CNlmw0726@gmail.com>
2024-06-21 21:02:51 +0800

    docs: 完成 所有转换器 的代码注释

--- spring-web/src/main/java/org/springframework/http/converter/ResourceRegionHttpMessageConverter.java
+++ spring-web/src/main/java/org/springframework/http/converter/ResourceRegionHttpMessageConverter.java
@@ -16,6 +16,14 @@
 
 package org.springframework.http.converter;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.*;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.util.StreamUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -24,21 +32,8 @@ import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.ResourceRegion;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpOutputMessage;
-import org.springframework.http.MediaType;
-import org.springframework.http.MediaTypeFactory;
-import org.springframework.lang.Nullable;
-import org.springframework.util.Assert;
-import org.springframework.util.MimeTypeUtils;
-import org.springframework.util.StreamUtils;
-
 /**
- * Implementation of {@link HttpMessageConverter} that can write a single {@link ResourceRegion},
- * or Collections of {@link ResourceRegion ResourceRegions}.
+ * {@link HttpMessageConverter} 的实现类，可以写入单个 {@link ResourceRegion} 或 {@link ResourceRegion} 集合。
  *
  * @author Brian Clozel
  * @author Juergen Hoeller
@@ -55,15 +50,20 @@ public class ResourceRegionHttpMessageConverter extends AbstractGenericHttpMessa
 	@SuppressWarnings("unchecked")
 	protected MediaType getDefaultContentType(Object object) {
 		Resource resource = null;
+
+		// 如果对象是ResourceRegion类型，获取其内部的资源
 		if (object instanceof ResourceRegion) {
 			resource = ((ResourceRegion) object).getResource();
-		}
-		else {
+		} else {
+			// 将对象转换为ResourceRegion的集合
 			Collection<ResourceRegion> regions = (Collection<ResourceRegion>) object;
+			// 如果集合不为空，获取集合中第一个ResourceRegion的资源
 			if (!regions.isEmpty()) {
 				resource = regions.iterator().next().getResource();
 			}
 		}
+
+		// 根据资源获取媒体类型，如果无法确定媒体类型，返回默认的 application_octet_stream 类型
 		return MediaTypeFactory.getMediaType(resource).orElse(MediaType.APPLICATION_OCTET_STREAM);
 	}
 
@@ -98,27 +98,45 @@ public class ResourceRegionHttpMessageConverter extends AbstractGenericHttpMessa
 
 	@Override
 	public boolean canWrite(@Nullable Type type, @Nullable Class<?> clazz, @Nullable MediaType mediaType) {
+		// 如果 类型 不是ParameterizedType类型
 		if (!(type instanceof ParameterizedType)) {
+			// 检查 类型 是否是Class类型并且是否可以赋值给ResourceRegion类
 			return (type instanceof Class && ResourceRegion.class.isAssignableFrom((Class<?>) type));
 		}
 
+		// 将 类型 转换为ParameterizedType
 		ParameterizedType parameterizedType = (ParameterizedType) type;
+
+		// 如果 参数化类型 的原始类型不是Class类型，返回false
 		if (!(parameterizedType.getRawType() instanceof Class)) {
 			return false;
 		}
+
+		// 获取原始类型的Class对象
 		Class<?> rawType = (Class<?>) parameterizedType.getRawType();
+
+		// 如果 原始类型 不是集合类型，返回false
 		if (!(Collection.class.isAssignableFrom(rawType))) {
 			return false;
 		}
+
+		// 如果 参数化类型 的实际类型参数数量不是1，返回false
 		if (parameterizedType.getActualTypeArguments().length != 1) {
 			return false;
 		}
+
+		// 获取第一个实际类型参数
 		Type typeArgument = parameterizedType.getActualTypeArguments()[0];
+
+		// 如果 类型参数 不是Class类型，返回false
 		if (!(typeArgument instanceof Class)) {
 			return false;
 		}
 
+		// 将 类型参数 转换为Class类型
 		Class<?> typeArgumentClass = (Class<?>) typeArgument;
+
+		// 检查 类型参数类 是否是ResourceRegion类或者它的子类
 		return ResourceRegion.class.isAssignableFrom(typeArgumentClass);
 	}
 
@@ -127,15 +145,19 @@ public class ResourceRegionHttpMessageConverter extends AbstractGenericHttpMessa
 	protected void writeInternal(Object object, @Nullable Type type, HttpOutputMessage outputMessage)
 			throws IOException, HttpMessageNotWritableException {
 
+		// 如果对象是ResourceRegion类型
 		if (object instanceof ResourceRegion) {
+			// 写入单个ResourceRegion到输出消息
 			writeResourceRegion((ResourceRegion) object, outputMessage);
-		}
-		else {
+		} else {
+			// 将对象转换为ResourceRegion的集合
 			Collection<ResourceRegion> regions = (Collection<ResourceRegion>) object;
+			// 如果集合大小为1
 			if (regions.size() == 1) {
+				// 写入集合中的第一个ResourceRegion到输出消息
 				writeResourceRegion(regions.iterator().next(), outputMessage);
-			}
-			else {
+			} else {
+				// 写入ResourceRegion集合到输出消息
 				writeResourceRegionCollection((Collection<ResourceRegion>) object, outputMessage);
 			}
 		}
@@ -144,41 +166,64 @@ public class ResourceRegionHttpMessageConverter extends AbstractGenericHttpMessa
 
 	protected void writeResourceRegion(ResourceRegion region, HttpOutputMessage outputMessage) throws IOException {
 		Assert.notNull(region, "ResourceRegion must not be null");
+		// 获取输出消息的HttpHeaders
 		HttpHeaders responseHeaders = outputMessage.getHeaders();
 
+		// 获取资源区域的起始位置
 		long start = region.getPosition();
+
+		// 计算资源区域的结束位置
 		long end = start + region.getCount() - 1;
+
+		// 获取资源的总长度
 		long resourceLength = region.getResource().contentLength();
+
+		// 调整结束位置，确保不超过资源的总长度
 		end = Math.min(end, resourceLength - 1);
+
+		// 计算传输范围的长度
 		long rangeLength = end - start + 1;
+
+		// 添加Content-Range头信息
 		responseHeaders.add("Content-Range", "bytes " + start + '-' + end + '/' + resourceLength);
+
+		// 设置Content-Length头信息
 		responseHeaders.setContentLength(rangeLength);
 
+		// 获取资源的输入流
 		InputStream in = region.getResource().getInputStream();
-		// We cannot use try-with-resources here for the InputStream, since we have
-		// custom handling of the close() method in a finally-block.
+		// 我们不能在这里为InputStream使用try-with-resources，因为我们有
+		// 自定义处理finally块中的close() 方法。
 		try {
+			// 将指定范围的内容从输入流复制到输出消息的主体
 			StreamUtils.copyRange(in, outputMessage.getBody(), start, end);
-		}
-		finally {
+		} finally {
+			// 最后关闭输入流
 			try {
 				in.close();
-			}
-			catch (IOException ex) {
-				// ignore
+			} catch (IOException ex) {
+				// 忽略关闭异常
 			}
 		}
 	}
 
 	private void writeResourceRegionCollection(Collection<ResourceRegion> resourceRegions,
-			HttpOutputMessage outputMessage) throws IOException {
+											   HttpOutputMessage outputMessage) throws IOException {
 
 		Assert.notNull(resourceRegions, "Collection of ResourceRegion should not be null");
+		// 获取输出消息的HttpHeaders
 		HttpHeaders responseHeaders = outputMessage.getHeaders();
 
+		// 获取内容类型
 		MediaType contentType = responseHeaders.getContentType();
+
+		// 生成多部分边界字符串
 		String boundaryString = MimeTypeUtils.generateMultipartBoundaryString();
+
+		// 设置ContentType为multipart/byteranges，并包含边界字符串
 		responseHeaders.set(HttpHeaders.CONTENT_TYPE, "multipart/byteranges; boundary=" + boundaryString);
+
+		// 获取输出流
 		OutputStream out = outputMessage.getBody();
 
 		Resource resource = null;
@@ -186,49 +231,72 @@ public class ResourceRegionHttpMessageConverter extends AbstractGenericHttpMessa
 		long inputStreamPosition = 0;
 
 		try {
+			// 遍历资源区域
 			for (ResourceRegion region : resourceRegions) {
+				// 计算区域起始位置与当前输入流位置的差异
 				long start = region.getPosition() - inputStreamPosition;
+
+				// 如果起始位置小于0或资源不同，重新获取输入流
 				if (start < 0 || resource != region.getResource()) {
 					if (in != null) {
+						// 关闭当前输入流
 						in.close();
 					}
+					// 获取新资源
 					resource = region.getResource();
+					// 获取新输入流
 					in = resource.getInputStream();
+					// 重置输入流位置
 					inputStreamPosition = 0;
+					// 更新起始位置
 					start = region.getPosition();
 				}
+
+				// 计算区域结束位置
 				long end = start + region.getCount() - 1;
-				// Writing MIME header.
+
+				// 写入MIME头信息
 				println(out);
 				print(out, "--" + boundaryString);
 				println(out);
+
+				// 如果内容类型不为null，写入Content-Type头信息
 				if (contentType != null) {
 					print(out, "Content-Type: " + contentType);
 					println(out);
 				}
+
+				// 获取资源长度
 				long resourceLength = region.getResource().contentLength();
+
+				// 调整结束位置，确保不超过资源长度
 				end = Math.min(end, resourceLength - inputStreamPosition - 1);
+
+				// 写入Content-Range头信息
 				print(out, "Content-Range: bytes " +
 						region.getPosition() + '-' + (region.getPosition() + region.getCount() - 1) +
 						'/' + resourceLength);
 				println(out);
 				println(out);
-				// Printing content
+
+				// 将指定范围的内容从输入流复制到输出流
 				StreamUtils.copyRange(in, out, start, end);
+
+				// 更新输入流位置
 				inputStreamPosition += (end + 1);
 			}
-		}
-		finally {
+		} finally {
+			// 最后关闭输入流
 			try {
 				if (in != null) {
 					in.close();
 				}
-			}
-			catch (IOException ex) {
-				// ignore
+			} catch (IOException ex) {
+				// 忽略关闭异常
 			}
 		}
 
+		// 写入多部分结束标记
 		println(out);
 		print(out, "--" + boundaryString + "--");
 	}
--- spring-web/src/main/java/org/springframework/http/converter/json/KotlinSerializationJsonHttpMessageConverter.java
+++ spring-web/src/main/java/org/springframework/http/converter/json/KotlinSerializationJsonHttpMessageConverter.java
@@ -16,21 +16,12 @@
 
 package org.springframework.http.converter.json;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import kotlinx.serialization.KSerializer;
 import kotlinx.serialization.SerializationException;
 import kotlinx.serialization.SerializersKt;
 import kotlinx.serialization.descriptors.PolymorphicKind;
 import kotlinx.serialization.descriptors.SerialDescriptor;
 import kotlinx.serialization.json.Json;
-
 import org.springframework.core.GenericTypeResolver;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
@@ -42,16 +33,21 @@ import org.springframework.lang.Nullable;
 import org.springframework.util.ConcurrentReferenceHashMap;
 import org.springframework.util.StreamUtils;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
- * Implementation of {@link org.springframework.http.converter.HttpMessageConverter}
- * that can read and write JSON using
- * <a href="https://github.com/Kotlin/kotlinx.serialization">kotlinx.serialization</a>.
+ * {@link org.springframework.http.converter.HttpMessageConverter} 的实现类，
+ * 可以使用 <a href="https://github.com/Kotlin/kotlinx.serialization">kotlinx.serialization</a> 读取和写入 JSON。
  *
- * <p>This converter can be used to bind {@code @Serializable} Kotlin classes,
- * <a href="https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#open-polymorphism">open polymorphic serialization</a>
- * is not supported.
- * It supports {@code application/json} and {@code application/*+json} with
- * various character sets, {@code UTF-8} being the default.
+ * <p>此转换器可用于绑定 {@code @Serializable} Kotlin 类，
+ * 不支持 <a href="https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#open-polymorphism">开放多态序列化</a>。
+ * 它支持 {@code application/json} 和 {@code application/*+json}，并支持各种字符集，默认字符集为 {@code UTF-8}。
  *
  * @author Andreas Ahlenstorf
  * @author Sebastien Deleuze
@@ -60,22 +56,31 @@ import org.springframework.util.StreamUtils;
  */
 public class KotlinSerializationJsonHttpMessageConverter extends AbstractGenericHttpMessageConverter<Object> {
 
+	/**
+	 * 默认字符集
+	 */
 	private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
+	/**
+	 * 序列化器缓存
+	 */
 	private static final Map<Type, KSerializer<Object>> serializerCache = new ConcurrentReferenceHashMap<>();
 
+	/**
+	 * JSON实例
+	 */
 	private final Json json;
 
 
 	/**
-	 * Construct a new {@code KotlinSerializationJsonHttpMessageConverter} with the default configuration.
+	 * 使用默认配置构造一个新的 {@code KotlinSerializationJsonHttpMessageConverter}。
 	 */
 	public KotlinSerializationJsonHttpMessageConverter() {
 		this(Json.Default);
 	}
 
 	/**
-	 * Construct a new {@code KotlinSerializationJsonHttpMessageConverter} with a custom configuration.
+	 * 使用自定义配置构造一个新的 {@code KotlinSerializationJsonHttpMessageConverter}。
 	 */
 	public KotlinSerializationJsonHttpMessageConverter(Json json) {
 		super(MediaType.APPLICATION_JSON, new MediaType("application", "*+json"));
@@ -86,10 +91,12 @@ public class KotlinSerializationJsonHttpMessageConverter extends AbstractGeneric
 	@Override
 	protected boolean supports(Class<?> clazz) {
 		try {
+			// 调用序列化器方法处理类
 			serializer(clazz);
+			// 如果没有异常，返回true
 			return true;
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
+			// 如果发生异常，返回false
 			return false;
 		}
 	}
@@ -97,21 +104,26 @@ public class KotlinSerializationJsonHttpMessageConverter extends AbstractGeneric
 	@Override
 	public boolean canRead(Type type, @Nullable Class<?> contextClass, @Nullable MediaType mediaType) {
 		try {
+			// 调用 序列化器 方法处理通过 泛型类型解析器 解析出的类型
 			serializer(GenericTypeResolver.resolveType(type, contextClass));
+			// 检查指定的媒体类型是否可写
 			return canRead(mediaType);
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
+			// 如果发生异常，返回false
 			return false;
 		}
 	}
 
 	@Override
 	public boolean canWrite(@Nullable Type type, Class<?> clazz, @Nullable MediaType mediaType) {
+		// 尝试执行
 		try {
+			// 如果类型不为null，通过 泛型类型解析器 解析类型，否则使用类
 			serializer(type != null ? GenericTypeResolver.resolveType(type, clazz) : clazz);
+			// 检查指定的媒体类型是否可写
 			return canWrite(mediaType);
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
+			// 如果发生异常，返回false
 			return false;
 		}
 	}
@@ -133,13 +145,18 @@ public class KotlinSerializationJsonHttpMessageConverter extends AbstractGeneric
 	private Object decode(KSerializer<Object> serializer, HttpInputMessage inputMessage)
 			throws IOException, HttpMessageNotReadableException {
 
+		// 获取输入消息的内容类型
 		MediaType contentType = inputMessage.getHeaders().getContentType();
+
+		// 将输入消息的内容流转换为字符串，使用适当的字符集
 		String jsonText = StreamUtils.copyToString(inputMessage.getBody(), getCharsetToUse(contentType));
+
 		try {
-			// TODO Use stream based API when available
+			// 尝试使用流式API（当可用时）
+			// 从字符串解码
 			return this.json.decodeFromString(serializer, jsonText);
-		}
-		catch (SerializationException ex) {
+		} catch (SerializationException ex) {
+			// 如果解析过程中发生异常，抛出 Http消息不可读异常
 			throw new HttpMessageNotReadableException("Could not read JSON: " + ex.getMessage(), ex, inputMessage);
 		}
 	}
@@ -155,58 +172,91 @@ public class KotlinSerializationJsonHttpMessageConverter extends AbstractGeneric
 			throws IOException, HttpMessageNotWritableException {
 
 		try {
+			// 使用JSON编码器将对象转换为JSON字符串
 			String json = this.json.encodeToString(serializer, object);
+
+			// 获取输出消息的内容类型
 			MediaType contentType = outputMessage.getHeaders().getContentType();
+
+			// 将JSON字符串以适当的字符集写入输出流
 			outputMessage.getBody().write(json.getBytes(getCharsetToUse(contentType)));
+
+			// 刷新输出流
 			outputMessage.getBody().flush();
-		}
-		catch (IOException ex) {
+		} catch (IOException ex) {
+			// 如果IO操作发生异常，直接抛出该异常
 			throw ex;
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
+			// 如果其他异常发生，抛出 Http消息不可写异常
 			throw new HttpMessageNotWritableException("Could not write JSON: " + ex.getMessage(), ex);
 		}
 	}
 
 	private Charset getCharsetToUse(@Nullable MediaType contentType) {
+		// 如果 内容类型 不为null，并且包含字符集信息，则返回其字符集
 		if (contentType != null && contentType.getCharset() != null) {
 			return contentType.getCharset();
 		}
+
+		// 返回默认字符集
 		return DEFAULT_CHARSET;
 	}
 
 	/**
-	 * Tries to find a serializer that can marshall or unmarshall instances of the given type
-	 * using kotlinx.serialization. If no serializer can be found, an exception is thrown.
-	 * <p>Resolved serializers are cached and cached results are returned on successive calls.
-	 * TODO Avoid relying on throwing exception when https://github.com/Kotlin/kotlinx.serialization/pull/1164 is fixed
-	 * @param type the type to find a serializer for
-	 * @return a resolved serializer for the given type
-	 * @throws RuntimeException if no serializer supporting the given type can be found
+	 * 尝试查找能够使用 kotlinx.serialization 对给定类型的实例进行序列化或反序列化的序列化器。
+	 * 如果找不到适合的序列化器，则抛出异常。
+	 * <p>已解析的序列化器将被缓存，并在后续调用时返回缓存的结果。
+	 * TODO 当 https://github.com/Kotlin/kotlinx.serialization/pull/1164 被修复后，避免依赖于抛出异常
+	 *
+	 * @param type 要查找序列化器的类型
+	 * @return 给定类型的已解析序列化器
+	 * @throws RuntimeException 如果找不到支持给定类型的序列化器
 	 */
 	private KSerializer<Object> serializer(Type type) {
+		// 从 序列化器 缓存中获取与type对应的序列化器
 		KSerializer<Object> serializer = serializerCache.get(type);
+
+		// 如果 序列化器 为null，则需要创建并缓存新的序列化器
 		if (serializer == null) {
+			// 使用SerializersKt.serializer方法创建序列化器
 			serializer = SerializersKt.serializer(type);
+
+			// 检查序列化器是否具有多态性，如果有则抛出不支持的操作异常
 			if (hasPolymorphism(serializer.getDescriptor(), new HashSet<>())) {
 				throw new UnsupportedOperationException("Open polymorphic serialization is not supported yet");
 			}
+
+			// 将新创建的序列化器放入 序列化器缓存 中缓存起来
 			serializerCache.put(type, serializer);
 		}
+
+		// 返回获取到的序列化器
 		return serializer;
 	}
 
 	private boolean hasPolymorphism(SerialDescriptor descriptor, Set<String> alreadyProcessed) {
+		// 将当前描述符的序列化名称添加到已处理集合中
 		alreadyProcessed.add(descriptor.getSerialName());
+
+		// 如果描述符的种类是开放式多态
 		if (descriptor.getKind().equals(PolymorphicKind.OPEN.INSTANCE)) {
+			// 返回true
 			return true;
 		}
-		for (int i = 0 ; i < descriptor.getElementsCount() ; i++) {
+
+		// 遍历描述符的所有元素
+		for (int i = 0; i < descriptor.getElementsCount(); i++) {
+			// 获取当前元素的序列化描述符
 			SerialDescriptor elementDescriptor = descriptor.getElementDescriptor(i);
+
+			// 如果已处理集合中不包含当前元素的序列化名称，并且当前元素具有多态性
 			if (!alreadyProcessed.contains(elementDescriptor.getSerialName()) && hasPolymorphism(elementDescriptor, alreadyProcessed)) {
+				// 返回true
 				return true;
 			}
 		}
+
+		// 如果以上条件都不满足，则返回false
 		return false;
 	}
 
