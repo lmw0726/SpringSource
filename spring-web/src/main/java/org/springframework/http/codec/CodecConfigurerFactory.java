0569055435b73d4b8f6b162b574e0180adb71000
LMW <CNlmw0726@gmail.com>
2024-06-21 23:04:10 +0800

    docs: 完成 CodecConfigurerFactory 的代码注释

--- spring-web/src/main/java/org/springframework/http/codec/CodecConfigurerFactory.java
+++ spring-web/src/main/java/org/springframework/http/codec/CodecConfigurerFactory.java
@@ -16,44 +16,57 @@
 
 package org.springframework.http.codec;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.util.ClassUtils;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 /**
- * Internal delegate for loading the default codec configurer class names.
- * Models a loose relationship with the default implementations in the support
- * package, literally only needing to know the default class name to use.
+ * 内部委托类，用于加载默认编解码器配置器类名。
+ * 与支持包中的默认实现形成松散的关系，仅需知道要使用的默认类名。
  *
  * @author Juergen Hoeller
- * @since 5.0.1
  * @see ClientCodecConfigurer#create()
  * @see ServerCodecConfigurer#create()
+ * @since 5.0.1
  */
 final class CodecConfigurerFactory {
 
+	/**
+	 * 默认配置程序路径
+	 */
 	private static final String DEFAULT_CONFIGURERS_PATH = "CodecConfigurer.properties";
 
+	/**
+	 * 默认编解码器配置程序映射
+	 */
 	private static final Map<Class<?>, Class<?>> defaultCodecConfigurers = new HashMap<>(4);
 
 	static {
 		try {
+			// 加载位于默认配置路径下的属性文件
 			Properties props = PropertiesLoaderUtils.loadProperties(
 					new ClassPathResource(DEFAULT_CONFIGURERS_PATH, CodecConfigurerFactory.class));
+
+			// 遍历属性文件中的所有属性名
 			for (String ifcName : props.stringPropertyNames()) {
+				// 获取接口名对应的实现类名
 				String implName = props.getProperty(ifcName);
+
+				// 根据类名加载对应的接口和实现类
 				Class<?> ifc = ClassUtils.forName(ifcName, CodecConfigurerFactory.class.getClassLoader());
 				Class<?> impl = ClassUtils.forName(implName, CodecConfigurerFactory.class.getClassLoader());
+
+				// 将接口和实现类的映射关系放入 默认编解码器配置程序映射 中
 				defaultCodecConfigurers.put(ifc, impl);
 			}
-		}
-		catch (IOException | ClassNotFoundException ex) {
+		} catch (IOException | ClassNotFoundException ex) {
+			// 捕获 IO异常 或 未找到类异常 异常，并抛出 非法状态异常
 			throw new IllegalStateException(ex);
 		}
 	}
@@ -65,10 +78,15 @@ final class CodecConfigurerFactory {
 
 	@SuppressWarnings("unchecked")
 	public static <T extends CodecConfigurer> T create(Class<T> ifc) {
+		// 获取 指定类型 对应的实现类
 		Class<?> impl = defaultCodecConfigurers.get(ifc);
+
+		// 如果实现类为 null，抛出 非法状态异常
 		if (impl == null) {
 			throw new IllegalStateException("No default codec configurer found for " + ifc);
 		}
+
+		// 实例化实现类并返回
 		return (T) BeanUtils.instantiateClass(impl);
 	}
 
