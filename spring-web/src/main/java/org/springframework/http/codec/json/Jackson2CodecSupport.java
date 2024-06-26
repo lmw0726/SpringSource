8e9e6c3b8153350c2854da444618968b64b1b1bb
LMW <CNlmw0726@gmail.com>
2024-06-26 23:10:23 +0800

    docs: 添加 Jackson2编解码和Jackson2令牌化 的代码注释

--- spring-web/src/main/java/org/springframework/http/codec/json/Jackson2CodecSupport.java
+++ spring-web/src/main/java/org/springframework/http/codec/json/Jackson2CodecSupport.java
@@ -16,23 +16,11 @@
 
 package org.springframework.http.codec.json;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
-
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.logging.Log;
-
 import org.springframework.core.GenericTypeResolver;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.ResolvableType;
@@ -47,8 +35,13 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.MimeType;
 import org.springframework.util.ObjectUtils;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.function.Consumer;
+
 /**
- * Base class providing support methods for Jackson 2.9 encoding and decoding.
+ * 提供对 Jackson 2.9 编码和解码的支持方法的基类。
  *
  * @author Sebastien Deleuze
  * @author Rossen Stoyanchev
@@ -57,55 +50,74 @@ import org.springframework.util.ObjectUtils;
 public abstract class Jackson2CodecSupport {
 
 	/**
-	 * The key for the hint to specify a "JSON View" for encoding or decoding
-	 * with the value expected to be a {@link Class}.
+	 * 用于指定编码或解码的 "JSON View" 的提示键，其值应为 {@link Class} 类型。
+	 *
 	 * @see <a href="https://www.baeldung.com/jackson-json-view-annotation">Jackson JSON Views</a>
 	 */
 	public static final String JSON_VIEW_HINT = Jackson2CodecSupport.class.getName() + ".jsonView";
 
 	/**
-	 * The key for the hint to access the actual ResolvableType passed into
-	 * {@link org.springframework.http.codec.HttpMessageReader#read(ResolvableType, ResolvableType, ServerHttpRequest, ServerHttpResponse, Map)}
-	 * (server-side only). Currently set when the method argument has generics because
-	 * in case of reactive types, use of {@code ResolvableType.getGeneric()} means no
-	 * MethodParameter source and no knowledge of the containing class.
+	 * 用于访问传递给 {@link org.springframework.http.codec.HttpMessageReader#read(ResolvableType, ResolvableType, ServerHttpRequest, ServerHttpResponse, Map)} 的实际 ResolvableType 的提示键
+	 * （仅服务器端）。目前在方法参数具有泛型时设置，因为在响应式类型的情况下，使用 {@code ResolvableType.getGeneric()} 意味着没有 MethodParameter 源，并且不知道包含的类。
 	 */
 	static final String ACTUAL_TYPE_HINT = Jackson2CodecSupport.class.getName() + ".actualType";
 
+	/**
+	 * JSON视图提示错误
+	 */
 	private static final String JSON_VIEW_HINT_ERROR =
 			"@JsonView only supported for write hints with exactly 1 class argument: ";
 
+	/**
+	 * 默认MIME类型列表
+	 */
 	private static final List<MimeType> DEFAULT_MIME_TYPES = Collections.unmodifiableList(
 			Arrays.asList(
 					MediaType.APPLICATION_JSON,
 					new MediaType("application", "*+json"),
 					MediaType.APPLICATION_NDJSON));
 
-
+	/**
+	 * 日志记录器
+	 */
 	protected final Log logger = HttpLogging.forLogName(getClass());
 
+	/**
+	 * 默认对象映射器
+	 */
 	private ObjectMapper defaultObjectMapper;
 
+	/**
+	 * 对象映射器注册表
+	 */
 	@Nullable
 	private Map<Class<?>, Map<MimeType, ObjectMapper>> objectMapperRegistrations;
 
+	/**
+	 * MIME类型列表
+	 */
 	private final List<MimeType> mimeTypes;
 
 
 	/**
-	 * Constructor with a Jackson {@link ObjectMapper} to use.
+	 * 使用指定的 Jackson {@link ObjectMapper} 构造一个实例。
+	 *
+	 * @param objectMapper 要使用的 ObjectMapper 实例
+	 * @param mimeTypes    支持的 MIME 类型
 	 */
 	protected Jackson2CodecSupport(ObjectMapper objectMapper, MimeType... mimeTypes) {
 		Assert.notNull(objectMapper, "ObjectMapper must not be null");
 		this.defaultObjectMapper = objectMapper;
+		// 如果Mime类型不为空，则返回不可更改的Mime类型列表；否则返回默认的Mime类型列表
 		this.mimeTypes = !ObjectUtils.isEmpty(mimeTypes) ?
 				Collections.unmodifiableList(Arrays.asList(mimeTypes)) : DEFAULT_MIME_TYPES;
 	}
 
 
 	/**
-	 * Configure the default ObjectMapper instance to use.
-	 * @param objectMapper the ObjectMapper instance
+	 * 配置默认使用的 ObjectMapper 实例。
+	 *
+	 * @param objectMapper 要使用的 ObjectMapper 实例
 	 * @since 5.3.4
 	 */
 	public void setObjectMapper(ObjectMapper objectMapper) {
@@ -114,51 +126,59 @@ public abstract class Jackson2CodecSupport {
 	}
 
 	/**
-	 * Return the {@link #setObjectMapper configured} default ObjectMapper.
+	 * 返回 {@link #setObjectMapper(ObjectMapper) 配置的} 默认 ObjectMapper。
+	 *
+	 * @return 默认的 ObjectMapper 实例
 	 */
 	public ObjectMapper getObjectMapper() {
 		return this.defaultObjectMapper;
 	}
 
 	/**
-	 * Configure the {@link ObjectMapper} instances to use for the given
-	 * {@link Class}. This is useful when you want to deviate from the
-	 * {@link #getObjectMapper() default} ObjectMapper or have the
-	 * {@code ObjectMapper} vary by {@code MediaType}.
-	 * <p><strong>Note:</strong> Use of this method effectively turns off use of
-	 * the default {@link #getObjectMapper() ObjectMapper} and supported
-	 * {@link #getMimeTypes() MimeTypes} for the given class. Therefore it is
-	 * important for the mappings configured here to
-	 * {@link MediaType#includes(MediaType) include} every MediaType that must
-	 * be supported for the given class.
-	 * @param clazz the type of Object to register ObjectMapper instances for
-	 * @param registrar a consumer to populate or otherwise update the
-	 * MediaType-to-ObjectMapper associations for the given Class
+	 * 配置用于特定 {@link Class} 的 {@link ObjectMapper} 实例。这在需要偏离默认的
+	 * {@link #getObjectMapper() ObjectMapper} 或者根据 {@code MediaType} 变化的
+	 * {@code ObjectMapper} 时非常有用。
+	 * <p><strong>注意:</strong> 使用此方法实际上会关闭给定类的默认 {@link #getObjectMapper() ObjectMapper}
+	 * 和支持的 {@link #getMimeTypes() MimeTypes}。因此，这里配置的映射需要 {@link MediaType#includes(MediaType)}
+	 * 包括所有必须支持的给定类的 MediaType。
+	 *
+	 * @param clazz     要注册 ObjectMapper 实例的对象类型
+	 * @param registrar 用于填充或更新给定类的 MediaType-to-ObjectMapper 关联的消费者
 	 * @since 5.3.4
 	 */
 	public void registerObjectMappersForType(Class<?> clazz, Consumer<Map<MimeType, ObjectMapper>> registrar) {
+		// 如果 对象映射器注册表 为 null
 		if (this.objectMapperRegistrations == null) {
+			// 初始化 对象映射器注册表 为一个新的 LinkedHashMap
 			this.objectMapperRegistrations = new LinkedHashMap<>();
 		}
+
+		// 从 对象映射器注册表 中获取 指定类型 的注册表，如果不存在则创建一个新的 LinkedHashMap
 		Map<MimeType, ObjectMapper> registrations =
 				this.objectMapperRegistrations.computeIfAbsent(clazz, c -> new LinkedHashMap<>());
+
+		// 调用 注册消费函数 的 accept 方法，消费注册表
 		registrar.accept(registrations);
 	}
 
 	/**
-	 * Return ObjectMapper registrations for the given class, if any.
-	 * @param clazz the class to look up for registrations for
-	 * @return a map with registered MediaType-to-ObjectMapper registrations,
-	 * or empty if in case of no registrations for the given class.
+	 * 返回给定类的 ObjectMapper 注册表（如果有）。
+	 *
+	 * @param clazz 要查找注册表的类
+	 * @return 注册的 MediaType-to-ObjectMapper 映射的地图，如果没有注册表，则为空
 	 * @since 5.3.4
 	 */
 	@Nullable
 	public Map<MimeType, ObjectMapper> getObjectMappersForType(Class<?> clazz) {
+		// 遍历 对象映射器注册表 的条目集合
 		for (Map.Entry<Class<?>, Map<MimeType, ObjectMapper>> entry : getObjectMapperRegistrations().entrySet()) {
+			// 如果 条目 的键（Class 类型）是 目标类型 的父类或同类
 			if (entry.getKey().isAssignableFrom(clazz)) {
+				// 返回 条目 的值（对应的注册表）
 				return entry.getValue();
 			}
 		}
+		// 如果没有找到匹配的条目，返回一个空的 Map
 		return Collections.emptyMap();
 	}
 
@@ -167,51 +187,77 @@ public abstract class Jackson2CodecSupport {
 	}
 
 	/**
-	 * Subclasses should expose this as "decodable" or "encodable" mime types.
+	 * 子类应该将此方法公开为 "decodable" 或 "encodable" 的 MIME 类型。
+	 *
+	 * @return 支持的 MIME 类型列表
 	 */
 	protected List<MimeType> getMimeTypes() {
 		return this.mimeTypes;
 	}
 
+	/**
+	 * 根据给定的元素类型获取 MIME 类型。
+	 * <p>如果为特定类型注册了特定的 ObjectMapper 实例，那么将返回这些 MIME 类型。
+	 * <p>否则，返回默认的 MIME 类型。
+	 *
+	 * @param elementType 元素类型
+	 * @return 支持的 MIME 类型列表
+	 */
 	protected List<MimeType> getMimeTypes(ResolvableType elementType) {
+		// 获取 元素类型 的类对象
 		Class<?> elementClass = elementType.toClass();
 		List<MimeType> result = null;
+		// 遍历 对象映射器注册表 的条目集合
 		for (Map.Entry<Class<?>, Map<MimeType, ObjectMapper>> entry : getObjectMapperRegistrations().entrySet()) {
+			// 如果 条目 的键（Class 类型）是 元素类 的父类或同类
 			if (entry.getKey().isAssignableFrom(elementClass)) {
+				// 如果 结果列表 为 null，则初始化 结果列表
 				result = (result != null ? result : new ArrayList<>(entry.getValue().size()));
+				// 将 条目 中值的所有键（MimeType）添加到 结果列表 中
 				result.addAll(entry.getValue().keySet());
 			}
 		}
+		// 如果 结果列表 为空，则返回默认的 MIME 类型集合；否则返回 结果列表
 		return (CollectionUtils.isEmpty(result) ? getMimeTypes() : result);
 	}
 
 	protected boolean supportsMimeType(@Nullable MimeType mimeType) {
+		// 如果 MIME类型 为空，则返回 true
 		if (mimeType == null) {
 			return true;
 		}
+
+		// 遍历支持的 MIME 类型集合
 		for (MimeType supportedMimeType : this.mimeTypes) {
+			// 如果支持的 MIME类型 兼容传入的 MIME类型 ，则返回 true
 			if (supportedMimeType.isCompatibleWith(mimeType)) {
 				return true;
 			}
 		}
+
+		// 如果没有找到兼容的 MIME 类型，则返回 false
 		return false;
 	}
 
 	/**
-	 * Determine whether to log the given exception coming from a
-	 * {@link ObjectMapper#canDeserialize} / {@link ObjectMapper#canSerialize} check.
-	 * @param type the class that Jackson tested for (de-)serializability
-	 * @param cause the Jackson-thrown exception to evaluate
-	 * (typically a {@link JsonMappingException})
+	 * 确定是否记录来自 {@link ObjectMapper#canDeserialize} / {@link ObjectMapper#canSerialize} 检查的异常。
+	 *
+	 * @param type  Jackson 测试的（反）序列化类
+	 * @param cause 需要评估的 Jackson 抛出的异常（通常是 {@link JsonMappingException}）
 	 * @since 5.3.1
 	 */
 	protected void logWarningIfNecessary(Type type, @Nullable Throwable cause) {
+		// 如果 错误 为空，则返回
 		if (cause == null) {
 			return;
 		}
+
+		// 如果 日志记录器 处于调试模式
 		if (logger.isDebugEnabled()) {
+			// 构建调试信息消息
 			String msg = "Failed to evaluate Jackson " + (type instanceof JavaType ? "de" : "") +
 					"serialization for type [" + type + "]";
+			// 记录调试信息和异常原因
 			logger.debug(msg, cause);
 		}
 	}
@@ -221,24 +267,44 @@ public abstract class Jackson2CodecSupport {
 	}
 
 	protected Map<String, Object> getHints(ResolvableType resolvableType) {
+		// 获取方法参数
 		MethodParameter param = getParameter(resolvableType);
+
+		// 如果参数不为空
 		if (param != null) {
 			Map<String, Object> hints = null;
+
+			// 如果 可解析类型 有泛型
 			if (resolvableType.hasGenerics()) {
+				// 初始化 提示信息，容量为 2
 				hints = new HashMap<>(2);
+				// 添加实际类型提示信息
 				hints.put(ACTUAL_TYPE_HINT, resolvableType);
 			}
+
+			// 获取参数上的 JsonView 注解
 			JsonView annotation = getAnnotation(param, JsonView.class);
+
+			// 如果注解不为空
 			if (annotation != null) {
+				// 获取注解的值
 				Class<?>[] classes = annotation.value();
+				// 断言注解值的长度为 1，否则抛出异常
 				Assert.isTrue(classes.length == 1, JSON_VIEW_HINT_ERROR + param);
+
+				// 初始化 提示信息，如果未初始化，则容量为 1
 				hints = (hints != null ? hints : new HashMap<>(1));
+				// 添加 JSON 视图提示信息
 				hints.put(JSON_VIEW_HINT, classes[0]);
 			}
+
+			// 如果 提示信息 不为空，则返回 提示信息
 			if (hints != null) {
 				return hints;
 			}
 		}
+
+		// 如果没有任何提示信息，则返回空的提示信息
 		return Hints.none();
 	}
 
@@ -251,29 +317,40 @@ public abstract class Jackson2CodecSupport {
 	protected abstract <A extends Annotation> A getAnnotation(MethodParameter parameter, Class<A> annotType);
 
 	/**
-	 * Select an ObjectMapper to use, either the main ObjectMapper or another
-	 * if the handling for the given Class has been customized through
-	 * {@link #registerObjectMappersForType(Class, Consumer)}.
+	 * 选择一个 ObjectMapper 使用，可以是主 ObjectMapper 或者是另一个
+	 * 如果通过 {@link #registerObjectMappersForType(Class, Consumer)} 为给定类的处理进行了定制。
+	 *
 	 * @since 5.3.4
 	 */
 	@Nullable
 	protected ObjectMapper selectObjectMapper(ResolvableType targetType, @Nullable MimeType targetMimeType) {
+		// 如果目标媒体类型为空，或者 对象映射器注册表 为空
 		if (targetMimeType == null || CollectionUtils.isEmpty(this.objectMapperRegistrations)) {
+			// 返回默认的对象映射器
 			return this.defaultObjectMapper;
 		}
+
+		// 获取目标类型的 类
 		Class<?> targetClass = targetType.toClass();
+
+		// 遍历 对象映射器注册表 的条目
 		for (Map.Entry<Class<?>, Map<MimeType, ObjectMapper>> typeEntry : getObjectMapperRegistrations().entrySet()) {
+			// 如果 类型条目 的键是 目标类型 的父类或接口
 			if (typeEntry.getKey().isAssignableFrom(targetClass)) {
+				// 遍历 类型条目 的值，即 MimeType 和 对象映射器 的映射关系
 				for (Map.Entry<MimeType, ObjectMapper> objectMapperEntry : typeEntry.getValue().entrySet()) {
+					// 如果 对象映射器条目 的键包含目标媒体类型
 					if (objectMapperEntry.getKey().includes(targetMimeType)) {
+						// 返回匹配的 对象映射器
 						return objectMapperEntry.getValue();
 					}
 				}
-				// No matching registrations
+				// 没有匹配的注册项，返回 null
 				return null;
 			}
 		}
-		// No registrations
+
+		// 没有注册项匹配目标类型，返回默认的对象映射器
 		return this.defaultObjectMapper;
 	}
 
--- spring-web/src/main/java/org/springframework/http/codec/json/Jackson2Tokenizer.java
+++ spring-web/src/main/java/org/springframework/http/codec/json/Jackson2Tokenizer.java
@@ -16,11 +16,6 @@
 
 package org.springframework.http.codec.json;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -30,18 +25,21 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext;
 import com.fasterxml.jackson.databind.util.TokenBuffer;
-import reactor.core.Exceptions;
-import reactor.core.publisher.Flux;
-
 import org.springframework.core.codec.DecodingException;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferLimitException;
 import org.springframework.core.io.buffer.DataBufferUtils;
+import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 /**
- * {@link Function} to transform a JSON stream of arbitrary size, byte array
- * chunks into a {@code Flux<TokenBuffer>} where each token buffer is a
- * well-formed JSON object.
+ * {@link Function} 用于将任意大小的 JSON 流、字节数组块转换为 {@code Flux<TokenBuffer>}，
+ * 其中每个 token buffer 都是一个格式良好的 JSON 对象。
  *
  * @author Arjen Poutsma
  * @author Rossen Stoyanchev
@@ -49,33 +47,61 @@ import org.springframework.core.io.buffer.DataBufferUtils;
  * @since 5.0
  */
 final class Jackson2Tokenizer {
-
+	/**
+	 * Json解析器
+	 */
 	private final JsonParser parser;
 
+	/**
+	 * 反序列化上下文
+	 */
 	private final DeserializationContext deserializationContext;
 
+	/**
+	 * 是否标记数组元素
+	 */
 	private final boolean tokenizeArrayElements;
 
+	/**
+	 * 是否强制使用BigDecimal
+	 */
 	private final boolean forceUseOfBigDecimal;
 
+	/**
+	 * 最大内存大小
+	 */
 	private final int maxInMemorySize;
 
+	/**
+	 * 对象深度
+	 */
 	private int objectDepth;
 
+	/**
+	 * 数组深度
+	 */
 	private int arrayDepth;
 
+	/**
+	 * 字节计数
+	 */
 	private int byteCount;
 
+	/**
+	 * 令牌缓冲区
+	 */
 	private TokenBuffer tokenBuffer;
 
-
-	// TODO: change to ByteBufferFeeder when supported by Jackson
-	// See https://github.com/FasterXML/jackson-core/issues/478
+	/**
+	 * 输入喂入器
+	 * TODO: 在 Jackson 支持时更改为 ByteBufferFeeder
+	 * 查看 https://github.com/FasterXML/jackson-core/issues/478
+	 */
 	private final ByteArrayFeeder inputFeeder;
 
 
 	private Jackson2Tokenizer(JsonParser parser, DeserializationContext deserializationContext,
-			boolean tokenizeArrayElements, boolean forceUseOfBigDecimal, int maxInMemorySize) {
+							  boolean tokenizeArrayElements, boolean forceUseOfBigDecimal, int maxInMemorySize) {
 
 		this.parser = parser;
 		this.deserializationContext = deserializationContext;
@@ -87,112 +113,165 @@ final class Jackson2Tokenizer {
 	}
 
 
-
 	private List<TokenBuffer> tokenize(DataBuffer dataBuffer) {
+		// 获取数据缓冲区的可读字节数
 		int bufferSize = dataBuffer.readableByteCount();
+
+		// 创建一个字节数组
 		byte[] bytes = new byte[bufferSize];
+
+		// 将数据缓冲区中的数据读取到字节数组中
 		dataBuffer.read(bytes);
+
+		// 释放数据缓冲区
 		DataBufferUtils.release(dataBuffer);
 
 		try {
+			// 将字节数组的数据提供给输入喂入器
 			this.inputFeeder.feedInput(bytes, 0, bytes.length);
+
+			// 解析 令牌缓冲区 并返回结果列表
 			List<TokenBuffer> result = parseTokenBufferFlux();
+
+			// 断言内存大小是否符合预期
 			assertInMemorySize(bufferSize, result);
+
+			// 返回解析后的 令牌缓冲区 列表
 			return result;
-		}
-		catch (JsonProcessingException ex) {
+		} catch (JsonProcessingException ex) {
+			// 处理 JSON 解码异常
 			throw new DecodingException("JSON decoding error: " + ex.getOriginalMessage(), ex);
-		}
-		catch (IOException ex) {
+		} catch (IOException ex) {
+			// 处理 IO 异常
 			throw Exceptions.propagate(ex);
 		}
 	}
 
 	private Flux<TokenBuffer> endOfInput() {
+		// 延迟创建 Flux 对象
 		return Flux.defer(() -> {
+			// 结束输入流
 			this.inputFeeder.endOfInput();
 			try {
+				// 解析 令牌缓冲区 并 创建 Flux 对象
 				return Flux.fromIterable(parseTokenBufferFlux());
-			}
-			catch (JsonProcessingException ex) {
+			} catch (JsonProcessingException ex) {
+				// 处理 JSON 解码异常
 				throw new DecodingException("JSON decoding error: " + ex.getOriginalMessage(), ex);
-			}
-			catch (IOException ex) {
+			} catch (IOException ex) {
+				// 处理 IO 异常
 				throw Exceptions.propagate(ex);
 			}
 		});
 	}
 
 	private List<TokenBuffer> parseTokenBufferFlux() throws IOException {
+		// 创建一个 令牌缓冲区 列表
 		List<TokenBuffer> result = new ArrayList<>();
 
-		// SPR-16151: Smile data format uses null to separate documents
+		// SPR-16151: Smile 数据格式使用 null 来分隔文档
 		boolean previousNull = false;
+
+		// 当解析器未关闭时循环解析 JSON 数据
 		while (!this.parser.isClosed()) {
+			// 获取下一个 JSON 令牌
 			JsonToken token = this.parser.nextToken();
+
+			// 如果令牌为 不可用
+			// 或者为 null 且前一个令牌也为 null
 			if (token == JsonToken.NOT_AVAILABLE ||
-					token == null && previousNull) {
+					(token == null && previousNull)) {
+				// 跳出循环
 				break;
-			}
-			else if (token == null ) { // !previousNull
+			} else if (token == null) {
+				// 如果前一个令牌为 null，将前一个token为空的标志设置为true
 				previousNull = true;
+				// 继续下一个循环
 				continue;
-			}
-			else {
+			} else {
+				// 否则将前一个token为空的标志设置为false
 				previousNull = false;
 			}
+
+			// 更新对象和数组的深度
 			updateDepth(token);
+
+			// 根据配置选择处理 JSON 令牌的方式
 			if (!this.tokenizeArrayElements) {
+				// 如果不需要解析数组元素，则按普通方式处理令牌
 				processTokenNormal(token, result);
-			}
-			else {
+			} else {
+				// 如果需要解析数组元素，则按数组方式处理令牌
 				processTokenArray(token, result);
 			}
 		}
+
+		// 返回解析后的 令牌缓冲区 列表
 		return result;
 	}
 
 	private void updateDepth(JsonToken token) {
+		// 根据 JSON 令牌类型处理对象和数组的深度
 		switch (token) {
 			case START_OBJECT:
+				// 如果令牌是 开始对象，增加对象深度
 				this.objectDepth++;
 				break;
 			case END_OBJECT:
+				// 如果令牌是 结束对象，减少对象深度
 				this.objectDepth--;
 				break;
 			case START_ARRAY:
+				// 如果令牌是 开始数组，增加数组深度
 				this.arrayDepth++;
 				break;
 			case END_ARRAY:
+				// 如果令牌是 结束数组，减少数组深度
 				this.arrayDepth--;
 				break;
 		}
 	}
 
 	private void processTokenNormal(JsonToken token, List<TokenBuffer> result) throws IOException {
+		// 将当前事件从解析器复制到 令牌缓冲区 中
 		this.tokenBuffer.copyCurrentEvent(this.parser);
 
+		// 如果令牌是结构结束或标量值，并且 对象深度 为 0 且 数组深度 为 0
 		if ((token.isStructEnd() || token.isScalarValue()) && this.objectDepth == 0 && this.arrayDepth == 0) {
+			// 将当前 令牌缓冲区 添加到结果列表中
 			result.add(this.tokenBuffer);
+			// 创建一个新的 令牌缓冲区
 			this.tokenBuffer = createToken();
 		}
 	}
 
 	private void processTokenArray(JsonToken token, List<TokenBuffer> result) throws IOException {
+		// 如果当前的 JSON 令牌不是顶级数组令牌
 		if (!isTopLevelArrayToken(token)) {
+			// 将当前事件从解析器复制到 令牌缓冲区 中
 			this.tokenBuffer.copyCurrentEvent(this.parser);
 		}
 
+		// 如果 对象深度 为 0 ，
+		// 且 数组深度 为 0 或 1 ，
+		// 且令牌是 结束对象 或 标量值
 		if (this.objectDepth == 0 && (this.arrayDepth == 0 || this.arrayDepth == 1) &&
 				(token == JsonToken.END_OBJECT || token.isScalarValue())) {
+			// 将当前 令牌缓冲区 添加到结果列表中
 			result.add(this.tokenBuffer);
+			// 创建一个新的 令牌缓冲区
 			this.tokenBuffer = createToken();
 		}
 	}
 
 	private TokenBuffer createToken() {
+		// 创建一个用于存储解析的 JSON 数据的 TokenBuffer 对象
 		TokenBuffer tokenBuffer = new TokenBuffer(this.parser, this.deserializationContext);
+
+		// 强制使用 BigDecimal 来处理数字类型的数据
 		tokenBuffer.forceUseOfBigDecimal(this.forceUseOfBigDecimal);
+
+		// 返回创建的 TokenBuffer 对象
 		return tokenBuffer;
 	}
 
@@ -202,15 +281,19 @@ final class Jackson2Tokenizer {
 	}
 
 	private void assertInMemorySize(int currentBufferSize, List<TokenBuffer> result) {
+		// 如果 最大内存大小 大于等于 0
 		if (this.maxInMemorySize >= 0) {
+			// 如果 结果 不为空
 			if (!result.isEmpty()) {
+				// 将 字节计数 重置为 0
 				this.byteCount = 0;
-			}
-			else if (currentBufferSize > Integer.MAX_VALUE - this.byteCount) {
+			} else if (currentBufferSize > Integer.MAX_VALUE - this.byteCount) {
+				// 如果 当前缓冲区大小 加上 字节计数 超过了 Integer.MAX_VALUE，抛出限制异常
 				raiseLimitException();
-			}
-			else {
+			} else {
+				// 累加当前缓冲区大小到 字节计数
 				this.byteCount += currentBufferSize;
+				// 如果 字节计数 超过了 最大内存大小，抛出限制异常
 				if (this.byteCount > this.maxInMemorySize) {
 					raiseLimitException();
 				}
@@ -225,32 +308,41 @@ final class Jackson2Tokenizer {
 
 
 	/**
-	 * Tokenize the given {@code Flux<DataBuffer>} into {@code Flux<TokenBuffer>}.
-	 * @param dataBuffers the source data buffers
-	 * @param jsonFactory the factory to use
-	 * @param objectMapper the current mapper instance
-	 * @param tokenizeArrays if {@code true} and the "top level" JSON object is
-	 * an array, each element is returned individually immediately after it is received
-	 * @param forceUseOfBigDecimal if {@code true}, any floating point values encountered
-	 * in source will use {@link java.math.BigDecimal}
-	 * @param maxInMemorySize maximum memory size
-	 * @return the resulting token buffers
+	 * 将给定的 {@code Flux<DataBuffer>} 标记化为 {@code Flux<TokenBuffer>}。
+	 *
+	 * @param dataBuffers          源数据缓冲区
+	 * @param jsonFactory          用于创建 JSON 解析器的工厂
+	 * @param objectMapper         当前的 ObjectMapper 实例
+	 * @param tokenizeArrays       如果为 {@code true} 且 "顶级" JSON 对象是数组，则在接收到每个元素后立即单独返回
+	 * @param forceUseOfBigDecimal 如果为 {@code true}，则在源中遇到的任何浮点值将使用 {@link java.math.BigDecimal}
+	 * @param maxInMemorySize      最大内存大小，用于限制解析过程中使用的内存
+	 * @return 解析后的 token 缓冲区流
 	 */
 	public static Flux<TokenBuffer> tokenize(Flux<DataBuffer> dataBuffers, JsonFactory jsonFactory,
-			ObjectMapper objectMapper, boolean tokenizeArrays, boolean forceUseOfBigDecimal, int maxInMemorySize) {
+											 ObjectMapper objectMapper, boolean tokenizeArrays, boolean forceUseOfBigDecimal, int maxInMemorySize) {
 
 		try {
+			// 创建一个非阻塞的字节数组解析器
 			JsonParser parser = jsonFactory.createNonBlockingByteArrayParser();
+
+			// 获取 ObjectMapper 的反序列化上下文
 			DeserializationContext context = objectMapper.getDeserializationContext();
+
+			// 如果反序列化上下文是 DefaultDeserializationContext 的实例
 			if (context instanceof DefaultDeserializationContext) {
+				// 创建一个新的反序列化上下文实例
 				context = ((DefaultDeserializationContext) context).createInstance(
 						objectMapper.getDeserializationConfig(), parser, objectMapper.getInjectableValues());
 			}
+
+			// 创建 Jackson2Tokenizer 对象，用于将 JSON 数据进行标记化处理
 			Jackson2Tokenizer tokenizer =
 					new Jackson2Tokenizer(parser, context, tokenizeArrays, forceUseOfBigDecimal, maxInMemorySize);
+
+			// 将数据缓冲区进行连接并标记化处理，最后与 输入结束结果 进行连接
 			return dataBuffers.concatMapIterable(tokenizer::tokenize).concatWith(tokenizer.endOfInput());
-		}
-		catch (IOException ex) {
+		} catch (IOException ex) {
+			// 如果发生 IO异常，则返回一个错误的 Flux 对象
 			return Flux.error(ex);
 		}
 	}
