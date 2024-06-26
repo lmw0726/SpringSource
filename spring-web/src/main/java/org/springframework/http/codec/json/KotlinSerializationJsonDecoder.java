9880fa82f789629928ff6cfbdc9990514d99fbc7
LMW <CNlmw0726@gmail.com>
2024-06-26 12:58:34 +0800

    docs: 添加 Kotlin序列化相关代码 的注释

--- spring-web/src/main/java/org/springframework/http/codec/json/KotlinSerializationJsonDecoder.java
+++ spring-web/src/main/java/org/springframework/http/codec/json/KotlinSerializationJsonDecoder.java
@@ -16,20 +16,12 @@
 
 package org.springframework.http.codec.json;
 
-import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import kotlinx.serialization.KSerializer;
 import kotlinx.serialization.SerializersKt;
 import kotlinx.serialization.descriptors.PolymorphicKind;
 import kotlinx.serialization.descriptors.SerialDescriptor;
 import kotlinx.serialization.json.Json;
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
 import org.springframework.core.ResolvableType;
 import org.springframework.core.codec.AbstractDecoder;
 import org.springframework.core.codec.StringDecoder;
@@ -38,31 +30,44 @@ import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ConcurrentReferenceHashMap;
 import org.springframework.util.MimeType;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * Decode a byte stream into JSON and convert to Object's with
- * <a href="https://github.com/Kotlin/kotlinx.serialization">kotlinx.serialization</a>.
+ * 使用 <a href="https://github.com/Kotlin/kotlinx.serialization">kotlinx.serialization</a>
+ * 将字节流解码为 JSON 并转换为对象。
  *
- * <p>This decoder can be used to bind {@code @Serializable} Kotlin classes,
- * <a href="https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#open-polymorphism">open polymorphic serialization</a>
- * is not supported.
- * It supports {@code application/json} and {@code application/*+json} with
- * various character sets, {@code UTF-8} being the default.
+ * <p>此解码器可用于绑定 {@code @Serializable} Kotlin 类，
+ * 不支持 <a href="https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#open-polymorphism">开放多态序列化</a>。
+ * 它支持带有各种字符集的 {@code application/json} 和 {@code application/*+json}，默认字符集为 {@code UTF-8}。
  *
- * <p>Decoding streams is not supported yet, see
+ * <p>解码流尚不支持，详情见
  * <a href="https://github.com/Kotlin/kotlinx.serialization/issues/1073">kotlinx.serialization/issues/1073</a>
- * related issue.
+ * 相关问题。
  *
  * @author Sebastien Deleuze
  * @since 5.3
  */
 public class KotlinSerializationJsonDecoder extends AbstractDecoder<Object> {
 
+	/**
+	 * 类型 —— 序列化器缓存
+	 */
 	private static final Map<Type, KSerializer<Object>> serializerCache = new ConcurrentReferenceHashMap<>();
 
+	/**
+	 * Json实例
+	 */
 	private final Json json;
 
-	// String decoding needed for now, see https://github.com/Kotlin/kotlinx.serialization/issues/204 for more details
+	/**
+	 * 目前需要字符串解码，详情见 https://github.com/Kotlin/kotlinx.serialization/issues/204
+	 */
 	private final StringDecoder stringDecoder = StringDecoder.allMimeTypes(StringDecoder.DEFAULT_DELIMITERS, false);
 
 
@@ -76,22 +81,20 @@ public class KotlinSerializationJsonDecoder extends AbstractDecoder<Object> {
 	}
 
 	/**
-	 * Configure a limit on the number of bytes that can be buffered whenever
-	 * the input stream needs to be aggregated. This can be a result of
-	 * decoding to a single {@code DataBuffer},
-	 * {@link java.nio.ByteBuffer ByteBuffer}, {@code byte[]},
-	 * {@link org.springframework.core.io.Resource Resource}, {@code String}, etc.
-	 * It can also occur when splitting the input stream, e.g. delimited text,
-	 * in which case the limit applies to data buffered between delimiters.
-	 * <p>By default this is set to 256K.
-	 * @param byteCount the max number of bytes to buffer, or -1 for unlimited
+	 * 配置在需要聚合输入流时可以缓冲的最大字节数。这可能是解码到单个 {@code DataBuffer}、
+	 * {@link java.nio.ByteBuffer ByteBuffer}、{@code byte[]}、
+	 * {@link org.springframework.core.io.Resource Resource}、{@code String} 等的结果。
+	 * 这也可能在拆分输入流时发生，例如分隔文本，在这种情况下，限制适用于分隔符之间缓冲的数据。
+	 * <p>默认情况下设置为 256K。
+	 *
+	 * @param byteCount 最大缓冲字节数，或 -1 表示无限制
 	 */
 	public void setMaxInMemorySize(int byteCount) {
 		this.stringDecoder.setMaxInMemorySize(byteCount);
 	}
 
 	/**
-	 * Return the {@link #setMaxInMemorySize configured} byte count limit.
+	 * 返回 {@link #setMaxInMemorySize 配置的}字节数限制。
 	 */
 	public int getMaxInMemorySize() {
 		return this.stringDecoder.getMaxInMemorySize();
@@ -101,62 +104,85 @@ public class KotlinSerializationJsonDecoder extends AbstractDecoder<Object> {
 	@Override
 	public boolean canDecode(ResolvableType elementType, @Nullable MimeType mimeType) {
 		try {
+			// 序列化元素类型的类型
 			serializer(elementType.getType());
+			// 是否可以解码，且 元素类型 不是 CharSequence 的子类
 			return (super.canDecode(elementType, mimeType) && !CharSequence.class.isAssignableFrom(elementType.toClass()));
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			return false;
 		}
 	}
 
 	@Override
 	public Flux<Object> decode(Publisher<DataBuffer> inputStream, ResolvableType elementType,
-			@Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
+							   @Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
 
 		return Flux.error(new UnsupportedOperationException());
 	}
 
 	@Override
 	public Mono<Object> decodeToMono(Publisher<DataBuffer> inputStream, ResolvableType elementType,
-			@Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
+									 @Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
 
+		// 将 输入流 解码为 Mono
 		return this.stringDecoder
 				.decodeToMono(inputStream, elementType, mimeType, hints)
+				// 解码为指定类型的对象
 				.map(jsonText -> this.json.decodeFromString(serializer(elementType.getType()), jsonText));
 	}
 
 	/**
-	 * Tries to find a serializer that can marshall or unmarshall instances of the given type
-	 * using kotlinx.serialization. If no serializer can be found, an exception is thrown.
-	 * <p>Resolved serializers are cached and cached results are returned on successive calls.
-	 * TODO Avoid relying on throwing exception when https://github.com/Kotlin/kotlinx.serialization/pull/1164 is fixed
-	 * @param type the type to find a serializer for
-	 * @return a resolved serializer for the given type
-	 * @throws RuntimeException if no serializer supporting the given type can be found
+	 * 尝试找到能够使用 kotlinx.serialization 编组或解组给定类型实例的序列化器。如果找不到序列化器，将抛出异常。
+	 * <p>解析的序列化器将被缓存，并在后续调用时返回缓存结果。
+	 * TODO 避免依赖抛出异常，当 https://github.com/Kotlin/kotlinx.serialization/pull/1164 修复时
+	 *
+	 * @param type 要查找序列化器的类型
+	 * @return 给定类型的解析序列化器
+	 * @throws RuntimeException 如果找不到支持给定类型的序列化器
 	 */
 	private KSerializer<Object> serializer(Type type) {
+		// 从 序列化器缓存 中获取指定类型的序列化器
 		KSerializer<Object> serializer = serializerCache.get(type);
+
+		// 如果缓存中没有对应的序列化器
 		if (serializer == null) {
+			// 使用 SerializersKt 创建新的序列化器
 			serializer = SerializersKt.serializer(type);
+
+			// 检查序列化器的描述符是否包含多态性
 			if (hasPolymorphism(serializer.getDescriptor(), new HashSet<>())) {
+				// 如果包含多态性，抛出不支持的操作异常
 				throw new UnsupportedOperationException("Open polymorphic serialization is not supported yet");
 			}
+
+			// 将新的序列化器存入缓存
 			serializerCache.put(type, serializer);
 		}
+
+		// 返回序列化器
 		return serializer;
 	}
 
 	private boolean hasPolymorphism(SerialDescriptor descriptor, Set<String> alreadyProcessed) {
+		// 将当前描述符的名称添加到 已处理集合 中
 		alreadyProcessed.add(descriptor.getSerialName());
+
+		// 如果描述符的类型是开放的多态类型，则返回 true
 		if (descriptor.getKind().equals(PolymorphicKind.OPEN.INSTANCE)) {
 			return true;
 		}
-		for (int i = 0 ; i < descriptor.getElementsCount() ; i++) {
+
+		// 遍历描述符的所有元素
+		for (int i = 0; i < descriptor.getElementsCount(); i++) {
+			// 获取当前元素的描述符
 			SerialDescriptor elementDescriptor = descriptor.getElementDescriptor(i);
+			// 如果当前元素的名称不在 已处理集合 中，并且递归调用 hasPolymorphism 方法返回 true，则返回 true
 			if (!alreadyProcessed.contains(elementDescriptor.getSerialName()) && hasPolymorphism(elementDescriptor, alreadyProcessed)) {
 				return true;
 			}
 		}
+
+		// 如果所有检查都不符合条件，则返回 false
 		return false;
 	}
 
--- spring-web/src/main/java/org/springframework/http/codec/json/KotlinSerializationJsonEncoder.java
+++ spring-web/src/main/java/org/springframework/http/codec/json/KotlinSerializationJsonEncoder.java
@@ -16,21 +16,12 @@
 
 package org.springframework.http.codec.json;
 
-import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import kotlinx.serialization.KSerializer;
 import kotlinx.serialization.SerializersKt;
 import kotlinx.serialization.descriptors.PolymorphicKind;
 import kotlinx.serialization.descriptors.SerialDescriptor;
 import kotlinx.serialization.json.Json;
 import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
 import org.springframework.core.ResolvableType;
 import org.springframework.core.codec.AbstractEncoder;
 import org.springframework.core.codec.CharSequenceEncoder;
@@ -41,27 +32,51 @@ import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.lang.Nullable;
 import org.springframework.util.ConcurrentReferenceHashMap;
 import org.springframework.util.MimeType;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * Encode from an {@code Object} stream to a byte stream of JSON objects using
- * <a href="https://github.com/Kotlin/kotlinx.serialization">kotlinx.serialization</a>.
+ * 使用 <a href="https://github.com/Kotlin/kotlinx.serialization">kotlinx.serialization</a>
+ * 从 {@code Object} 流编码到 JSON 对象的字节流。
  *
- * <p>This encoder can be used to bind {@code @Serializable} Kotlin classes,
- * <a href="https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#open-polymorphism">open polymorphic serialization</a>
- * is not supported.
- * It supports {@code application/json} and {@code application/*+json} with
- * various character sets, {@code UTF-8} being the default.
+ * <p>此编码器可用于绑定 {@code @Serializable} Kotlin 类，
+ * 不支持 <a href="https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/polymorphism.md#open-polymorphism">开放多态序列化</a>。
+ * 它支持带有各种字符集的 {@code application/json} 和 {@code application/*+json}，默认字符集为 {@code UTF-8}。
+ *
+ * <p>注意：默认情况下，Protobuf 消息的实例生成空字节数组，因此通过网络发送 {@code Mono.just(Msg.getDefaultInstance())}
+ * 将被反序列化为空的 {@link Mono}。
+ *
+ * <p>要生成 {@code Message} Java 类，需要安装 {@code protoc} 二进制文件。
+ *
+ * <p>此解码器需要 Protobuf 3 或更高版本，并支持官方的 {@code "com.google.protobuf:protobuf-java"} 库的
+ * {@code "application/x-protobuf"} 和 {@code "application/octet-stream"}。
+ *
+ * <p>此编码器需要 Protobuf 3 或更高版本，并支持官方的 {@code "com.google.protobuf:protobuf-java"} 库的
+ * {@code "application/x-protobuf"} 和 {@code "application/octet-stream"}。
  *
  * @author Sebastien Deleuze
  * @since 5.3
  */
 public class KotlinSerializationJsonEncoder extends AbstractEncoder<Object> {
-
+	/**
+	 * 类型 —— 解析序列化器缓存。
+	 */
 	private static final Map<Type, KSerializer<Object>> serializerCache = new ConcurrentReferenceHashMap<>();
 
+	/**
+	 * Json实例
+	 */
 	private final Json json;
 
-	// CharSequence encoding needed for now, see https://github.com/Kotlin/kotlinx.serialization/issues/204 for more details
+	/**
+	 * 目前需要 CharSequence 编码，详情见 https://github.com/Kotlin/kotlinx.serialization/issues/204
+	 */
 	private final CharSequenceEncoder charSequenceEncoder = CharSequenceEncoder.allMimeTypes();
 
 
@@ -78,73 +93,101 @@ public class KotlinSerializationJsonEncoder extends AbstractEncoder<Object> {
 	@Override
 	public boolean canEncode(ResolvableType elementType, @Nullable MimeType mimeType) {
 		try {
+			// 序列化元素类型的类型
 			serializer(elementType.getType());
+			// 如果可以进行编码，并且 元素类型 不是 String 类型，也不是 ServerSentEvent 类型，则返回 true
 			return (super.canEncode(elementType, mimeType) && !String.class.isAssignableFrom(elementType.toClass()) &&
 					!ServerSentEvent.class.isAssignableFrom(elementType.toClass()));
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
+			// 如果出现异常，则返回 false
 			return false;
 		}
 	}
 
 	@Override
 	public Flux<DataBuffer> encode(Publisher<?> inputStream, DataBufferFactory bufferFactory,
-			ResolvableType elementType, @Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
+								   ResolvableType elementType, @Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
 
 		if (inputStream instanceof Mono) {
+			// 如果 输入流 是 Mono 的实例
 			return Mono.from(inputStream)
+					// 将 Mono 中的值进行编码
 					.map(value -> encodeValue(value, bufferFactory, elementType, mimeType, hints))
+					// 将 Mono 转换为 Flux
 					.flux();
-		}
-		else {
+		} else {
+			// 如果 输入流 不是 Mono 的实例
+			// 获取带有泛型的 List 类型
 			ResolvableType listType = ResolvableType.forClassWithGenerics(List.class, elementType);
+			// 将 输入流 转换为 Flux
 			return Flux.from(inputStream)
+					// 将 Flux 中的元素收集到一个 List 中
 					.collectList()
+					// 对 List 进行编码
 					.map(list -> encodeValue(list, bufferFactory, listType, mimeType, hints))
+					// 将 Mono 转换为 Flux
 					.flux();
 		}
 	}
 
 	@Override
 	public DataBuffer encodeValue(Object value, DataBufferFactory bufferFactory,
-			ResolvableType valueType, @Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
+								  ResolvableType valueType, @Nullable MimeType mimeType, @Nullable Map<String, Object> hints) {
 
+		// 对 值和序列化类型 进行编码，得到 JSON 字符串
 		String json = this.json.encodeToString(serializer(valueType.getType()), value);
+
+		// 对 JSON 字符串进行编码，并返回结果
 		return this.charSequenceEncoder.encodeValue(json, bufferFactory, valueType, mimeType, null);
 	}
 
 	/**
-	 * Tries to find a serializer that can marshall or unmarshall instances of the given type
-	 * using kotlinx.serialization. If no serializer can be found, an exception is thrown.
-	 * <p>Resolved serializers are cached and cached results are returned on successive calls.
-	 * TODO Avoid relying on throwing exception when https://github.com/Kotlin/kotlinx.serialization/pull/1164 is fixed
-	 * @param type the type to find a serializer for
-	 * @return a resolved serializer for the given type
-	 * @throws RuntimeException if no serializer supporting the given type can be found
+	 * 尝试找到能够使用 kotlinx.serialization 编组或解组给定类型实例的序列化器。如果找不到序列化器，将抛出异常。
+	 * <p>解析的序列化器将被缓存，并在后续调用时返回缓存结果。
+	 * TODO 避免依赖抛出异常，当 https://github.com/Kotlin/kotlinx.serialization/pull/1164 修复时
+	 *
+	 * @param type 要查找序列化器的类型
+	 * @return 给定类型的解析序列化器
+	 * @throws RuntimeException 如果找不到支持给定类型的序列化器
 	 */
 	private KSerializer<Object> serializer(Type type) {
+		// 从 序列化器缓存 中获取 类型 对应的序列化器
 		KSerializer<Object> serializer = serializerCache.get(type);
 		if (serializer == null) {
+			// 如果缓存中没有，则使用 SerializersKt 获取序列化器
 			serializer = SerializersKt.serializer(type);
+			// 检查序列化器的描述符是否具有多态性
 			if (hasPolymorphism(serializer.getDescriptor(), new HashSet<>())) {
+				// 如果具有多态性，则抛出不支持的操作异常
 				throw new UnsupportedOperationException("Open polymorphic serialization is not supported yet");
 			}
+			// 将新的序列化器缓存到 序列化器缓存 中
 			serializerCache.put(type, serializer);
 		}
+		// 返回序列化器
 		return serializer;
 	}
 
 	private boolean hasPolymorphism(SerialDescriptor descriptor, Set<String> alreadyProcessed) {
+		// 将当前描述符的名称添加到 已处理集合 中
 		alreadyProcessed.add(descriptor.getSerialName());
+
+		// 如果描述符的类型是开放的多态类型，则返回 true
 		if (descriptor.getKind().equals(PolymorphicKind.OPEN.INSTANCE)) {
 			return true;
 		}
-		for (int i = 0 ; i < descriptor.getElementsCount() ; i++) {
+
+		// 遍历描述符的所有元素
+		for (int i = 0; i < descriptor.getElementsCount(); i++) {
+			// 获取当前元素的描述符
 			SerialDescriptor elementDescriptor = descriptor.getElementDescriptor(i);
+			// 如果当前元素的名称不在 已处理集合 中，并且递归调用 hasPolymorphism 方法返回 true，则返回 true
 			if (!alreadyProcessed.contains(elementDescriptor.getSerialName()) && hasPolymorphism(elementDescriptor, alreadyProcessed)) {
 				return true;
 			}
 		}
+
+		// 如果所有检查都不符合条件，则返回 false
 		return false;
 	}
 
