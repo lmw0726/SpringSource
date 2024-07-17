3ae2de1b4a4c328f5edbf939ecc476e0540feebb
LMW <CNlmw0726@gmail.com>
2024-07-17 23:32:08 +0800

    feat: 添加 PathVariableMethodArgumentResolver 的示例

---
+++ demo/spring-boot-learn/src/main/java/com/lmw/springmvc/argument/PathVariableMethodController.java
@@ -0,0 +1,44 @@
+package com.lmw.springmvc.argument;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * 参数解析器
+ * 带有@PathVariable的方法参数解析器示例
+ *
+ * @author LMW
+ * @version 1.0
+ * @since 2024-07-15 23:17
+ */
+@Controller
+@RequestMapping("/argument")
+public class PathVariableMethodController {
+	/**
+	 * 带有@PathVariable 路径变量的示例
+	 *
+	 * @param name 变量名称
+	 * @return 含有路径变量名称的结果
+	 */
+	@GetMapping("/path/variable/{name}")
+	public ResponseEntity<String> pathVariableName(@PathVariable("name") String name) {
+		return ResponseEntity.ok(name);
+	}
+
+	/**
+	 * 带有@PathVariable 路径变量，且使用Map参数进行接收的示例
+	 *
+	 * @param map 变量名称和值
+	 * @return 含有路径变量Map的结果
+	 */
+	@GetMapping("/path/variable/map/{name}/{value}")
+	public ResponseEntity<Map<String, Object>> pathVariableMap(@PathVariable Map<String, Object> map) {
+		return ResponseEntity.ok(map);
+	}
+
+}
