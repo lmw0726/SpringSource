1ad133a1c1274de33efa4a9c8ed0d47dbaf953e5
LMW <CNlmw0726@gmail.com>
2024-08-01 23:06:20 +0800

    feat: 添加 @MatrixVariable注解 的参数解析器示例

---
+++ demo/spring-boot-learn/src/main/java/com/lmw/springmvc/argument/MatrixVariableMapMethodController.java
@@ -0,0 +1,48 @@
+package com.lmw.springmvc.argument;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.MatrixVariableMapMethodArgumentResolver;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 带有@MatrixVariable注解的Map的示例
+ * 这个控制器是 {@link MatrixVariableMapMethodArgumentResolver} 参数解析器的一个示例。
+ *
+ * @author LMW
+ * @version 1.0
+ * @since 2024-08-01 23:04
+ */
+@Controller
+@RequestMapping("/argument")
+public class MatrixVariableMapMethodController {
+	/**
+	 * 多个路径变量
+	 * 访问 /argument/matrixVariable/first;a=1;b=2/second;c=3;d=4
+	 * 可以接受到结果
+	 *
+	 * @param first     第一个路径变量
+	 * @param firstMap  第一个矩阵变量
+	 * @param second    第二个路径变量
+	 * @param secondMap 第二个矩阵变量
+	 * @return 解析结果
+	 */
+	@GetMapping("/matrixVariable/{first}/{second}")
+	public ResponseEntity<Map<String, Object>> matrixMulti(
+			@PathVariable("first") String first,
+			@MatrixVariable(pathVar = "first") MultiValueMap<String, String> firstMap,
+			@PathVariable("second") String second,
+			@MatrixVariable(pathVar = "second") MultiValueMap<String, String> secondMap) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("first", firstMap);
+		map.put("second", secondMap);
+		return ResponseEntity.ok(map);
+	}
+}
---
+++ demo/spring-boot-learn/src/main/java/com/lmw/springmvc/argument/MatrixVariableMethodController.java
@@ -0,0 +1,86 @@
+package com.lmw.springmvc.argument;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.MatrixVariableMethodArgumentResolver;
+import org.springframework.web.util.UrlPathHelper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 方法上标注@MaxtrixVariable注解的示例
+ * 这个控制器是 {@link MatrixVariableMethodArgumentResolver} 参数解析器的一个示例。
+ *
+ * @author LMW
+ * @version 1.0
+ * @since 2024-08-01 21:23
+ */
+@Controller
+@RequestMapping("/argument")
+public class MatrixVariableMethodController {
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void configurePathMatch(PathMatchConfigurer configurer) {
+				UrlPathHelper urlPathHelper = new UrlPathHelper();
+				urlPathHelper.setRemoveSemicolonContent(false);
+				configurer.setUrlPathHelper(urlPathHelper);
+			}
+		};
+	}
+
+
+	/**
+	 * 矩阵变量列表示例，使用@MatrixVariable 修饰 List<String>
+	 * 访问：/argument/matrixVariable/list/list;matrixVariable=111,123
+	 * 可以接收到参数
+	 *
+	 * @param matrixVariable 矩阵变量列表
+	 * @return 解析结果
+	 */
+	@GetMapping("/matrixVariable/list/{list}")
+	public ResponseEntity<List<String>> matrixVariableList(@PathVariable String list, @MatrixVariable List<String> matrixVariable) {
+		return ResponseEntity.ok(matrixVariable);
+	}
+
+	/**
+	 * 矩阵变量映射示例，使用@MatrixVariable 修饰  Map<String, Object>
+	 * 访问 /argument/matrixVariable/map/map;name=LMW;age=18
+	 * 不能使用Map接收，因为返回的类型实际上是 List<String>，导致参数转换错误
+	 *
+	 * @param map 矩阵变量映射
+	 * @return 解析结果
+	 */
+	@GetMapping("/matrixVariable/map/{map}")
+	public ResponseEntity<String> matrixVariableMap(@PathVariable String map, @MatrixVariable("name") Map<String, Object> names) {
+		return ResponseEntity.ok(names.toString());
+	}
+
+	/**
+	 * 使用@MatrixVariable 修饰 String 示例
+	 * 访问 /argument/matrixVariable/string/map;name=LMW;age=18
+	 * 可以接收到结果
+	 *
+	 * @param name 名称
+	 * @param age  年龄
+	 * @return 解析结果
+	 */
+	@GetMapping("/matrixVariable/string/{pathName}")
+	public ResponseEntity<String> matrixVariableMap(@PathVariable String pathName,
+													@MatrixVariable String name,
+													@MatrixVariable String age) {
+		return ResponseEntity.ok(pathName + " " + name + " " + age);
+	}
+
+
+}
